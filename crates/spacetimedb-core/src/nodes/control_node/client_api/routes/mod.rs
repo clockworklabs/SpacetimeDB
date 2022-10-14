ce5282d20988a93b203b9fca6284f5987337a1b2
Ryan Daum <ryan@clockworklabs.io>
2022-10-13 20:16:38 -0400

    Rename budget_router -> energy_router

--- crates/spacetimedb-core/src/nodes/control_node/client_api/routes/mod.rs
+++ crates/spacetimedb-core/src/nodes/control_node/client_api/routes/mod.rs
@@ -7,7 +7,7 @@ mod prometheus;
 
 use self::prometheus::router as prometheus_router;
 use database::router as database_router;
-use energy::router as budget_router;
+use energy::router as energy_router;
 use gotham::{
     prelude::*,
     router::{build_simple_router, Router},
@@ -19,7 +19,7 @@ use node::router as node_router;
 pub fn router() -> Router {
     build_simple_router(|route| {
         route.delegate("/database").to_router(database_router());
-        route.delegate("/energy").to_router(budget_router());
+        route.delegate("/energy").to_router(energy_router());
         route.delegate("/identity").to_router(identity_router());
         route.delegate("/node").to_router(node_router());
         route.delegate("/prometheus").to_router(prometheus_router());
