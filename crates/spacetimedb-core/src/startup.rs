85fe92810bea5f837e2012561787548ef250dbf1
Noa <coolreader18@gmail.com>
2022-11-02 12:09:35 -0500

    Use cargo-chef for docker (#123)
    * Use cargo-chef for docker
    
    * Use cargo-binstall
    
    * Use buildkit to build in CI
    
    * reverted changes related to llvm
    
    Co-authored-by: Tyler Cloutier <cloutiertyler@aol.com>

--- crates/spacetimedb-core/src/startup.rs
+++ crates/spacetimedb-core/src/startup.rs
@@ -1,7 +1,18 @@
+use std::path::Path;
+
 pub fn configure_logging() {
     // Use this to change log levels at runtime.
     // This means you can change the default log level to trace
     // if you are trying to debug an issue and need more logs on then turn it off
     // once you are done.
-    log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+    let env = std::env::var_os("SPACETIMEDB_LOG_CONFIG");
+    let log4rs_file = env.as_deref().unwrap_or_else(|| {
+        let local = Path::new("log4rs.yaml");
+        if local.exists() {
+            local.as_ref()
+        } else {
+            "/etc/spacetimedb/log4rs.yaml".as_ref()
+        }
+    });
+    log4rs::init_file(log4rs_file, Default::default()).unwrap();
 }
