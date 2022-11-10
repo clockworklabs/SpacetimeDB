e3dc909083dd5f9833e8c947c1b8a9f492a8a1bf
Tyler Cloutier <cloutiertyler@aol.com>
2022-11-10 16:32:57 +0000

    Added a script to publish all of the crates

---
+++ publish-crates.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -euo pipefail
+
+DRY_RUN=""
+
+if [ "$#" != "0" ]; then
+    if [ "$1" != "--dry-run" ]; then
+        echo "$1 is not a valid flag";
+        exit 1;
+    else
+        DRY_RUN=$1
+    fi
+fi
+
+cd crates/spacetimedb-lib
+cargo publish $DRY_RUN
+cd ../..
+
+cd crates/spacetimedb-bindings-sys
+cargo publish $DRY_RUN
+cd ../..
+
+cd crates/spacetimedb-bindings-macro
+cargo publish $DRY_RUN
+cd ../..
+
+sleep 5
+
+cd crates/spacetimedb-bindings
+cargo publish $DRY_RUN
+cd ../..
+
+sleep 5
+
+cd crates/spacetimedb-cli
+cargo publish $DRY_RUN
+cd ../..
