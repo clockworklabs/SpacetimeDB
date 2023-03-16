f3d3ae3357e141268fb0f265224984dd4dd62ade
John Detter <no-reply@boppygames.gg>
2023-03-16 09:57:09 -0500

    Some converter updates

--- sdks/csharp/Assets/SpacetimeDB/Scripts/SomeConverter.cs
+++ sdks/csharp/Assets/SpacetimeDB/Scripts/SomeConverter.cs
@@ -1,37 +1,46 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace SpacetimeDB
 {
     public class SomeConverter : JsonConverter
     {
-        public override bool CanConvert(Type objectType) => true;
+        public override bool CanConvert(Type objectType) => ignoreValues.Where(value => value != null).All(value => value.GetType() != objectType);
 
         private readonly List<object> convertValues = new List<object>();
+        private readonly List<object> ignoreValues = new List<object>();
 
         public void Add(object o) => convertValues.Add(o);
-        
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        public void AddIgnore(object o) => ignoreValues.Add(o);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
         {
             throw new NotImplementedException();
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
+            var internalSerializer = new JsonSerializer
+            {
+                ContractResolver = serializer.ContractResolver,
+                DateFormatHandling = serializer.DateFormatHandling,
+                // Add any other settings you need from the original serializer
+            };
+
             if (convertValues.Contains(value))
             {
                 writer.WriteStartObject();
                 writer.WritePropertyName("some");
-                writer.WriteRawValue(JsonConvert.SerializeObject(value));
+                internalSerializer.Serialize(writer, value);
                 writer.WriteEndObject();
             }
             else
             {
-                writer.WriteRaw(JsonConvert.SerializeObject(value));
+                internalSerializer.Serialize(writer, value);
             }
-            
-            writer.WriteValue(BitConverter.ToString((byte[])value).Replace("-", string.Empty));
         }
     }
 }
