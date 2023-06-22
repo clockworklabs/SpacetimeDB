efd7c7f970a8ace0aab8b38526be9638c2ecbc28
Boppy <no-reply@boppygames.gg>
2023-06-22 11:09:17 -0500

    Removed unneeded clone

--- crates/core/src/host/wasm_common/module_host_actor.rs
+++ crates/core/src/host/wasm_common/module_host_actor.rs
@@ -556,7 +556,7 @@ impl<T: WasmInstance> WasmInstanceActor<T> {
             for orphan in known_tables.into_keys() {
                 if !orphan.starts_with("st_") {
                     self.system_logger()
-                        .warn(format!("Orphaned table: {}", orphan.clone()).as_str());
+                        .warn(format!("Orphaned table: {}", orphan).as_str());
                     tainted.push(orphan);
                 }
             }
