4e7da2cf3eb5a50b2d26de318405171bfdb1fb8c
Mazdak Farrokhzad <twingoow@gmail.com>
2023-06-22 23:43:53 +0200

    Refactor `bindings` a bit + document it (#8)
    * grandfathered docs improvements in bindings
    
    * improve bindings
    
    * address review comments
    
    * remove late bound lifetime
    
    * undo derive for Timestamp to bisect bug

--- crates/bindings/src/io.rs
+++ crates/bindings/src/io.rs
@@ -1,27 +1,36 @@
+//! Defines macros for basic IO corresponding to what the standard library provides.
+//! These macros include `println`, `print`, `eprintln`, `eprint`, and `dbg`.
+
+/// Prints the provided arguments, terminated by a newline, as an `INFO` message to the console.
 #[doc(hidden)]
 #[macro_export]
 macro_rules! println {
     ($($arg:tt)*) => ($crate::log::info!($($arg)*))
 }
 
+/// Prints the provided arguments as an `INFO` message to the console.
 #[doc(hidden)]
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => ($crate::log::info!($($arg)*))
 }
 
+/// Prints the provided arguments, terminated by a newline, as an `ERROR` message to the console.
 #[doc(hidden)]
 #[macro_export]
 macro_rules! eprintln {
     ($($arg:tt)*) => ($crate::log::error!($($arg)*))
 }
 
+/// Prints the provided arguments as an `ERROR` message to the console.
 #[doc(hidden)]
 #[macro_export]
 macro_rules! eprint {
     ($($arg:tt)*) => ($crate::log::error!($($arg)*))
 }
 
+/// Functions like the standard library's `dbg!` macro
+/// but prints as a `DEBUG` message to the console.
 #[macro_export]
 macro_rules! dbg {
     // NOTE: We cannot use `concat!` to make a static string as a format argument
