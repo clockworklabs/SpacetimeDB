bb8cf44a65a3320a58d9fa51fe7cca9c41d27dd7
Boppy <no-reply@boppygames.gg>
2023-07-01 11:55:55 -0500

    retab

--- test/run-smoke-tests.sh
+++ test/run-smoke-tests.sh
@@ -37,27 +37,27 @@ export SPACETIME_HOME=$PWD
 
 execute_test() {
     reset_test_out
-	reset_config
-	reset_project
+    reset_config
+    reset_project
     echo "TEST_OUT=$TEST_OUT PROJECT_PATH=$PROJECT_PATH SPACETIME_CONFIG_FILE=$SPACETIME_CONFIG_FILE"
-	TEST_PATH="test/tests/$1.sh"
-	printf " **************** Running %s... " "$1"
+    TEST_PATH="test/tests/$1.sh"
+    printf " **************** Running %s... " "$1"
     RETURN_CODE=0
-	if ! bash -x "$TEST_PATH" > "$OUT_TMP" 2>&1 ; then
-		printf "${RED}FAIL${CRST}\n"
-		cat "$OUT_TMP"
-		echo "Config file:"
+    if ! bash -x "$TEST_PATH" > "$OUT_TMP" 2>&1 ; then
+        printf "${RED}FAIL${CRST}\n"
+        cat "$OUT_TMP"
+        echo "Config file:"
         cat "$SPACETIME_CONFIG_FILE"
-		# docker logs "$CONTAINER_NAME"
-		failed_tests+=("$1")
+        # docker logs "$CONTAINER_NAME"
+        failed_tests+=("$1")
 
         if [ "$RUN_PARALLEL" == "true" ] ; then
             RETURN_CODE=1
         fi
-	else
-		printf "${GRN}PASS${CRST}\n"
-		passed_tests+=("$1")
-	fi
+    else
+        printf "${GRN}PASS${CRST}\n"
+        passed_tests+=("$1")
+    fi
 
     rm -rf "$PROJECT_PATH" "$TEST_OUT" "$SPACETIME_CONFIG_FILE"
     if [[ $RETURN_CODE != 0 ]] ; then
@@ -67,14 +67,14 @@ execute_test() {
 }
 
 list_contains() {
-	local a=$1
-	shift
-	for x in "$@"; do
-		if [[ "$x" == "$a" ]]; then
-			return 0
-		fi
-	done
-	return 1
+    local a=$1
+    shift
+    for x in "$@"; do
+        if [[ "$x" == "$a" ]]; then
+            return 0
+        fi
+    done
+    return 1
 }
 
 TESTS=(./test/tests/*.sh)
@@ -84,22 +84,22 @@ TESTS=("${TESTS[@]%.sh}")
 EXCLUDE_TESTS=()
 
 while [ $# != 0 ] ; do
-	case $1 in
-		-x)
-			shift
-			EXCLUDE_TESTS+=("$@")
+    case $1 in
+        -x)
+            shift
+            EXCLUDE_TESTS+=("$@")
             break
-		;;
+        ;;
         --parallel)
             shift
             RUN_PARALLEL=true
             echo "Running tests in parallel."
         ;;
-		*)
-			TESTS=("$@")
+        *)
+            TESTS=("$@")
             break
-		;;
-	esac
+        ;;
+    esac
 done
 
 TESTS_PID=()
@@ -108,11 +108,11 @@ TESTS_NAME=()
 for smoke_test in "${TESTS[@]}" ; do
     break;
 
-	if [ ${#EXCLUDE_TESTS[@]} -ne 0 ] && list_contains "$smoke_test" "${EXCLUDE_TESTS[@]}"; then
+    if [ ${#EXCLUDE_TESTS[@]} -ne 0 ] && list_contains "$smoke_test" "${EXCLUDE_TESTS[@]}"; then
         echo "Skipping test $smoke_test"
-		continue
-	fi
-	if [ -f "./test/tests/$smoke_test.sh" ]; then
+        continue
+    fi
+    if [ -f "./test/tests/$smoke_test.sh" ]; then
         if [ "$RUN_PARALLEL" == "true" ] ; then
             # Skip any non-parallizable tests if we're running in parallel
             if [[ "$smoke_test" == zz_* ]] ; then
@@ -125,51 +125,51 @@ for smoke_test in "${TESTS[@]}" ; do
             TESTS_OUT+=("$process_output_file")
             TESTS_NAME+=("$smoke_test")
         else
-		    execute_test "$smoke_test"
+            execute_test "$smoke_test"
         fi
-	else
-		echo "Unknown test: $smoke_test"
-		exit 1
-	fi
+    else
+        echo "Unknown test: $smoke_test"
+        exit 1
+    fi
 done
 
 
 if [ "$RUN_PARALLEL" == "true" ] ; then
     # Wait for all processes to end, and save their exit codes
-    # length=${#TESTS_PID[@]}
-    # for ((i=0; i<length; i++)) ; do
-    #     pid=${TESTS_PID[$i]}
-    #     out_file=${TESTS_OUT[$i]}
-    #     test_name=${TESTS_NAME[$i]}
-    #     set +e
-    #     wait "$pid"
-    #     RESULT_CODE=$?
-    #     set -e
-    #     echo "Process result code: $RESULT_CODE"
-    #     if [ $RESULT_CODE == 0 ] ; then
-	# 	    passed_tests+=("$test_name")
-    #     else
-    #         echo "+------------------------------------------+"
-    #         printf "$RED TEST FAILURE:$CRST $test_name\n"
-    #         echo
-    #         cat "$out_file"
-	# 	    failed_tests+=("$test_name")
-    #     fi
-    # done
+    length=${#TESTS_PID[@]}
+    for ((i=0; i<length; i++)) ; do
+        pid=${TESTS_PID[$i]}
+        out_file=${TESTS_OUT[$i]}
+        test_name=${TESTS_NAME[$i]}
+        set +e
+        wait "$pid"
+        RESULT_CODE=$?
+        set -e
+        echo "Process result code: $RESULT_CODE"
+        if [ $RESULT_CODE == 0 ] ; then
+            passed_tests+=("$test_name")
+        else
+            echo "+------------------------------------------+"
+            printf "$RED TEST FAILURE:$CRST $test_name\n"
+            echo
+            cat "$out_file"
+            failed_tests+=("$test_name")
+        fi
+    done
 
     # Now run any tests that cannot be parallelized
     for smoke_test in "${TESTS[@]}" ; do
-	    if [ ${#EXCLUDE_TESTS[@]} -ne 0 ] && list_contains "$smoke_test" "${EXCLUDE_TESTS[@]}"; then
-		    continue
-	    fi
+        if [ ${#EXCLUDE_TESTS[@]} -ne 0 ] && list_contains "$smoke_test" "${EXCLUDE_TESTS[@]}"; then
+            continue
+        fi
 
         if [[ "$smoke_test" == zz_* ]]; then
-	        if [ -f "./test/tests/$smoke_test.sh" ]; then
-		        execute_test "$smoke_test"
-	        else
-		        echo "Unknown test: $smoke_test"
-		        exit 1
-	        fi
+            if [ -f "./test/tests/$smoke_test.sh" ]; then
+                execute_test "$smoke_test"
+            else
+                echo "Unknown test: $smoke_test"
+                exit 1
+            fi
         fi
 
     done
@@ -182,25 +182,25 @@ printf "** Smoke Tests Summary **\n"
 printf "*************************\n\n"
 
 if [ ${#passed_tests[@]} -ne 0 ]; then
-	printf "${GRN}Passed${CRST} Tests:\n"
-	for t in "${passed_tests[@]}" ; do
-		echo "	$t"
-	done
+    printf "${GRN}Passed${CRST} Tests:\n"
+    for t in "${passed_tests[@]}" ; do
+        echo "  $t"
+    done
 fi
 
 if [ ${#failed_tests[@]} -eq 0 ] ; then
-	printf "\nNo failures reported.\n\n"
+    printf "\nNo failures reported.\n\n"
 else
-	printf "\n${RED}Failed${CRST} Tests:\n"
-	for t in "${failed_tests[@]}" ; do
-		echo "	$t"
-	done
+    printf "\n${RED}Failed${CRST} Tests:\n"
+    for t in "${failed_tests[@]}" ; do
+        echo "  $t"
+    done
 
-	printf "\nDescriptions for failed tests:\n"
-	for t in "${failed_tests[@]}" ; do
-		printf "\n%s\n\t" "$t"
-		DESCRIBE_TEST=1 bash "test/tests/${t}.sh"
-	done
+    printf "\nDescriptions for failed tests:\n"
+    for t in "${failed_tests[@]}" ; do
+        printf "\n%s\n\t" "$t"
+        DESCRIBE_TEST=1 bash "test/tests/${t}.sh"
+    done
 
-	exit 1
+    exit 1
 fi
