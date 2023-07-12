218ebe33f1579137a8d856e7ae97cc71d6e6248a
Mazdak Farrokhzad <twingoow@gmail.com>
2023-07-12 16:45:52 +0200

    document sats::bsatn

--- crates/sats/src/bsatn/ser.rs
+++ crates/sats/src/bsatn/ser.rs
@@ -4,31 +4,39 @@ use crate::buffer::BufWriter;
 
 use crate::ser::{self, Error, ForwardNamedToSeqProduct, Serialize, SerializeArray, SerializeMap, SerializeSeqProduct};
 
+/// Defines the BSATN serialization data format.
 pub struct Serializer<'a, W> {
     writer: &'a mut W,
 }
 
 impl<'a, W> Serializer<'a, W> {
+    /// Returns a serializer using the given `writer`.
     pub fn new(writer: &'a mut W) -> Self {
         Self { writer }
     }
 
+    /// Reborrows the serializer.
     #[inline]
     fn reborrow(&mut self) -> Serializer<'_, W> {
         Serializer { writer: self.writer }
     }
 }
 
+/// An error during BSATN serialization.
 #[derive(Debug)]
 pub struct BsatnError {
+    /// The error message for the BSATN error.
     custom: String,
 }
+
 impl fmt::Display for BsatnError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str(&self.custom)
     }
+
 }
 impl std::error::Error for BsatnError {}
+
 impl Error for BsatnError {
     fn custom<T: fmt::Display>(msg: T) -> Self {
         let custom = msg.to_string();
@@ -36,6 +44,9 @@ impl Error for BsatnError {
     }
 }
 
+/// Writes `len` converted to a `u32` to `writer`.
+///
+/// Errors if `len` would not fit in a `u32`.
 fn put_len(writer: &mut impl BufWriter, len: usize) -> Result<(), BsatnError> {
     let len = len.try_into().map_err(|_| BsatnError::custom("len too long"))?;
     writer.put_u32(len);
@@ -106,22 +117,23 @@ impl<W: BufWriter> ser::Serializer for Serializer<'_, W> {
         self.serialize_bytes(v.as_bytes())
     }
     fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
-        put_len(self.writer, v.len())?;
+        put_len(self.writer, v.len())?; // N.B. `v.len() > u32::MAX` isn't allowed.
         self.writer.put_slice(v);
         Ok(())
     }
     fn serialize_array(self, len: usize) -> Result<Self::SerializeArray, Self::Error> {
-        put_len(self.writer, len)?;
+        put_len(self.writer, len)?; // N.B. `len > u32::MAX` isn't allowed.
         Ok(self)
     }
     fn serialize_map(self, len: usize) -> Result<Self::SerializeMap, Self::Error> {
-        put_len(self.writer, len)?;
+        put_len(self.writer, len)?; // N.B. `len > u32::MAX` isn't allowed.
         Ok(self)
     }
     fn serialize_seq_product(self, _len: usize) -> Result<Self::SerializeSeqProduct, Self::Error> {
         Ok(self)
     }
     fn serialize_named_product(self, len: usize) -> Result<Self::SerializeNamedProduct, Self::Error> {
+        // Serialize named like unnamed.
         self.serialize_seq_product(len).map(ForwardNamedToSeqProduct::new)
     }
     fn serialize_variant<T: super::Serialize + ?Sized>(
