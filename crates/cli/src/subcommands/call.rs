2fa7a95c0a4c45df2ea9d5bea3369edb7c9f243f
Boppy <no-reply@boppygames.gg>
2023-07-22 14:43:52 -0500

    I don't know why this change was necessary, but this fixes the compilation issue

--- crates/cli/src/subcommands/call.rs
+++ crates/cli/src/subcommands/call.rs
@@ -246,28 +246,25 @@ async fn schema_json(config: Config, address: &str, auth_header: &Option<String>
 /// Returns all the names of items in `value` that match `type`.
 ///
 /// For example, `type` can be `"reducer"`.
-fn find_of_type_in_schema<'v, 't: 'v>(value: &'v Value, ty: &'t str) -> impl Iterator<Item = (&'v str, &'v Value)> {
-    let entities = match value
-        .as_object()
+fn find_of_type_in_schema<'v, 't: 'v>(
+    value: &'v serde_json::Value,
+    ty: &'t str,
+) -> impl Iterator<Item = (&'v str, &'v Value)> {
+    let Some(entities) = value.as_object()
         .and_then(|o| o.get("entities"))
         .and_then(|e| e.as_object())
-    {
-        Some(e) => e,
-        None => return Either::Left(iter::empty()),
-    };
+    else { return Either::Left(iter::empty()) };
 
     let iter = entities
         .into_iter()
         .filter(move |(_, value)| {
-            let obj = match value.as_object() {
-                Some(o) => o,
-                None => return false,
-            };
+            let Some(obj) = value.as_object() else { return false; };
             obj.get("type").filter(|x| x.as_str() == Some(ty)).is_some()
         })
         .map(|(key, value)| (key.as_str(), value));
     Either::Right(iter)
 }
+
 /// Returns the `Typespace` in the provided json schema.
 fn typespace(value: &serde_json::Value) -> Option<Typespace> {
     let types = value.as_object()?.get("typespace")?;
