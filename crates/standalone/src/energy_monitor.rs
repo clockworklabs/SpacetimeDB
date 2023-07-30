0dc9ee546e3559adf558fb75155d85b2b5c13b6c
Tyler Cloutier <cloutiertyler@aol.com>
2023-07-30 22:26:22 +0200

    Infinite budget in Standalone

--- crates/standalone/src/energy_monitor.rs
+++ crates/standalone/src/energy_monitor.rs
@@ -22,8 +22,9 @@ impl StandaloneEnergyMonitor {
 }
 
 impl EnergyMonitor for StandaloneEnergyMonitor {
-    fn reducer_budget(&self, fingerprint: &EnergyMonitorFingerprint<'_>) -> EnergyQuanta {
-        self.inner.lock().unwrap().reducer_budget(fingerprint)
+    fn reducer_budget(&self, _fingerprint: &EnergyMonitorFingerprint<'_>) -> EnergyQuanta {
+        // Infinitely large reducer budget in Standalone
+        EnergyQuanta(i128::max_value())
     }
 
     fn record(
@@ -52,7 +53,8 @@ impl Inner {
         self.standalone_env = Arc::downgrade(&worker_env);
     }
 
-    fn reducer_budget(&self, fingerprint: &EnergyMonitorFingerprint<'_>) -> EnergyQuanta {
+    /// To be used if we ever want to enable reducer budgets in Standalone
+    fn _reducer_budget(&self, fingerprint: &EnergyMonitorFingerprint<'_>) -> EnergyQuanta {
         let standalone_env = self.standalone_env.upgrade().expect("Standalone env was dropped.");
         let balance = standalone_env.control_db.get_energy_balance(&fingerprint.module_identity).unwrap().unwrap_or(EnergyQuanta(0));
         EnergyQuanta(i128::max(balance.0, 0))
