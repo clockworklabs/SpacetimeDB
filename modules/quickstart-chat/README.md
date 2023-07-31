03a4ba93d3cc1c820028c984df9633b35a2a8c1d
Phoebe Goldman <phoebe@goldman-tribe.org>
2023-07-31 11:02:35 -0400

    Add `quickstart-chat` example module and client

---
+++ modules/quickstart-chat/README.md
@@ -0,0 +1,22 @@
+# `quickstart-chat`
+
+A SpacetimeDB module which defines a simple chat server. This module is explained in-depth by [the SpacetimeDB Rust module quickstart](https://spacetimedb.com/docs/server-languages/rust/rust-module-quickstart-guide).
+
+## Clients
+
+### Rust
+
+A Rust command-line client for this module is defined in [the Rust SDK's examples](/crates/sdk/examples/quickstart-chat), and described by [the SpacetimeDB Rust client quickstart](https://spacetimedb.com/docs/client-languages/rust/rust-sdk-quickstart-guide).
+
+### C#
+
+A C# command-line client for this module is defined in [the C# SDK's examples](https://github.com/clockworklabs/spacetimedb-csharp-sdk/tree/master/examples/quickstart/client), and described by [the SpacetimeDB C# client quickstart](https://spacetimedb.com/docs/client-languages/csharp-sdk/csharp-sdk-quickstart-guide).
+
+### Python
+
+A Python command-line client for this module is defined in [the Python SDK's examples](https://github.com/clockworklabs/spacetimedb-python-sdk/tree/master/examples/quickstart/client), and described by [the SpacetimeDB Python client quickstart](https://spacetimedb.com/docs/client-languages/python/python-sdk-quickstart-guide).
+
+### TypeScript
+
+
+A web client for this module, built with TypeScript and React, is defined in [the TypeScript SDK's examples](https://github.com/spacetimedb-typescript-sdk/tree/master/examples/quickstart/client), and described by [the SpacetimeDB TypeScript client quickstart](https://spacetimedb.com/docs/client-languages/typescript/typescript-sdk-quickstart-guide).
---
+++ modules/quickstart-chat/src/lib.rs
@@ -0,0 +1,96 @@
+use spacetimedb::{spacetimedb, Identity, ReducerContext, Timestamp};
+
+#[spacetimedb(table)]
+pub struct User {
+    #[primarykey]
+    identity: Identity,
+    name: Option<String>,
+    online: bool,
+}
+
+#[spacetimedb(table)]
+pub struct Message {
+    sender: Identity,
+    sent: Timestamp,
+    text: String,
+}
+
+fn validate_name(name: String) -> Result<String, String> {
+    if name.is_empty() {
+        Err("Names must not be empty".to_string())
+    } else {
+        Ok(name)
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn set_name(ctx: ReducerContext, name: String) -> Result<(), String> {
+    let name = validate_name(name)?;
+    if let Some(user) = User::filter_by_identity(&ctx.sender) {
+        User::update_by_identity(
+            &ctx.sender,
+            User {
+                name: Some(name),
+                ..user
+            },
+        );
+        Ok(())
+    } else {
+        Err("Cannot set name for unknown user".to_string())
+    }
+}
+
+fn validate_message(text: String) -> Result<String, String> {
+    if text.is_empty() {
+        Err("Messages must not be empty".to_string())
+    } else {
+        Ok(text)
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn send_message(ctx: ReducerContext, text: String) -> Result<(), String> {
+    // Things to consider:
+    // - Rate-limit messages per-user.
+    // - Reject messages from unnamed users.
+    let text = validate_message(text)?;
+    Message::insert(Message {
+        sender: ctx.sender,
+        text,
+        sent: ctx.timestamp,
+    });
+    Ok(())
+}
+
+#[spacetimedb(init)]
+// Called when the module is initially published
+pub fn init() {}
+
+#[spacetimedb(connect)]
+pub fn identity_connected(ctx: ReducerContext) {
+    if let Some(user) = User::filter_by_identity(&ctx.sender) {
+        // If this is a returning user, i.e. we already have a `User` with this `Identity`,
+        // set `online: true`, but leave `name` and `identity` unchanged.
+        User::update_by_identity(&ctx.sender, User { online: true, ..user });
+    } else {
+        // If this is a new user, create a `User` row for the `Identity`,
+        // which is online, but hasn't set a name.
+        User::insert(User {
+            name: None,
+            identity: ctx.sender,
+            online: true,
+        })
+        .unwrap();
+    }
+}
+
+#[spacetimedb(disconnect)]
+pub fn identity_disconnected(ctx: ReducerContext) {
+    if let Some(user) = User::filter_by_identity(&ctx.sender) {
+        User::update_by_identity(&ctx.sender, User { online: false, ..user });
+    } else {
+        // This branch should be unreachable,
+        // as it doesn't make sense for a client to disconnect without connecting first.
+        log::warn!("Disconnect event for unknown user with identity {:?}", ctx.sender);
+    }
+}
