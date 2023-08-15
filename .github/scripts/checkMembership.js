69c8ba9269a652ad568a37baa118c804fec5d3d9
Boppy <no-reply@boppygames.gg>
2023-08-15 16:21:12 -0500

    Added node-fetch

--- .github/scripts/checkMembership.js
+++ .github/scripts/checkMembership.js
@@ -1,9 +1,13 @@
 const { Octokit } = require("@octokit/rest");
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fetch = require("node-fetch");
 
 const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
+    request: {
+        fetch: fetch
+    }
 });
 
 async function main() {
