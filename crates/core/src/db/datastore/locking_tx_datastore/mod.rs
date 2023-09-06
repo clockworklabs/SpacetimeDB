31dbb8171fa112794337662c90215cce77307cd3
Mario Montoya <mamcx@elmalabarista.com>
2023-09-06 00:47:24 -0500

    Fix recovery of sequences after restart (#158)
    * Remove unused datastore traits
    
    We previously anticipated factoring the database in such a way that
    the typed vs untyped distinctions, and the transactional vs not,
    semantics would be more visible. We have not used these distinctions
    and no longer need to expose these traits.
    
    * Fix recovery of sequences after restart
    
    ---------
    
    Co-authored-by: George Kulakowski <george@clockworklabs.io>

--- crates/core/src/db/datastore/locking_tx_datastore/mod.rs
+++ crates/core/src/db/datastore/locking_tx_datastore/mod.rs
@@ -384,6 +384,7 @@ impl Inner {
 
             // If any columns are auto incrementing, we need to create a sequence
             // NOTE: This code with the `seq_start` is particularly fragile.
+            // TODO: If we exceed  `SEQUENCE_PREALLOCATION_AMOUNT` we will get a unique violation
             if col.is_autoinc {
                 // The database is bootstrapped with the total of `SystemTables::total_` that identify what is the start of the sequence
                 let (seq_start, seq_id): (i128, SequenceId) = match TableId(schema.table_id) {
@@ -490,10 +491,25 @@ impl Inner {
         let rows = st_sequences.scan_rows().cloned().collect::<Vec<_>>();
         for row in rows {
             let sequence = StSequenceRow::try_from(&row)?;
+            // TODO: The system tables have initialized their value already, but this is wrong:
+            // If we exceed  `SEQUENCE_PREALLOCATION_AMOUNT` we will get a unique violation
+            let is_system_table = self
+                .committed_state
+                .tables
+                .get(&TableId(sequence.table_id))
+                .map_or(false, |x| x.schema.table_type == StTableType::System);
+
             let schema = (&sequence).into();
+
+            let mut seq = Sequence::new(schema);
+            // Now we need to recover the last allocation value.
+            if !is_system_table && seq.value < sequence.allocated + 1 {
+                seq.value = sequence.allocated + 1;
+            }
+
             self.sequence_state
                 .sequences
-                .insert(SequenceId(sequence.sequence_id), Sequence::new(schema));
+                .insert(SequenceId(sequence.sequence_id), seq);
         }
         Ok(())
     }
@@ -574,8 +590,9 @@ impl Inner {
                 return Err(SequenceError::NotFound(seq_id).into());
             };
 
-            // If there are allocated sequence values, return the new value.
-            if let Some(value) = sequence.gen_next_value() {
+            // If there are allocated sequence values, return the new value, if it is not bigger than
+            // the upper range of `sequence.allocated`
+            if let Some(value) = sequence.gen_next_value().filter(|v| v < &sequence.allocated()) {
                 return Ok(value);
             }
         }
@@ -597,8 +614,7 @@ impl Inner {
             };
             let old_seq_row_id = RowId(old_seq_row.to_data_key());
             let mut seq_row = StSequenceRow::try_from(&old_seq_row)?;
-            let num_to_allocate = 1024;
-            seq_row.allocated = sequence.nth_value(num_to_allocate);
+            seq_row.allocated = sequence.nth_value(SEQUENCE_PREALLOCATION_AMOUNT as usize);
             sequence.set_allocation(seq_row.allocated);
             (seq_row, old_seq_row_id)
         };
@@ -631,7 +647,7 @@ impl Inner {
             sequence_name: seq.sequence_name.as_str(),
             table_id: seq.table_id,
             col_id: seq.col_id,
-            allocated: 0,
+            allocated: seq.start.unwrap_or(1),
             increment: seq.increment,
             start: seq.start.unwrap_or(1),
             min_value: seq.min_value.unwrap_or(1),
