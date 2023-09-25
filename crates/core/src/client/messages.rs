94c77d61aed9557a1b9700f6b3e4eb55aef93d95
Phoebe Goldman <phoebe@goldman-tribe.org>
2023-09-25 13:04:16 -0400

    Add caller address to `EventJson` message

--- crates/core/src/client/messages.rs
+++ crates/core/src/client/messages.rs
@@ -75,6 +75,7 @@ impl ServerMessage for TransactionUpdateMessage<'_> {
             },
             energy_quanta_used: event.energy_quanta_used.0,
             message: errmsg,
+            caller_address: event.caller_address.unwrap_or(Address::ZERO).to_hex(),
         };
 
         let subscription_update = database_update.into_json();
