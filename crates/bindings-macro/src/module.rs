f17bb06353de75992403389eb10307e507e48144
Noa <coolreader18@gmail.com>
2023-09-27 02:58:49 -0500

    Fix derive(Deserialize) on void enum

--- crates/bindings-macro/src/module.rs
+++ crates/bindings-macro/src/module.rs
@@ -342,7 +342,7 @@ pub(crate) fn derive_deserialize(ty: &SatsType<'_>) -> TokenStream {
                         type Output = __Variant;
 
                         fn variant_names(&self, names: &mut dyn #spacetimedb_lib::de::ValidNames) {
-                            names.extend([#(#variant_names,)*])
+                            names.extend::<&[&str]>(&[#(#variant_names,)*])
                         }
 
                         fn visit_tag<E: #spacetimedb_lib::de::Error>(self, __tag: u8) -> Result<Self::Output, E> {
