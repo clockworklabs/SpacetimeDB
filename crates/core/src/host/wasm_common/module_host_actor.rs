36aaaa75b3a242ea4efb51c24b4372288a75482a
Noa <coolreader18@gmail.com>
2023-11-13 15:16:49 -0600

    Fix out of order subscription updates

--- crates/core/src/host/wasm_common/module_host_actor.rs
+++ crates/core/src/host/wasm_common/module_host_actor.rs
@@ -27,7 +27,7 @@ use crate::host::{
     ReducerId, ReducerOutcome, Timestamp,
 };
 use crate::identity::Identity;
-use crate::subscription::module_subscription_actor::{ModuleSubscriptionManager, SubscriptionEventSender};
+use crate::subscription::module_subscription_actor::ModuleSubscriptionManager;
 use crate::worker_metrics::WORKER_METRICS;
 
 use super::instrumentation::CallTimes;
@@ -81,7 +81,6 @@ pub(crate) struct WasmModuleHostActor<T: WasmModule> {
     module: T::InstancePre,
     initial_instance: Option<Box<WasmModuleInstance<T::Instance>>>,
     database_instance_context: Arc<DatabaseInstanceContext>,
-    event_tx: SubscriptionEventSender,
     scheduler: Scheduler,
     func_names: Arc<FuncNames>,
     info: Arc<ModuleInfo>,
@@ -151,7 +150,7 @@ impl<T: WasmModule> WasmModuleHostActor<T> {
 
         let owner_identity = database_instance_context.identity;
         let relational_db = database_instance_context.relational_db.clone();
-        let (subscription, event_tx) = ModuleSubscriptionManager::spawn(relational_db, owner_identity);
+        let subscription = ModuleSubscriptionManager::spawn(relational_db, owner_identity);
 
         let uninit_instance = module.instantiate_pre()?;
         let mut instance = uninit_instance.instantiate(
@@ -191,7 +190,6 @@ impl<T: WasmModule> WasmModuleHostActor<T> {
             initial_instance: None,
             func_names,
             info,
-            event_tx,
             database_instance_context,
             scheduler,
             energy_monitor,
@@ -207,7 +205,6 @@ impl<T: WasmModule> WasmModuleHostActor<T> {
         WasmModuleInstance {
             instance,
             info: self.info.clone(),
-            event_tx: self.event_tx.clone(),
             energy_monitor: self.energy_monitor.clone(),
             trapped: false,
         }
@@ -302,7 +299,6 @@ impl<T: WasmModule> Module for WasmModuleHostActor<T> {
 pub struct WasmModuleInstance<T: WasmInstance> {
     instance: T,
     info: Arc<ModuleInfo>,
-    event_tx: SubscriptionEventSender,
     energy_monitor: Arc<dyn EnergyMonitor>,
     trapped: bool,
 }
@@ -619,7 +615,7 @@ impl<T: WasmInstance> WasmModuleInstance<T> {
             energy_quanta_used: energy.used,
             host_execution_duration: timings.total_duration,
         };
-        self.event_tx.broadcast_event_blocking(client.as_ref(), event);
+        self.info.subscription.broadcast_event_blocking(client.as_ref(), event);
 
         ReducerCallResult {
             outcome,
