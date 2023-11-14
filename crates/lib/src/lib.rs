clockworklabs/SpacetimeDB

commit f12a238237a926e97ce621c65787050661067c1a

Moving object defs to SATS (#460)
* Moving object defs to SATS

* move ColumnIndexAttribute to primitives, nix path attr

* revert unnecessary reformating

* ProductValue: don't take ColId
ProductValues can be more than rows and can be nested

* simplify sats::db::def wrt. Constraints + impl ser/de

* cargo fmt

---------

Co-authored-by: Mazdak Farrokhzad <twingoow@gmail.com>

crates/lib/src/lib.rs
@@ -1,45 +1,36 @@
-use auth::StAccess;
-use auth::StTableType;
-use sats::impl_serialize;
-pub use spacetimedb_sats::buffer;
+use anyhow::Context;
+use spacetimedb_primitives::{ColId, ColumnIndexAttribute};
+use spacetimedb_sats::db::auth::{StAccess, StTableType};
+use spacetimedb_sats::db::def::{ColumnDef, IndexType, AUTO_TABLE_ID};
+use spacetimedb_sats::{impl_serialize, WithTypespace};
+use std::iter;
+
 pub mod address;
-pub mod data_key;
 pub mod filter;
-pub mod hex;
 pub mod identity;
-pub use spacetimedb_sats::de;
-pub mod error;
-pub mod hash;
 pub mod name;
 pub mod operator;
 pub mod primary_key;
-pub use spacetimedb_sats::ser;
 pub mod type_def {
     pub use spacetimedb_sats::{AlgebraicType, ProductType, ProductTypeElement, SumType};
 }
 pub mod type_value {
     pub use spacetimedb_sats::{AlgebraicValue, ProductValue};
 }
-pub mod auth;
+
+pub mod error;
 #[cfg(feature = "serde")]
 pub mod recovery;
-pub mod relation;
-pub mod table;
 #[cfg(feature = "cli")]
 pub mod util;
 pub mod version;
 
-pub use spacetimedb_sats::bsatn;
-
 pub use address::Address;
-pub use data_key::DataKey;
-pub use hash::Hash;
 pub use identity::Identity;
 pub use primary_key::PrimaryKey;
+pub use spacetimedb_sats::{self as sats, bsatn, buffer, de, ser};
 pub use type_def::*;
 pub use type_value::{AlgebraicValue, ProductValue};
-
-pub use spacetimedb_sats as sats;
 
 pub const MODULE_ABI_MAJOR_VERSION: u16 = 7;
 
@@ -91,8 +82,16 @@ impl std::fmt::Display for VersionTuple {
 
 extern crate self as spacetimedb_lib;
 
+#[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Ord, de::Deserialize, ser::Serialize)]
+pub struct IndexDef {
+    pub name: String,
+    pub index_type: IndexType,
+    pub cols: Vec<u8>,
+}
+
 //WARNING: Change this structure(or any of their members) is an ABI change.
 #[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Ord, de::Deserialize, ser::Serialize)]
+#[sats(crate = crate)]
 pub struct TableDef {
     pub name: String,
     /// data should always point to a ProductType in the typespace
@@ -101,6 +100,94 @@ pub struct TableDef {
     pub indexes: Vec<IndexDef>,
     pub table_type: StTableType,
     pub table_access: StAccess,
+}
+
+impl TableDef {
+    pub fn into_table_def(table: WithTypespace<'_, TableDef>) -> anyhow::Result<spacetimedb_sats::db::def::TableDef> {
+        let schema = table
+            .map(|t| &t.data)
+            .resolve_refs()
+            .context("recursive types not yet supported")?;
+        let schema = schema.into_product().ok().context("table not a product type?")?;
+        let table = table.ty();
+        anyhow::ensure!(
+            table.column_attrs.len() == schema.elements.len(),
+            "mismatched number of columns"
+        );
+
+        // Build single-column index definitions, determining `is_unique` from
+        // their respective column attributes.
+        let mut columns = Vec::with_capacity(schema.elements.len());
+        let mut indexes = Vec::new();
+        for (col_id, (ty, col_attr)) in std::iter::zip(&schema.elements, &table.column_attrs).enumerate() {
+            let col = ColumnDef {
+                col_name: ty.name.clone().context("column without name")?,
+                col_type: ty.algebraic_type.clone(),
+                is_autoinc: col_attr.has_autoinc(),
+            };
+
+            let index_for_column = table.indexes.iter().find(|index| {
+                // Ignore multi-column indexes
+                matches!(*index.cols, [index_col_id] if index_col_id as usize == col_id)
+            });
+
+            // If there's an index defined for this column already, use it,
+            // making sure that it is unique if the column has a unique constraint
+            let index_info = if let Some(index) = index_for_column {
+                Some((index.name.clone(), index.index_type))
+            } else if col_attr.has_unique() {
+                // If you didn't find an index, but the column is unique then create a unique btree index
+                // anyway.
+                Some((format!("{}_{}_unique", table.name, col.col_name), IndexType::BTree))
+            } else {
+                None
+            };
+            if let Some((name, ty)) = index_info {
+                match ty {
+                    IndexType::BTree => {}
+                    // TODO
+                    IndexType::Hash => anyhow::bail!("hash indexes not yet supported"),
+                }
+                indexes.push(spacetimedb_sats::db::def::IndexDef::new(
+                    name,
+                    AUTO_TABLE_ID,
+                    ColId(col_id as u32),
+                    col_attr.has_unique(),
+                ))
+            }
+            columns.push(col);
+        }
+
+        // Multi-column indexes cannot be unique (yet), so just add them.
+        let multi_col_indexes = table.indexes.iter().filter_map(|index| {
+            if let [a, b, rest @ ..] = &*index.cols {
+                let idx = spacetimedb_sats::db::def::IndexDef::new_cols(
+                    index.name.clone(),
+                    AUTO_TABLE_ID,
+                    false,
+                    (
+                        ColId::from(*a),
+                        iter::once(ColId::from(*b))
+                            .chain(rest.iter().copied().map(Into::into))
+                            .collect(),
+                    ),
+                );
+
+                Some(idx)
+            } else {
+                None
+            }
+        });
+        indexes.extend(multi_col_indexes);
+
+        Ok(spacetimedb_sats::db::def::TableDef {
+            table_name: table.name.clone(),
+            columns,
+            indexes,
+            table_type: table.table_type,
+            table_access: table.table_access,
+        })
+    }
 }
 
 #[derive(Debug, Clone, de::Deserialize, ser::Serialize)]
@@ -190,91 +277,4 @@ pub enum MiscModuleExport {
 pub struct TypeAlias {
     pub name: String,
     pub ty: sats::AlgebraicTypeRef,
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Ord, de::Deserialize, ser::Serialize)]
-pub struct IndexDef {
-    pub name: String,
-    pub index_type: IndexType,
-    pub cols: Vec<u8>,
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq, PartialOrd, Ord, de::Deserialize, ser::Serialize)]
-#[repr(u8)]
-pub enum IndexType {
-    BTree = 0,
-    Hash = 1,
-}
-
-impl From<IndexType> for u8 {
-    fn from(value: IndexType) -> Self {
-        value as u8
-    }
-}
-
-impl TryFrom<u8> for IndexType {
-    type Error = ();
-    fn try_from(v: u8) -> Result<Self, Self::Error> {
-        match v {
-            0 => Ok(IndexType::BTree),
-            1 => Ok(IndexType::Hash),
-            _ => Err(()),
-        }
-    }
-}
-
-// NOTE: Duplicated in `crates/bindings-macro/src/lib.rs`
-bitflags::bitflags! {
-    #[derive(Debug, Default, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-    pub struct ColumnIndexAttribute: u8 {
-        const UNSET = Self::empty().bits();
-        ///  Index no unique
-        const INDEXED = 0b0001;
-        /// Generate the next [Sequence]
-        const AUTO_INC = 0b0010;
-        /// Index unique
-        const UNIQUE = Self::INDEXED.bits() | 0b0100;
-        /// Unique + AutoInc
-        const IDENTITY = Self::UNIQUE.bits() | Self::AUTO_INC.bits();
-        /// Primary key column (implies Unique)
-        const PRIMARY_KEY = Self::UNIQUE.bits() | 0b1000;
-        /// PrimaryKey + AutoInc
-        const PRIMARY_KEY_AUTO = Self::PRIMARY_KEY.bits() | Self::AUTO_INC.bits();
-    }
-}
-
-impl ColumnIndexAttribute {
-    pub const fn has_unique(self) -> bool {
-        self.contains(Self::UNIQUE)
-    }
-    pub const fn has_indexed(self) -> bool {
-        self.contains(Self::INDEXED)
-    }
-    pub const fn has_autoinc(self) -> bool {
-        self.contains(Self::AUTO_INC)
-    }
-    pub const fn has_primary(self) -> bool {
-        self.contains(Self::PRIMARY_KEY)
-    }
-}
-
-impl TryFrom<u8> for ColumnIndexAttribute {
-    type Error = ();
-
-    fn try_from(v: u8) -> Result<Self, Self::Error> {
-        Self::from_bits(v).ok_or(())
-    }
-}
-
-impl<'de> de::Deserialize<'de> for ColumnIndexAttribute {
-    fn deserialize<D: de::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        Self::from_bits(deserializer.deserialize_u8()?)
-            .ok_or_else(|| de::Error::custom("invalid bitflags for ColumnIndexAttribute"))
-    }
-}
-
-impl ser::Serialize for ColumnIndexAttribute {
-    fn serialize<S: ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_u8(self.bits())
-    }
 }

crates/bindings-macro/src/lib.rs
@@ -14,10 +14,11 @@ extern crate proc_macro;
 use std::collections::HashMap;
 use std::time::Duration;
 
-use bitflags::{bitflags, Flags};
+use bitflags::Flags;
 use module::{derive_deserialize, derive_satstype, derive_serialize};
 use proc_macro2::{Span, TokenStream};
 use quote::{format_ident, quote, quote_spanned, TokenStreamExt};
+use spacetimedb_primitives::ColumnIndexAttribute;
 use syn::parse::{Parse, ParseStream};
 use syn::spanned::Spanned;
 use syn::{
@@ -302,7 +303,7 @@ fn gen_reducer(original_function: ItemFn, reducer_name: &str, extra: ReducerExtr
     // // TODO: better (non-string-based) validation for these
     // if !matches!(
     //     &*arg1.to_token_stream().to_string(),
-    //     "spacetimedb::spacetimedb_lib::hash::Hash" | "Hash"
+    //     "spacetimedb::spacetimedb_sats::hash::Hash" | "Hash"
     // ) {
     //     return Err(syn::Error::new_spanned(
     //         &arg1,
@@ -433,26 +434,6 @@ struct Column<'a> {
     index: u8,
     field: &'a module::SatsField<'a>,
     attr: ColumnIndexAttribute,
-}
-
-// TODO: any way to avoid duplication with same structure in bindings crate? Extra crate?
-bitflags! {
-    #[derive(Debug, Default, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-    struct ColumnIndexAttribute: u8 {
-        const UNSET = Self::empty().bits();
-        ///  Index no unique
-        const INDEXED = 0b0001;
-        /// Generate the next [Sequence]
-        const AUTO_INC = 0b0010;
-        /// Index unique
-        const UNIQUE = Self::INDEXED.bits() | 0b0100;
-        /// Unique + AutoInc
-        const IDENTITY = Self::UNIQUE.bits() | Self::AUTO_INC.bits();
-        /// Primary key column (implies Unique)
-        const PRIMARY_KEY = Self::UNIQUE.bits() | 0b1000;
-        /// PrimaryKey + AutoInc
-        const PRIMARY_KEY_AUTO = Self::PRIMARY_KEY.bits() | Self::AUTO_INC.bits();
-    }
 }
 
 fn spacetimedb_table(item: TokenStream) -> syn::Result<TokenStream> {
@@ -613,7 +594,7 @@ fn spacetimedb_tabletype_impl(item: syn::DeriveInput) -> syn::Result<TokenStream
         let name = name.as_deref().unwrap_or("default_index");
         indexes.push(quote!(spacetimedb::IndexDef {
             name: #name,
-            ty: spacetimedb::spacetimedb_lib::IndexType::#ty,
+            ty: spacetimedb::sats::db::def::IndexType::#ty,
             col_ids: &[#(#col_ids),*],
         }));
     }
@@ -725,8 +706,8 @@ fn spacetimedb_tabletype_impl(item: syn::DeriveInput) -> syn::Result<TokenStream
     let tabletype_impl = quote! {
         impl spacetimedb::TableType for #original_struct_ident {
             const TABLE_NAME: &'static str = #table_name;
-            const COLUMN_ATTRS: &'static [spacetimedb::spacetimedb_lib::ColumnIndexAttribute] = &[
-                #(spacetimedb::spacetimedb_lib::ColumnIndexAttribute::#column_attrs),*
+            const COLUMN_ATTRS: &'static [spacetimedb::sats::db::attr::ColumnIndexAttribute] = &[
+                #(spacetimedb::sats::db::attr::ColumnIndexAttribute::#column_attrs),*
             ];
             const INDEXES: &'static [spacetimedb::IndexDef<'static>] = &[#(#indexes),*];
             type InsertResult = #insert_result;

crates/bindings/src/lib.rs
@@ -10,32 +10,31 @@ pub mod rt;
 pub mod time_span;
 mod timestamp;
 
+use crate::sats::db::def::IndexType;
 use spacetimedb_lib::buffer::{BufReader, BufWriter, Cursor, DecodeError};
-pub use spacetimedb_lib::de::{Deserialize, DeserializeOwned};
 use spacetimedb_lib::sats::{impl_deserialize, impl_serialize, impl_st};
-pub use spacetimedb_lib::ser::Serialize;
-use spacetimedb_lib::{bsatn, ColumnIndexAttribute, IndexType, PrimaryKey, ProductType, ProductValue};
+use spacetimedb_lib::{bsatn, PrimaryKey, ProductType, ProductValue};
+use spacetimedb_primitives::ColumnIndexAttribute;
 use std::cell::RefCell;
 use std::marker::PhantomData;
 use std::slice::from_ref;
 use std::{fmt, panic};
+use sys::{Buffer, BufferIter};
 
-pub use spacetimedb_bindings_macro::{duration, query, spacetimedb, TableType};
-
+pub use log;
 pub use sats::SpacetimeType;
+pub use spacetimedb_bindings_macro::{duration, query, spacetimedb, TableType};
+pub use spacetimedb_bindings_sys as sys;
 pub use spacetimedb_lib;
+pub use spacetimedb_lib::de::{Deserialize, DeserializeOwned};
 pub use spacetimedb_lib::sats;
+pub use spacetimedb_lib::ser::Serialize;
 pub use spacetimedb_lib::Address;
 pub use spacetimedb_lib::AlgebraicValue;
 pub use spacetimedb_lib::Identity;
 pub use spacetimedb_primitives::TableId;
-pub use timestamp::Timestamp;
-
-pub use spacetimedb_bindings_sys as sys;
 pub use sys::Errno;
-use sys::{Buffer, BufferIter};
-
-pub use log;
+pub use timestamp::Timestamp;
 
 pub type Result<T = (), E = Errno> = core::result::Result<T, E>;
 

crates/core/src/lib.rs
@@ -25,8 +25,8 @@ pub mod db;
 pub mod messages;
 pub use spacetimedb_lib::Identity;
 pub mod error;
-pub use spacetimedb_lib::hash;
 pub use spacetimedb_lib::identity;
+pub use spacetimedb_sats::hash;
 pub mod protobuf {
     pub use spacetimedb_client_api_messages::*;
 }

crates/primitives/src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+mod attr;
 mod ids;
 
+pub use attr::{AttributeKind, ColumnIndexAttribute};
 pub use ids::{ColId, ConstraintId, IndexId, SequenceId, TableId};

crates/sats/src/lib.rs
@@ -7,13 +7,18 @@ pub mod bsatn;
 pub mod buffer;
 pub mod builtin_type;
 pub mod convert;
+pub mod data_key;
+pub mod db;
 pub mod de;
+pub mod hash;
+pub mod hex;
 pub mod map_type;
 pub mod map_value;
 pub mod meta_type;
 pub mod product_type;
 pub mod product_type_element;
 pub mod product_value;
+pub mod relation;
 mod resolve_refs;
 pub mod satn;
 pub mod ser;
@@ -28,6 +33,7 @@ pub use algebraic_value::{AlgebraicValue, F32, F64};
 pub use array_type::ArrayType;
 pub use array_value::ArrayValue;
 pub use builtin_type::BuiltinType;
+pub use data_key::DataKey;
 pub use map_type::MapType;
 pub use map_value::MapValue;
 pub use product_type::ProductType;

commit 6d2c388d4c0ce13031746c52e51f642a25927eb8

Fixes client API breaks introduced by #460

crates/lib/src/lib.rs
@@ -29,6 +29,9 @@ pub use address::Address;
 pub use identity::Identity;
 pub use primary_key::PrimaryKey;
 pub use spacetimedb_sats::{self as sats, bsatn, buffer, de, ser};
+pub use spacetimedb_sats::hash::{self, hash_bytes, Hash};
+pub use spacetimedb_sats::DataKey;
+pub use spacetimedb_sats::relation;
 pub use type_def::*;
 pub use type_value::{AlgebraicValue, ProductValue};
 