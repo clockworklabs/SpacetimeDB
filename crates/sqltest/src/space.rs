23fe9db0f878d6eb923328215da96de53ef7038b
Bilbo Metropolis <jameshgilles@gmail.com>
2023-11-21 16:35:34 -0500

    Missed fix

--- crates/sqltest/src/space.rs
+++ crates/sqltest/src/space.rs
@@ -83,7 +83,7 @@ impl SpaceDb {
     pub(crate) fn run_sql(&self, sql: &str) -> anyhow::Result<Vec<MemTable>> {
         self.conn.with_auto_commit(&ExecutionContext::default(), |tx| {
             let ast = compile_sql(&self.conn, tx, sql)?;
-            let result = execute_sql(&self.conn, tx, ast, &QueryDebugInfo::from_source(sql), self.auth)?;
+            let result = execute_sql(&self.conn, tx, ast, Some(&QueryDebugInfo::from_source(sql)), self.auth)?;
             //remove comments to see which SQL worked. Can't collect it outside from lack of a hook in the external `sqllogictest` crate... :(
             //append_file(&std::path::PathBuf::from(".ok.sql"), sql)?;
             Ok(result)
