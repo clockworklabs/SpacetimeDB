1c25f030d5a51bc129505ae4c0d489542859d788
Ingvar Stepanyan <me@rreverser.com>
2023-11-29 17:06:55 +0000

    Bunch of extra checks for better errors

--- crates/cli/src/tasks/csharp.rs
+++ crates/cli/src/tasks/csharp.rs
@@ -3,20 +3,50 @@ use duct::cmd;
 use std::fs;
 use std::path::{Path, PathBuf};
 
+fn parse_major_version(version: &str) -> Option<u8> {
+    version.split('.').next()?.parse::<u8>().ok()
+}
+
 pub(crate) fn build_csharp(project_path: &Path, build_debug: bool) -> anyhow::Result<PathBuf> {
-    // Make sure that we have the wasm target installed (ok to run if its already installed)
-    cmd!(
-        "dotnet",
-        "workload",
-        "install",
-        "wasi-experimental",
-        "--skip-manifest-update"
-    )
-    .run()?;
+    // Check if the `wasi-experimental` workload is installed. Unfortunately, we
+    // have to do this by inspecting the human-readable output. There is a
+    // hidden `--machine-readable` flag but it also mixes in human-readable
+    // output as well as unnecessarily updates various unrelated manifests.
+    match cmd!("dotnet", "workload", "list").read() {
+        Ok(workloads) if workloads.contains("wasi-experimental") => {}
+        Ok(_) => {
+            // If wasi-experimental is not found, first check if we're running
+            // on .NET 8.0. We can't even install that workload on older
+            // versions, so this helps to provide a nicer message than "Workload
+            // ID wasi-experimental is not recognized.".
+            let version = cmd!("dotnet", "--version").read().unwrap_or_default();
+            if parse_major_version(&version) < Some(8) {
+                anyhow::bail!(".NET 8.0 is required, but found {version}.");
+            }
+
+            // Finally, try to install the workload ourselves. On some systems
+            // this might require elevated privileges, so print a nice error
+            // message if it fails.
+            cmd!(
+                "dotnet",
+                "workload",
+                "install",
+                "wasi-experimental",
+                "--skip-manifest-update"
+            )
+            .run()
+            .context(concat!(
+                "Couldn't install the required wasi-experimental workload.\n",
+                "You might need to install it manually by running `dotnet workload install wasi-experimental` with privileged rights."
+            ))?;
+        }
+        Err(error) if error.kind() == std::io::ErrorKind::NotFound => {
+            anyhow::bail!("dotnet not found in PATH. Please install .NET 8.0.")
+        }
+        Err(error) => anyhow::bail!("{error}"),
+    };
 
     let config_name = if build_debug { "Debug" } else { "Release" };
-
-    let mut output_path = project_path.join(format!("bin/{config_name}/net8.0/wasi-wasm/AppBundle/StdbModule.wasm"));
 
     // Ensure the project path exists.
     fs::metadata(project_path).with_context(|| {
@@ -27,27 +57,18 @@ pub(crate) fn build_csharp(project_path: &Path, build_debug: bool) -> anyhow::Re
     })?;
 
     // run dotnet publish using cmd macro
-    let result = cmd!("dotnet", "publish", "-c", config_name).dir(project_path).run();
-    match result {
-        Ok(_) => {}
-        Err(error) => {
-            if error.kind() == std::io::ErrorKind::NotFound {
-                anyhow::bail!("Failed to build project. dotnet not found in path. Please install the .NET Core SDK.");
-            } else {
-                anyhow::bail!("Failed to build project. {}", error);
-            }
-        }
-    }
+    cmd!("dotnet", "publish", "-c", config_name).dir(project_path).run()?;
 
     // check if file exists
+    let mut output_path = project_path.join(format!("bin/{config_name}/net8.0/wasi-wasm/AppBundle/StdbModule.wasm"));
     if !output_path.exists() {
         // check for the old .NET 7 path for projects that haven't migrated yet
         output_path = project_path.join(format!("bin/{config_name}/net7.0/StdbModule.wasm"));
         if output_path.exists() {
-            eprintln!("Warning: Your project is using the old .NET 7.0 WebAssembly bindings.");
-            eprintln!(
-                "Those will be deprecated in the future. Please migrate your project to the new .NET 8.0 template."
-            );
+            anyhow::bail!(concat!(
+                "Looks like your project is using the deprecated .NET 7.0 WebAssembly bindings.\n",
+                "Please migrate your project to the new .NET 8.0 template."
+            ));
         } else {
             anyhow::bail!("Built project successfully but couldn't find the output file.");
         }
