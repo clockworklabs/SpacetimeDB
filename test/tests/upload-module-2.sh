31b20c16eb1ab96b7b680950ada2d22a9ca20a4d
Kim Altintop <kim@eagain.io>
2023-12-14 12:13:03 +0100

    test: Build modules in release mode (#672)
    Debug now creates wasm artifacts which are bigger than what we can
    support currently.

--- test/tests/upload-module-2.sh
+++ test/tests/upload-module-2.sh
@@ -23,7 +23,7 @@ pub fn my_repeating_reducer(prev: Timestamp) {
 }
 EOF
 
-run_test cargo run publish -S -d --project-path "$PROJECT_PATH" --clear-database
+run_test cargo run publish --skip_clippy --project-path "$PROJECT_PATH" --clear-database
 [ "1" == "$(grep -c "reated new database" "$TEST_OUT")" ]
 ADDRESS="$(grep "reated new database" "$TEST_OUT" | awk 'NF>1{print $NF}')"
 sleep 2
