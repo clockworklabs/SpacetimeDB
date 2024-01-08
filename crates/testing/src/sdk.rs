d320ab0130a061d4050749fce0e1e0cae5880070
Ingvar Stepanyan <me@rreverser.com>
2024-01-08 13:23:55 +0000

    Add memoization

--- crates/testing/src/sdk.rs
+++ crates/testing/src/sdk.rs
@@ -1,11 +1,13 @@
 use duct::cmd;
 use lazy_static::lazy_static;
 use rand::distributions::{Alphanumeric, DistString};
+use std::collections::HashMap;
+use std::fs::create_dir_all;
+use std::sync::Mutex;
 use std::thread::JoinHandle;
-use std::{collections::HashSet, fs::create_dir_all, sync::Mutex};
 
 use crate::invoke_cli;
-use crate::modules::module_path;
+use crate::modules::{CompilationMode, CompiledModule};
 use tempfile::TempDir;
 
 pub fn ensure_standalone_process() {
@@ -35,6 +37,7 @@ pub fn ensure_standalone_process() {
 
 pub struct Test {
     /// A human-readable name for this test.
+    #[allow(dead_code)] // TODO: should we just remove this now that it's unused?
     name: String,
 
     /// Must name a module in the SpacetimeDB/modules directory.
@@ -73,19 +76,21 @@ impl Test {
     pub fn builder() -> TestBuilder {
         TestBuilder::default()
     }
-    pub fn run(&self) {
+    pub fn run(self) {
         ensure_standalone_process();
+
+        let wasm_file = compile_module(&self.module_name);
 
         generate_bindings(
             &self.generate_language,
-            &self.module_name,
+            &wasm_file,
             &self.client_project,
             &self.generate_subdir,
         );
 
         compile_client(&self.compile_command, &self.client_project);
 
-        let db_name = publish_module(&self.module_name);
+        let db_name = publish_module(&wasm_file);
 
         run_client(&self.run_command, &self.client_project, &db_name);
     }
@@ -107,82 +112,96 @@ fn random_module_name() -> String {
     Alphanumeric.sample_string(&mut rand::thread_rng(), 16)
 }
 
-fn publish_module(module: &str) -> String {
-    static PUBLISHED_MODULES: Mutex<()> = Mutex::new(());
+macro_rules! memoized {
+    (|$key:ident: $key_ty:ty| -> $value_ty:ty $body:block) => {{
+        static MEMOIZED: Mutex<Option<HashMap<$key_ty, $value_ty>>> = Mutex::new(None);
 
-    let _lock = PUBLISHED_MODULES.lock().expect("PUBLISHED_MODULES Mutex is poisoned");
+        MEMOIZED
+            .lock()
+            .unwrap()
+            .get_or_insert_with(HashMap::new)
+            .entry($key)
+            .or_insert_with_key(|$key| -> $value_ty { $body })
+            .clone()
+    }};
+}
 
+// Note: this function is memoized to ensure we compile each module only once.
+// Without this lock, if multiple `Test`s ran concurrently in the same process,
+// the test harness would compile each module multiple times concurrently,
+// which is bad both for performance reasons as well as can lead to errors
+// with toolchains like .NET which don't expect parallel invocations
+// of their build tools on the same project folder.
+fn compile_module(module: &str) -> String {
+    let module = module.to_owned();
+
+    memoized!(|module: String| -> String {
+        let module = CompiledModule::compile(&module, CompilationMode::Debug);
+        module.path().to_str().unwrap().to_owned()
+    })
+}
+
+// Note: this function does not memoize because we want each test to publish the same
+// module as a separate clean database instance for isolation purposes.
+fn publish_module(wasm_file: &str) -> String {
     let name = random_module_name();
     invoke_cli(&[
         "publish",
         "--debug",
         "--project-path",
-        module_path(module).to_str().unwrap(),
+        wasm_file,
         "--skip_clippy",
         &name,
     ]);
     name
 }
 
-fn generate_bindings(language: &str, module_name: &str, client_project: &str, generate_subdir: &str) {
-    /// An exclusive lock which ensures we only run `spacetime generate` once for each target directory.
-    ///
-    /// Without this lock, if multiple `Test`s ran concurrently in the same process
-    /// with the same `client_project` and `generate_subdir`,
-    /// the test harness would run `spacetime generate` multiple times concurrently,
-    /// each of which would remove and re-populate the bindings directory,
-    /// potentially sweeping them out from under a compile or run process.
-    ///
-    /// This lock ensures that only one `spacetime generate` process runs at a time,
-    /// and the `HashSet` ensures that we run `spacetime generate` only once for each output directory.
-    ///
-    /// Circumstances where this will still break:
-    /// - If multiple tests want to use the same client_project/generate_subdir pair,
-    ///   but for different modules' bindings, only one module's bindings will ever be generated.
-    ///   If you need bindings for multiple different modules, put them in different subdirs.
-    /// - If multiple distinct test harness processes run concurrently,
-    ///   they will encounter the race condition described above,
-    ///   because the `BINDINGS_GENERATED` lock is not shared between harness processes.
-    ///   Running multiple test harness processes concurrently will break anyways
-    ///   because each will try to run `spacetime start` as a subprocess and will therefore
-    ///   contend over port 3000.
-    ///   Prefer constructing multiple `Test`s and `Test::run`ing them
-    ///   from within the same harness process.
-    //
-    // I (pgoldman 2023-09-11) considered, as an alternative to this lock,
-    // having `Test::run` copy the `client_project` into a fresh temporary directory.
-    // That would be more complicated, as we'd need to re-write dependencies
-    // on the client language's SpacetimeDB SDK to use a local absolute path.
-    // Doing so portably across all our SDK languages seemed infeasible.
-    static BINDINGS_GENERATED: Mutex<Option<HashSet<String>>> = Mutex::new(None);
+/// Note: this function is memoized to ensure we only run `spacetime generate` once for each target directory.
+///
+/// Without this lock, if multiple `Test`s ran concurrently in the same process
+/// with the same `client_project` and `generate_subdir`,
+/// the test harness would run `spacetime generate` multiple times concurrently,
+/// each of which would remove and re-populate the bindings directory,
+/// potentially sweeping them out from under a compile or run process.
+///
+/// This lock ensures that only one `spacetime generate` process runs at a time,
+/// and the `HashSet` ensures that we run `spacetime generate` only once for each output directory.
+///
+/// Circumstances where this will still break:
+/// - If multiple tests want to use the same client_project/generate_subdir pair,
+///   but for different modules' bindings, only one module's bindings will ever be generated.
+///   If you need bindings for multiple different modules, put them in different subdirs.
+/// - If multiple distinct test harness processes run concurrently,
+///   they will encounter the race condition described above,
+///   because the `BINDINGS_GENERATED` lock is not shared between harness processes.
+///   Running multiple test harness processes concurrently will break anyways
+///   because each will try to run `spacetime start` as a subprocess and will therefore
+///   contend over port 3000.
+///   Prefer constructing multiple `Test`s and `Test::run`ing them
+///   from within the same harness process.
+//
+// I (pgoldman 2023-09-11) considered, as an alternative to this lock,
+// having `Test::run` copy the `client_project` into a fresh temporary directory.
+// That would be more complicated, as we'd need to re-write dependencies
+// on the client language's SpacetimeDB SDK to use a local absolute path.
+// Doing so portably across all our SDK languages seemed infeasible.
+fn generate_bindings(language: &str, wasm_file: &str, client_project: &str, generate_subdir: &str) {
+    let generate_dir = format!("{client_project}/{generate_subdir}");
 
-    let mut bindings_lock = BINDINGS_GENERATED.lock().expect("BINDINGS_GENERATED Mutex is poisoned");
-
-    let generate_dir = format!("{}/{}", client_project, generate_subdir);
-
-    // If we've already generated bindings in this directory,
-    // return early.
-    // Otherwise, we'll hold the lock for the duration of the subprocess,
-    // so other tests will wait before overwriting our output.
-    if !bindings_lock
-        .get_or_insert_with(HashSet::new)
-        .insert(generate_dir.clone())
-    {
-        return;
-    }
-
-    create_dir_all(&generate_dir).expect("Error creating generate subdir");
-    invoke_cli(&[
-        "generate",
-        "--debug",
-        "--skip_clippy",
-        "--lang",
-        language,
-        "--project-path",
-        module_path(module_name).to_str().unwrap(),
-        "--out-dir",
-        &generate_dir,
-    ]);
+    memoized!(|generate_dir: String| -> () {
+        create_dir_all(&generate_dir).expect("Error creating generate subdir");
+        invoke_cli(&[
+            "generate",
+            "--debug",
+            "--skip_clippy",
+            "--lang",
+            language,
+            "--wasm-file",
+            wasm_file,
+            "--out-dir",
+            &generate_dir,
+        ]);
+    })
 }
 
 fn split_command_string(command: &str) -> (&str, Vec<&str>) {
@@ -192,19 +211,24 @@ fn split_command_string(command: &str) -> (&str, Vec<&str>) {
     (exe, args)
 }
 
+// Note: this function is memoized to ensure we only compile each client once.
 fn compile_client(compile_command: &str, client_project: &str) {
-    let (exe, args) = split_command_string(compile_command);
+    let client_project = client_project.to_owned();
 
-    let output = cmd(exe, args)
-        .dir(client_project)
-        .env(TEST_CLIENT_PROJECT_ENV_VAR, client_project)
-        .stderr_to_stdout()
-        .stdout_capture()
-        .unchecked()
-        .run()
-        .expect("Error running compile command");
+    memoized!(|client_project: String| -> () {
+        let (exe, args) = split_command_string(&compile_command);
 
-    status_ok_or_panic(output, compile_command, "(compiling)");
+        let output = cmd(exe, args)
+            .dir(&client_project)
+            .env(TEST_CLIENT_PROJECT_ENV_VAR, client_project)
+            .stderr_to_stdout()
+            .stdout_capture()
+            .unchecked()
+            .run()
+            .expect("Error running compile command");
+
+        status_ok_or_panic(output, &compile_command, "(compiling)");
+    })
 }
 
 fn run_client(run_command: &str, client_project: &str, db_name: &str) {
