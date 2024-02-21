26dbad88d7688d0b24b07a51ccfe2318bbefc19b
Mario Alejandro Montoya Cortés <mamcx@elmalabarista.com>
2024-02-21 11:42:13 -0500

    Add small 'realistic' workload benchmark integration test

---
+++ modules/benchmarks/src/circles.rs
@@ -0,0 +1,161 @@
+//! STDB module used for benchmarks based on "realistic" workloads we are focusing in improving.
+use crate::Load;
+use spacetimedb::{log, spacetimedb, SpacetimeType, Timestamp};
+use std::hint::black_box;
+
+#[derive(SpacetimeType, Debug, Clone, Copy)]
+pub struct Vector2 {
+    pub x: f32,
+    pub y: f32,
+}
+
+// ---------- schemas ----------
+
+#[spacetimedb(table)]
+pub struct Entity {
+    #[autoinc]
+    #[primarykey]
+    pub id: u32,
+    pub position: Vector2,
+    pub mass: u32,
+}
+
+impl Entity {
+    pub fn new(id: u32, x: f32, y: f32, mass: u32) -> Self {
+        Self {
+            id,
+            mass,
+            position: Vector2 { x, y },
+        }
+    }
+}
+
+#[spacetimedb(table)]
+#[spacetimedb(index(btree, name = "player_id_index", player_id))]
+pub struct Circle {
+    #[primarykey]
+    pub entity_id: u32,
+    pub player_id: u32,
+    pub direction: Vector2,
+    pub magnitude: f32,
+    pub last_split_time: Timestamp,
+}
+
+impl Circle {
+    pub fn new(entity_id: u32, player_id: u32, x: f32, y: f32, magnitude: f32) -> Self {
+        Self {
+            entity_id,
+            player_id,
+            direction: Vector2 { x, y },
+            magnitude,
+            last_split_time: Timestamp::now(),
+        }
+    }
+}
+
+#[spacetimedb(table)]
+pub struct Food {
+    #[primarykey]
+    pub entity_id: u32,
+}
+
+impl Food {
+    pub fn new(entity_id: u32) -> Self {
+        Self { entity_id }
+    }
+}
+
+fn mass_to_radius(mass: u32) -> f32 {
+    (mass as f32).sqrt()
+}
+
+fn is_overlapping(entity1: &Entity, entity2: &Entity) -> bool {
+    let entity1_radius = mass_to_radius(entity1.mass);
+    let entity2_radius = mass_to_radius(entity2.mass);
+    let distance =
+        ((entity1.position.x - entity2.position.x).powi(2) + (entity1.position.y - entity2.position.y).powi(2)).sqrt();
+    distance < entity1_radius.max(entity2_radius)
+}
+
+// ---------- insert bulk ----------
+#[spacetimedb(reducer)]
+pub fn insert_bulk_entity(count: u32) {
+    for id in 0..count {
+        Entity::insert(Entity::new(0, id as f32, (id + 5) as f32, id * 5)).unwrap();
+    }
+    log::info!("INSERT ENTITY: {count}");
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_bulk_circle(count: u32) {
+    for id in 0..count {
+        Circle::insert(Circle::new(id, id, id as f32, (id + 5) as f32, (id * 5) as f32)).unwrap();
+    }
+    log::info!("INSERT CIRCLE: {count}");
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_bulk_food(count: u32) {
+    for id in 1..=count {
+        Food::insert(Food::new(id)).unwrap();
+    }
+    log::info!("INSERT FOOD: {count}");
+}
+
+// Simulate
+// ```
+// SELECT * FROM Circle, Entity, Food
+// ```
+#[spacetimedb(reducer)]
+pub fn cross_join_all(expected: u32) {
+    let mut count = 0;
+    for _circle in Circle::iter() {
+        for _entity in Entity::iter() {
+            for _food in Food::iter() {
+                count += 1;
+            }
+        }
+    }
+
+    log::info!("CROSS JOIN ALL: {expected}, processed: {count}");
+}
+
+// Simulate
+// ```
+// SELECT * FROM Circle JOIN ENTITY USING(entity_id), Food JOIN ENTITY USING(entity_id)
+// ```
+#[spacetimedb(reducer)]
+pub fn cross_join_circle_food(expected: u32) {
+    let mut count = 0;
+    for circle in Circle::iter() {
+        let Some(circle_entity) = Entity::filter_by_id(&circle.entity_id) else {
+            continue;
+        };
+
+        for food in Food::iter() {
+            count += 1;
+            let food_entity = Entity::filter_by_id(&food.entity_id)
+                .unwrap_or_else(|| panic!("Entity not found: {})", food.entity_id));
+            black_box(is_overlapping(&circle_entity, &food_entity));
+        }
+    }
+
+    log::info!("CROSS JOIN CIRCLE FOOD: {expected}, processed: {count}");
+}
+
+#[spacetimedb(reducer)]
+pub fn init_game_circles(initial_load: u32) {
+    let load = Load::new(initial_load);
+
+    insert_bulk_food(load.initial_load);
+    insert_bulk_entity(load.initial_load);
+    insert_bulk_circle(load.small_table);
+}
+
+#[spacetimedb(reducer)]
+pub fn run_game_circles(initial_load: u32) {
+    let load = Load::new(initial_load);
+
+    cross_join_circle_food(initial_load * load.small_table);
+    cross_join_all(initial_load * initial_load * load.small_table);
+}
---
+++ modules/benchmarks/src/synthetic.rs
@@ -0,0 +1,448 @@
+//! STDB module used for benchmarks.
+//!
+//! This file is tightly bound to the `benchmarks` crate (`crates/bench`).
+//!
+//! The various tables in this file need to remain synced with `crates/bench/src/schemas.rs`
+//! Field orders, names, and types should be the same.
+//!
+//! We instantiate multiple copies of each table. These should be identical
+//! aside from indexing strategy. Table names must match the template:
+//!
+//! `{IndexStrategy}{TableName}`, in PascalCase.
+//!
+//! The reducers need to remain synced with `crates/bench/src/spacetime_module.rs`
+//! Reducer names must match the template:
+//!
+//! `{operation}_{index_strategy}_{table_name}`, in snake_case.
+//!
+//! The three index strategies are:
+//! - `unique`: a single unique key, declared first in the struct.
+//! - `no_index`: no indexes.
+//! - `btree_each_column`: one index for each column.
+//!
+//! Obviously more could be added...
+
+use spacetimedb::{println, spacetimedb};
+use std::hint::black_box;
+
+// ---------- schemas ----------
+
+#[spacetimedb(table)]
+pub struct unique_0_u32_u64_str {
+    #[unique]
+    id: u32,
+    age: u64,
+    name: String,
+}
+
+#[spacetimedb(table)]
+pub struct no_index_u32_u64_str {
+    id: u32,
+    age: u64,
+    name: String,
+}
+
+#[spacetimedb(table)]
+#[spacetimedb(index(btree, name = "id", id))]
+#[spacetimedb(index(btree, name = "name", name))]
+#[spacetimedb(index(btree, name = "age", age))]
+pub struct btree_each_column_u32_u64_str {
+    id: u32,
+    age: u64,
+    name: String,
+}
+
+#[spacetimedb(table)]
+pub struct unique_0_u32_u64_u64 {
+    #[unique]
+    id: u32,
+    x: u64,
+    y: u64,
+}
+
+#[spacetimedb(table)]
+pub struct no_index_u32_u64_u64 {
+    id: u32,
+    x: u64,
+    y: u64,
+}
+
+#[spacetimedb(table)]
+#[spacetimedb(index(btree, name = "id", id))]
+#[spacetimedb(index(btree, name = "x", x))]
+#[spacetimedb(index(btree, name = "y", y))]
+pub struct btree_each_column_u32_u64_u64 {
+    id: u32,
+    x: u64,
+    y: u64,
+}
+
+// ---------- empty ----------
+
+#[spacetimedb(reducer)]
+pub fn empty() {}
+
+// ---------- insert ----------
+#[spacetimedb(reducer)]
+pub fn insert_unique_0_u32_u64_str(id: u32, age: u64, name: String) {
+    unique_0_u32_u64_str::insert(unique_0_u32_u64_str { id, name, age }).unwrap();
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_no_index_u32_u64_str(id: u32, age: u64, name: String) {
+    no_index_u32_u64_str::insert(no_index_u32_u64_str { id, name, age });
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_btree_each_column_u32_u64_str(id: u32, age: u64, name: String) {
+    btree_each_column_u32_u64_str::insert(btree_each_column_u32_u64_str { id, name, age });
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_unique_0_u32_u64_u64(id: u32, x: u64, y: u64) {
+    unique_0_u32_u64_u64::insert(unique_0_u32_u64_u64 { id, x, y }).unwrap();
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_no_index_u32_u64_u64(id: u32, x: u64, y: u64) {
+    no_index_u32_u64_u64::insert(no_index_u32_u64_u64 { id, x, y });
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_btree_each_column_u32_u64_u64(id: u32, x: u64, y: u64) {
+    btree_each_column_u32_u64_u64::insert(btree_each_column_u32_u64_u64 { id, x, y });
+}
+
+// ---------- insert bulk ----------
+
+#[spacetimedb(reducer)]
+pub fn insert_bulk_unique_0_u32_u64_u64(locs: Vec<unique_0_u32_u64_u64>) {
+    for loc in locs {
+        unique_0_u32_u64_u64::insert(loc).unwrap();
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_bulk_no_index_u32_u64_u64(locs: Vec<no_index_u32_u64_u64>) {
+    for loc in locs {
+        no_index_u32_u64_u64::insert(loc);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_bulk_btree_each_column_u32_u64_u64(locs: Vec<btree_each_column_u32_u64_u64>) {
+    for loc in locs {
+        btree_each_column_u32_u64_u64::insert(loc);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_bulk_unique_0_u32_u64_str(people: Vec<unique_0_u32_u64_str>) {
+    for u32_u64_str in people {
+        unique_0_u32_u64_str::insert(u32_u64_str).unwrap();
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_bulk_no_index_u32_u64_str(people: Vec<no_index_u32_u64_str>) {
+    for u32_u64_str in people {
+        no_index_u32_u64_str::insert(u32_u64_str);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn insert_bulk_btree_each_column_u32_u64_str(people: Vec<btree_each_column_u32_u64_str>) {
+    for u32_u64_str in people {
+        btree_each_column_u32_u64_str::insert(u32_u64_str);
+    }
+}
+
+// ---------- update ----------
+
+#[spacetimedb(reducer)]
+pub fn update_bulk_unique_0_u32_u64_u64(row_count: u32) {
+    let mut hit: u32 = 0;
+    for loc in unique_0_u32_u64_u64::iter().take(row_count as usize) {
+        hit += 1;
+        assert!(
+            unique_0_u32_u64_u64::update_by_id(
+                &loc.id,
+                unique_0_u32_u64_u64 {
+                    id: loc.id,
+                    x: loc.x.wrapping_add(1),
+                    y: loc.y,
+                },
+            ),
+            "failed to update u32_u64_u64"
+        );
+    }
+    assert_eq!(hit, row_count, "not enough rows to perform requested amount of updates");
+}
+
+#[spacetimedb(reducer)]
+pub fn update_bulk_unique_0_u32_u64_str(row_count: u32) {
+    let mut hit: u32 = 0;
+    for u32_u64_str in unique_0_u32_u64_str::iter().take(row_count as usize) {
+        hit += 1;
+        assert!(
+            unique_0_u32_u64_str::update_by_id(
+                &u32_u64_str.id,
+                unique_0_u32_u64_str {
+                    id: u32_u64_str.id,
+                    name: u32_u64_str.name,
+                    age: u32_u64_str.age.wrapping_add(1),
+                },
+            ),
+            "failed to update u32_u64_str"
+        );
+    }
+    assert_eq!(hit, row_count, "not enough rows to perform requested amount of updates");
+}
+
+// ---------- iterate ----------
+
+#[spacetimedb(reducer)]
+pub fn iterate_unique_0_u32_u64_str() {
+    for u32_u64_str in unique_0_u32_u64_str::iter() {
+        black_box(u32_u64_str);
+    }
+}
+#[spacetimedb(reducer)]
+pub fn iterate_unique_0_u32_u64_u64() {
+    for u32_u64_u64 in unique_0_u32_u64_u64::iter() {
+        black_box(u32_u64_u64);
+    }
+}
+
+// ---------- filtering ----------
+
+#[spacetimedb(reducer)]
+pub fn filter_unique_0_u32_u64_str_by_id(id: u32) {
+    if let Some(p) = unique_0_u32_u64_str::filter_by_id(&id) {
+        black_box(p);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_no_index_u32_u64_str_by_id(id: u32) {
+    for p in no_index_u32_u64_str::filter_by_id(&id) {
+        black_box(p);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_btree_each_column_u32_u64_str_by_id(id: u32) {
+    for p in btree_each_column_u32_u64_str::filter_by_id(&id) {
+        black_box(p);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_unique_0_u32_u64_str_by_name(name: String) {
+    for p in unique_0_u32_u64_str::filter_by_name(&name) {
+        black_box(p);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_no_index_u32_u64_str_by_name(name: String) {
+    for p in no_index_u32_u64_str::filter_by_name(&name) {
+        black_box(p);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_btree_each_column_u32_u64_str_by_name(name: String) {
+    for p in btree_each_column_u32_u64_str::filter_by_name(&name) {
+        black_box(p);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_unique_0_u32_u64_u64_by_id(id: u32) {
+    if let Some(loc) = unique_0_u32_u64_u64::filter_by_id(&id) {
+        black_box(loc);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_no_index_u32_u64_u64_by_id(id: u32) {
+    for loc in no_index_u32_u64_u64::filter_by_id(&id) {
+        black_box(loc);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_btree_each_column_u32_u64_u64_by_id(id: u32) {
+    for loc in btree_each_column_u32_u64_u64::filter_by_id(&id) {
+        black_box(loc);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_unique_0_u32_u64_u64_by_x(x: u64) {
+    for loc in unique_0_u32_u64_u64::filter_by_x(&x) {
+        black_box(loc);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_no_index_u32_u64_u64_by_x(x: u64) {
+    for loc in no_index_u32_u64_u64::filter_by_x(&x) {
+        black_box(loc);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_btree_each_column_u32_u64_u64_by_x(x: u64) {
+    for loc in btree_each_column_u32_u64_u64::filter_by_x(&x) {
+        black_box(loc);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_unique_0_u32_u64_u64_by_y(x: u64) {
+    for loc in unique_0_u32_u64_u64::filter_by_y(&x) {
+        black_box(loc);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_no_index_u32_u64_u64_by_y(x: u64) {
+    for loc in no_index_u32_u64_u64::filter_by_y(&x) {
+        black_box(loc);
+    }
+}
+
+#[spacetimedb(reducer)]
+pub fn filter_btree_each_column_u32_u64_u64_by_y(x: u64) {
+    for loc in btree_each_column_u32_u64_u64::filter_by_y(&x) {
+        black_box(loc);
+    }
+}
+
+// ---------- delete ----------
+
+// FIXME: current nonunique delete interface is UNUSABLE!!!!
+
+#[spacetimedb(reducer)]
+pub fn delete_unique_0_u32_u64_str_by_id(id: u32) {
+    unique_0_u32_u64_str::delete_by_id(&id);
+}
+
+#[spacetimedb(reducer)]
+pub fn delete_unique_0_u32_u64_u64_by_id(id: u32) {
+    unique_0_u32_u64_u64::delete_by_id(&id);
+}
+
+// ---------- clear table ----------
+#[spacetimedb(reducer)]
+pub fn clear_table_unique_0_u32_u64_str() {
+    unimplemented!("Modules currently have no interface to clear a table");
+}
+
+#[spacetimedb(reducer)]
+pub fn clear_table_no_index_u32_u64_str() {
+    unimplemented!("Modules currently have no interface to clear a table");
+}
+
+#[spacetimedb(reducer)]
+pub fn clear_table_btree_each_column_u32_u64_str() {
+    unimplemented!("Modules currently have no interface to clear a table");
+}
+
+#[spacetimedb(reducer)]
+pub fn clear_table_unique_0_u32_u64_u64() {
+    unimplemented!("Modules currently have no interface to clear a table");
+}
+
+#[spacetimedb(reducer)]
+pub fn clear_table_no_index_u32_u64_u64() {
+    unimplemented!("Modules currently have no interface to clear a table");
+}
+
+#[spacetimedb(reducer)]
+pub fn clear_table_btree_each_column_u32_u64_u64() {
+    unimplemented!("Modules currently have no interface to clear a table");
+}
+// ---------- count ----------
+
+// You need to inspect the module outputs to actually read the result from these.
+
+#[spacetimedb(reducer)]
+pub fn count_unique_0_u32_u64_str() {
+    println!("COUNT: {}", unique_0_u32_u64_str::iter().count());
+}
+
+#[spacetimedb(reducer)]
+pub fn count_no_index_u32_u64_str() {
+    println!("COUNT: {}", no_index_u32_u64_str::iter().count());
+}
+
+#[spacetimedb(reducer)]
+pub fn count_btree_each_column_u32_u64_str() {
+    println!("COUNT: {}", btree_each_column_u32_u64_str::iter().count());
+}
+
+#[spacetimedb(reducer)]
+pub fn count_unique_0_u32_u64_u64() {
+    println!("COUNT: {}", unique_0_u32_u64_u64::iter().count());
+}
+
+#[spacetimedb(reducer)]
+pub fn count_no_index_u32_u64_u64() {
+    println!("COUNT: {}", no_index_u32_u64_u64::iter().count());
+}
+
+#[spacetimedb(reducer)]
+pub fn count_btree_each_column_u32_u64_u64() {
+    println!("COUNT: {}", btree_each_column_u32_u64_u64::iter().count());
+}
+// ---------- module-specific stuff ----------
+
+#[spacetimedb(reducer)]
+pub fn fn_with_1_args(_arg: String) {}
+
+#[spacetimedb(reducer)]
+pub fn fn_with_32_args(
+    _arg1: String,
+    _arg2: String,
+    _arg3: String,
+    _arg4: String,
+    _arg5: String,
+    _arg6: String,
+    _arg7: String,
+    _arg8: String,
+    _arg9: String,
+    _arg10: String,
+    _arg11: String,
+    _arg12: String,
+    _arg13: String,
+    _arg14: String,
+    _arg15: String,
+    _arg16: String,
+    _arg17: String,
+    _arg18: String,
+    _arg19: String,
+    _arg20: String,
+    _arg21: String,
+    _arg22: String,
+    _arg23: String,
+    _arg24: String,
+    _arg25: String,
+    _arg26: String,
+    _arg27: String,
+    _arg28: String,
+    _arg29: String,
+    _arg30: String,
+    _arg31: String,
+    _arg32: String,
+) {
+}
+
+#[spacetimedb(reducer)]
+pub fn print_many_things(n: u32) {
+    for _ in 0..n {
+        println!("hello again!");
+    }
+}
