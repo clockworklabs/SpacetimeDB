b415fa651f01a8410b17c2b011a284069706c8d5
Boppy <no-reply@boppygames.gg>
2024-02-22 15:17:34 -0600

    Non-controversial script improvements

--- tools/perf.sh
+++ tools/perf.sh
@@ -1,10 +1,53 @@
 #!/bin/env bash
 
-# Before running, download d3-flamegraph-base.html
-# from https://cdn.jsdelivr.net/npm/d3-flame-graph@4.1.3/dist/templates/d3-flamegraph-base.html
-# and put it at /usr/share/d3-flame-graph/d3-flamegraph-base.html
+set -euo pipefail
 
-set -e
+# Function to print help information
+print_help() {
+    cat <<EOF
+Run perf against an existing SpacetimeDB process, and produce a flamegraph.
+
+Options:
+-o FILE, --output FILE   Filename to save flamegraph.             Default: flamegraph.html
+-d FILE, --output FILE   Filename to store perf data.             Default: perf.data
+-p PID,  --pid PID       Process ID of the SpacetimeDB process.   Default: determined by ps
+-t SEC,  --time SEC      Duration to sample for.                  Default: 10
+-h,      --help          Print this message, then exit.
+EOF
+}
+
+# Check if no arguments were passed
+if [ $# -eq 0 ]; then
+    print_help
+    exit 0
+fi
+
+if ! [ -f /usr/share/d3-flame-graph/d3-flamegraph-base.html ] ; then
+    read -p "Could not find d3-flamegraph-base.html, should we download it? (y/n): " answer
+
+    case $answer in
+        [Yy]* )
+            url="https://cdn.jsdelivr.net/npm/d3-flame-graph@4.1.3/dist/templates/d3-flamegraph-base.html"
+            target_dir="/usr/share/d3-flame-graph"
+            target_file="${target_dir}/d3-flamegraph-base.html"
+            sudo mkdir -p "$target_dir"
+            echo "Downloading d3-flamegraph-base.html to ${target_file}..."
+            sudo curl -o "$target_file" "$url"
+
+            if [ $? -eq 0 ]; then
+                echo "Download complete."
+            else
+                echo "Download failed. Please check your internet connection and the URL."
+            fi
+            ;;
+        [Nn]* )
+            echo "Download skipped."
+            ;;
+        * )
+            echo "Invalid response. Please answer y or n."
+            ;;
+    esac
+fi
 
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -29,16 +72,7 @@ while [[ $# -gt 0 ]]; do
             shift
             ;;
         -h|--help)
-            >&2 echo <<EOF
-Run perf against an existing SpacetimeDB process, and produce a flamegraph.
-
-Options:
--o FILE, --output FILE   Filename to save flamegraph.             Default: flamegraph.html
--d FILE, --output FILE   Filename to store perf data.             Default: perf.data
--p PID,  --pid PID       Process ID of the SpacetimeDB process.   Default: determined by ps
--t SEC,  --time SEC      Duration to sample for.                  Default: 10
--h,      --help          Print this message, then exit.
-EOF
+            print_help
             exit 0
             ;;
         *)
@@ -48,6 +82,8 @@ EOF
     esac
 done
 
+set +u
+
 if [[ -z "$OUTFILE" ]]; then
     OUTFILE=flamegraph.html
 fi
@@ -55,6 +91,11 @@ if [[ -z "$DATAFILE" ]]; then
     DATAFILE=perf.data
 fi
 if [[ -z "$SPACETIME_PID" ]]; then
+    if [[ $(ps -a | grep spacetime | wc -l) != 1 ]] ; then
+        echo "spacetime PID not found, is it running?"
+	exit 1
+    fi
+
     SPACETIME_PID=$(ps -a | grep spacetime | awk '{print $1}')
 fi
 if ! [[ $SPACETIME_PID =~ ^[0-9]+$ ]]; then
@@ -64,8 +105,10 @@ fi
 if [[ -z "$SLEEP_TIME" ]]; then
     SLEEP_TIME=10
 fi
+
 echo "Instrumenting PID $SPACETIME_PID for $SLEEP_TIME seconds."
 echo "Writing data to $DATAFILE and flamegraph to $OUTFILE."
 
 perf record -m 8192 --call-graph lbr --pid $SPACETIME_PID -o $DATAFILE sleep $SLEEP_TIME
 perf script report flamegraph -i $DATAFILE -o $OUTFILE
+
