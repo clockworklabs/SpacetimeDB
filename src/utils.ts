bc53b9d345c5859c24cef736719f7d00b7e842d0
Nathaniel Richards <46858886+NateTheDev1@users.noreply.github.com>
2024-03-12 10:23:58 -0400

    Cleaned up code and added better error handling and responses for invalid tables (#32)
    * Cleaned up code and added better error handling and responses for invalid tables
    
    * Fixed breaking test
    
    * Implemented new logging system
    
    * made a small change to a WS error log

--- src/utils.ts
+++ src/utils.ts
@@ -18,3 +18,11 @@ export function _tableProxy<T>(t: any, client: SpacetimeDBClient): T {
     },
   }) as unknown as typeof t;
 }
+
+export function toPascalCase(s: string): string {
+  const str = s.replace(/([-_][a-z])/gi, ($1) => {
+    return $1.toUpperCase().replace("-", "").replace("_", "");
+  });
+
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
