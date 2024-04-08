98363888ffab0f7edce61a8697f0e760d407ad38
Mazdak Farrokhzad <twingoow@gmail.com>
2024-04-09 01:48:41 +0200

    `IndexSemijoin::next`: do a bit less cloning and work (#1044)
    * IndexSemijoin::next: do a bit less cloning and work
    
    * address Joshua's review

--- crates/core/src/vm.rs
+++ crates/core/src/vm.rs
@@ -10,7 +10,7 @@ use spacetimedb_lib::identity::AuthCtx;
 use spacetimedb_lib::relation::RowCount;
 use spacetimedb_primitives::*;
 use spacetimedb_sats::db::def::TableDef;
-use spacetimedb_sats::relation::{DbTable, FieldExpr, FieldExprRef, FieldName, Header, Relation};
+use spacetimedb_sats::relation::{DbTable, FieldExpr, FieldExprRef, Header, Relation};
 use spacetimedb_sats::{AlgebraicValue, ProductValue};
 use spacetimedb_vm::errors::ErrorVm;
 use spacetimedb_vm::eval::IterRows;
@@ -123,12 +123,16 @@ pub fn build_query<'a>(
                 let index_table = index_side.table_id().unwrap();
                 let index_header = index_side.head();
                 let probe_side = build_query(ctx, stdb, tx, probe_side, sources)?;
+                let probe_col = probe_side
+                    .head()
+                    .column_pos(probe_field)
+                    .expect("query compiler should have ensured the column exist");
                 Box::new(IndexSemiJoin {
                     ctx,
                     db: stdb,
                     tx,
                     probe_side,
-                    probe_field,
+                    probe_col,
                     index_header,
                     index_select,
                     index_table,
@@ -291,8 +295,8 @@ pub struct IndexSemiJoin<'a, 'c, Rhs: RelOps<'a>> {
     /// An iterator for the probe side.
     /// The values returned will be used to probe the index.
     pub probe_side: Rhs,
-    /// The field whose value will be used to probe the index.
-    pub probe_field: &'c FieldName,
+    /// The column whose value will be used to probe the index.
+    pub probe_col: ColId,
     /// The header for the index side of the join.
     pub index_header: &'c Arc<Header>,
     /// An optional predicate to evaluate over the matching rows of the index.
@@ -301,7 +305,7 @@ pub struct IndexSemiJoin<'a, 'c, Rhs: RelOps<'a>> {
     pub index_table: TableId,
     /// The column id for which the index is defined.
     pub index_col: ColId,
-    /// Is this a left or right semijion?
+    /// Is this a left or right semijoin?
     pub return_index_rows: bool,
     /// An iterator for the index side.
     /// A new iterator will be instantiated for each row on the probe side.
@@ -354,24 +358,19 @@ impl<'a, Rhs: RelOps<'a>> RelOps<'a> for IndexSemiJoin<'a, '_, Rhs> {
         }
 
         // Otherwise probe the index with a row from the probe side.
-        while let Some(row) = self.probe_side.next()? {
-            if let Some(pos) = self.probe_side.head().column_pos(self.probe_field) {
-                if let Some(value) = row.read_column(pos.idx()) {
-                    let table_id = self.index_table;
-                    let col_id = self.index_col;
-
-                    let value = value.into_owned();
-
-                    let mut index_iter = match self.tx {
-                        TxMode::MutTx(tx) => self.db.iter_by_col_range_mut(self.ctx, tx, table_id, col_id, value)?,
-                        TxMode::Tx(tx) => self.db.iter_by_col_range(self.ctx, tx, table_id, col_id, value)?,
-                    };
-                    while let Some(value) = index_iter.next() {
-                        let value = RelValue::Row(value);
-                        if self.filter(&value)? {
-                            self.index_iter = Some(index_iter);
-                            return Ok(Some(self.map(value, Some(row))));
-                        }
+        let table_id = self.index_table;
+        let col_id = self.index_col;
+        while let Some(mut row) = self.probe_side.next()? {
+            if let Some(value) = row.read_or_take_column(self.probe_col.idx()) {
+                let mut index_iter = match self.tx {
+                    TxMode::MutTx(tx) => self.db.iter_by_col_range_mut(self.ctx, tx, table_id, col_id, value)?,
+                    TxMode::Tx(tx) => self.db.iter_by_col_range(self.ctx, tx, table_id, col_id, value)?,
+                };
+                while let Some(value) = index_iter.next() {
+                    let value = RelValue::Row(value);
+                    if self.filter(&value)? {
+                        self.index_iter = Some(index_iter);
+                        return Ok(Some(self.map(value, Some(row))));
                     }
                 }
             }
