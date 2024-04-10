3b8f3ef53ab837fc527c43f2c688d683d86535be
Michael Ney <ney.quentin.michael@gmail.com>
2024-04-09 21:37:44 -0700

    Make list command also show the db_name associated with the db_address. Gets db_name from dns lookup for db_address

--- crates/cli/src/subcommands/list.rs
+++ crates/cli/src/subcommands/list.rs
@@ -1,9 +1,14 @@
-use crate::Config;
+use crate::{config::IdentityConfig, Config};
 use anyhow::Context;
 use clap::{Arg, ArgMatches, Command};
+use futures::future::join_all;
 use reqwest::StatusCode;
 use serde::Deserialize;
-use spacetimedb_lib::Address;
+
+use spacetimedb_lib::{
+    name::{DomainName, ReverseDNSResponse},
+    Address,
+};
 use tabled::{
     settings::{object::Columns, Alignment, Modify, Style},
     Table, Tabled,
@@ -33,6 +38,12 @@ struct DatabasesResult {
 #[derive(Tabled, Deserialize)]
 #[serde(transparent)]
 struct AddressRow {
+    pub db_address: Address,
+}
+
+#[derive(Tabled, Deserialize)]
+struct DatabaseRow {
+    pub db_name: DomainName,
     pub db_address: Address,
 }
 
@@ -69,15 +80,81 @@ pub async fn exec(config: Config, args: &ArgMatches) -> Result<(), anyhow::Error
 
     let identity = identity_config.nick_or_identity();
     if !result.addresses.is_empty() {
-        let mut table = Table::new(result.addresses);
-        table
-            .with(Style::psql())
-            .with(Modify::new(Columns::first()).with(Alignment::left()));
-        println!("Associated database addresses for {}:\n", identity);
-        println!("{}", table);
+        let databases_dns =
+            get_dns_for_database_addresses(config.clone(), server, identity_config, &result.addresses).await;
+
+        let combined_dns_address_rows: Vec<DatabaseRow> = result
+            .addresses
+            .iter()
+            .enumerate()
+            .map(|(index, address)| DatabaseRow {
+                db_name: databases_dns.as_ref().unwrap().get(index).unwrap().clone(),
+                db_address: address.db_address,
+            })
+            .collect();
+
+        if !combined_dns_address_rows.is_empty() {
+            let mut table = Table::new(combined_dns_address_rows);
+            table
+                .with(Style::psql())
+                .with(Modify::new(Columns::first()).with(Alignment::left()));
+            println!("Associated databases for {}:\n", identity);
+            println!("{}", table);
+        } else {
+            println!("No databases found for {}.", identity);
+        }
+
+        Ok(())
     } else {
-        println!("No databases found for {}.", identity);
+        return Err(anyhow::anyhow!(format!(
+            "Unable to retrieve databases for identity: {}",
+            identity
+        )));
+    }
+}
+
+async fn get_dns_for_database_addresses(
+    config: Config,
+    server: Option<&str>,
+    identity_config: &IdentityConfig,
+    addresses: &Vec<AddressRow>,
+) -> Result<Vec<DomainName>, anyhow::Error> {
+    let client = reqwest::Client::new();
+    let mut database_names: Vec<DomainName> = vec![];
+    let futures = addresses.iter().map(|address| async {
+        let res = client
+            .get(format!(
+                "{}/database/reverse_dns/{}",
+                config.get_host_url(server)?,
+                address.db_address
+            ))
+            .basic_auth("token", Some(&identity_config.token))
+            .send()
+            .await?;
+
+        if res.status() != StatusCode::OK {
+            return Err(anyhow::anyhow!(format!(
+                "Unable to retrieve database name for database {}: {}",
+                address.db_address,
+                res.status()
+            )));
+        }
+
+        let result: ReverseDNSResponse = res.json().await?;
+        if result.names.get(0).is_some() {
+            return Ok(result.names.get(0).unwrap().clone());
+        } else {
+            return Err(anyhow::anyhow!(format!(
+                "Unable to retrieve database name for database {}",
+                address.db_address,
+            )));
+        }
+    });
+
+    let result = join_all(futures).await;
+    for domain in result {
+        database_names.push(domain.unwrap());
     }
 
-    Ok(())
+    return Ok(database_names);
 }
