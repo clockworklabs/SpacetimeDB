7472667c711d8cfaaa50fc97e6b4f543f806f272
Mazdak Farrokhzad <twingoow@gmail.com>
2024-05-24 17:31:50 +0200

    specialize JoinInner map (index) for key types + use SmallVec

--- crates/vm/src/rel_ops.rs
+++ crates/vm/src/rel_ops.rs
@@ -1,6 +1,8 @@
 use crate::errors::ErrorVm;
 use crate::relation::RelValue;
-use spacetimedb_data_structures::map::HashMap;
+use core::hash::{BuildHasher, Hash};
+use smallvec::SmallVec;
+use spacetimedb_data_structures::map::{HashMap, IntMap};
 use spacetimedb_sats::relation::{FieldExpr, Header, RowCount};
 use spacetimedb_sats::AlgebraicValue;
 use std::sync::Arc;
@@ -213,6 +215,25 @@ where
     }
 }
 
+type IndexValues<'a> = SmallVec<[RelValue<'a>; 1]>;
+#[derive(Clone, Debug)]
+enum HashIndex<'a> {
+    Bool(HashMap<bool, IndexValues<'a>>),
+    U8(IntMap<u8, IndexValues<'a>>),
+    I8(IntMap<i8, IndexValues<'a>>),
+    U16(IntMap<u16, IndexValues<'a>>),
+    I16(IntMap<i16, IndexValues<'a>>),
+    U32(IntMap<u32, IndexValues<'a>>),
+    I32(IntMap<i32, IndexValues<'a>>),
+    U64(IntMap<u64, IndexValues<'a>>),
+    I64(IntMap<i64, IndexValues<'a>>),
+    U128(HashMap<u128, IndexValues<'a>>),
+    I128(HashMap<i128, IndexValues<'a>>),
+    String(HashMap<Box<str>, IndexValues<'a>>),
+    AV(HashMap<AlgebraicValue, IndexValues<'a>>),
+    Empty,
+}
+
 #[derive(Clone, Debug)]
 pub struct JoinInner<'a, Lhs, Rhs, KeyLhs, KeyRhs, Pred, Proj> {
     pub(crate) head: Arc<Header>,
@@ -222,7 +243,7 @@ pub struct JoinInner<'a, Lhs, Rhs, KeyLhs, KeyRhs, Pred, Proj> {
     pub(crate) key_rhs: KeyRhs,
     pub(crate) predicate: Pred,
     pub(crate) projection: Proj,
-    map: HashMap<AlgebraicValue, Vec<RelValue<'a>>>,
+    map: HashIndex<'a>,
     filled_rhs: bool,
     left: Option<RelValue<'a>>,
 }
@@ -239,7 +260,7 @@ impl<'a, Lhs, Rhs, KeyLhs, KeyRhs, Pred, Proj> JoinInner<'a, Lhs, Rhs, KeyLhs, K
     ) -> Self {
         Self {
             head,
-            map: HashMap::new(),
+            map: HashIndex::Empty,
             lhs,
             rhs,
             key_lhs,
@@ -249,6 +270,59 @@ impl<'a, Lhs, Rhs, KeyLhs, KeyRhs, Pred, Proj> JoinInner<'a, Lhs, Rhs, KeyLhs, K
             filled_rhs: false,
             left: None,
         }
+    }
+
+    fn build_index<K: Hash + Eq, S: Default + BuildHasher>(
+        &mut self,
+        first_key: K,
+        first_row: RelValue<'a>,
+        mut ext: impl FnMut(AlgebraicValue) -> Result<K, AlgebraicValue>,
+    ) -> Result<HashMap<K, IndexValues<'a>, S>, ErrorVm>
+    where
+        Rhs: RelOps<'a>,
+        KeyRhs: FnMut(&RelValue<'a>) -> AlgebraicValue,
+    {
+        let mut map: HashMap<K, IndexValues<'a>, S> = <_>::default();
+        map.entry(first_key).or_default().push(first_row);
+        while let Some(row_rhs) = self.rhs.next()? {
+            let key_rhs = ext((self.key_rhs)(&row_rhs)).unwrap();
+            map.entry(key_rhs).or_default().push(row_rhs);
+        }
+        Ok(map)
+    }
+}
+
+fn relate_loop<'a, K: Hash + Eq, S: BuildHasher>(
+    left: &mut Option<RelValue<'a>>,
+    lhs: &mut impl RelOps<'a>,
+    key_lhs: &mut impl FnMut(&RelValue<'a>) -> AlgebraicValue,
+    pred: &mut impl FnMut(&RelValue<'a>, &RelValue<'a>) -> bool,
+    proj: &mut impl FnMut(RelValue<'a>, RelValue<'a>) -> RelValue<'a>,
+    map: &mut HashMap<K, IndexValues<'a>, S>,
+    mut ext: impl FnMut(AlgebraicValue) -> Result<K, AlgebraicValue>,
+) -> Result<Option<RelValue<'a>>, ErrorVm> {
+    loop {
+        // Consume a row in `Lhs` and project to `KeyLhs`.
+        let lhs = match &left {
+            Some(left) => left,
+            None => match lhs.next()? {
+                Some(x) => left.insert(x),
+                None => return Ok(None),
+            },
+        };
+        let k = ext((key_lhs)(lhs)).unwrap();
+
+        // If we can relate `KeyLhs` and `KeyRhs`, we have candidate.
+        // If that candidate still has rhs elements, test against the predicate and yield.
+        if let Some(rvv) = map.get_mut(&k) {
+            if let Some(rhs) = rvv.pop() {
+                if (pred)(lhs, &rhs) {
+                    return Ok(Some((proj)(lhs.clone(), rhs)));
+                }
+            }
+        }
+        *left = None;
+        continue;
     }
 }
 
@@ -266,38 +340,53 @@ where
     }
 
     fn next(&mut self) -> Result<Option<RelValue<'a>>, ErrorVm> {
+        use HashIndex::*;
+
         // Consume `Rhs`, building a map `KeyRhs => Rhs`.
         if !self.filled_rhs {
-            self.map = HashMap::with_capacity(self.rhs.row_count().min);
-            while let Some(row_rhs) = self.rhs.next()? {
+            if let Some(row_rhs) = self.rhs.next()? {
                 let key_rhs = (self.key_rhs)(&row_rhs);
-                self.map.entry(key_rhs).or_default().push(row_rhs);
+                self.map = match key_rhs {
+                    AlgebraicValue::Bool(k) => Bool(self.build_index(k, row_rhs, AlgebraicValue::into_bool)?),
+                    AlgebraicValue::I8(k) => I8(self.build_index(k, row_rhs, AlgebraicValue::into_i8)?),
+                    AlgebraicValue::U8(k) => U8(self.build_index(k, row_rhs, AlgebraicValue::into_u8)?),
+                    AlgebraicValue::I16(k) => I16(self.build_index(k, row_rhs, AlgebraicValue::into_i16)?),
+                    AlgebraicValue::U16(k) => U16(self.build_index(k, row_rhs, AlgebraicValue::into_u16)?),
+                    AlgebraicValue::I32(k) => I32(self.build_index(k, row_rhs, AlgebraicValue::into_i32)?),
+                    AlgebraicValue::U32(k) => U32(self.build_index(k, row_rhs, AlgebraicValue::into_u32)?),
+                    AlgebraicValue::I64(k) => I64(self.build_index(k, row_rhs, AlgebraicValue::into_i64)?),
+                    AlgebraicValue::U64(k) => U64(self.build_index(k, row_rhs, AlgebraicValue::into_u64)?),
+                    AlgebraicValue::I128(k) => I128(self.build_index(k.0, row_rhs, |k| k.into_i128().map(|k| k.0))?),
+                    AlgebraicValue::U128(k) => U128(self.build_index(k.0, row_rhs, |k| k.into_u128().map(|k| k.0))?),
+                    AlgebraicValue::String(k) => String(self.build_index(k, row_rhs, AlgebraicValue::into_string)?),
+                    k => AV(self.build_index(k, row_rhs, |k| Ok(k))?),
+                };
+            } else {
+                self.map = Empty;
             }
             self.filled_rhs = true;
         }
 
-        loop {
-            // Consume a row in `Lhs` and project to `KeyLhs`.
-            let lhs = match &self.left {
-                Some(left) => left,
-                None => match self.lhs.next()? {
-                    Some(x) => self.left.insert(x),
-                    None => return Ok(None),
-                },
-            };
-            let k = (self.key_lhs)(lhs);
-
-            // If we can relate `KeyLhs` and `KeyRhs`, we have candidate.
-            // If that candidate still has rhs elements, test against the predicate and yield.
-            if let Some(rvv) = self.map.get_mut(&k) {
-                if let Some(rhs) = rvv.pop() {
-                    if (self.predicate)(lhs, &rhs) {
-                        return Ok(Some((self.projection)(lhs.clone(), rhs)));
-                    }
-                }
-            }
-            self.left = None;
-            continue;
+        let left = &mut self.left;
+        let lhs = &mut self.lhs;
+        let klhs = &mut self.key_lhs;
+        let pred = &mut self.predicate;
+        let proj = &mut self.projection;
+        match &mut self.map {
+            Bool(map) => relate_loop(left, lhs, klhs, pred, proj, map, AlgebraicValue::into_bool),
+            U8(map) => relate_loop(left, lhs, klhs, pred, proj, map, AlgebraicValue::into_u8),
+            I8(map) => relate_loop(left, lhs, klhs, pred, proj, map, AlgebraicValue::into_i8),
+            U16(map) => relate_loop(left, lhs, klhs, pred, proj, map, AlgebraicValue::into_u16),
+            I16(map) => relate_loop(left, lhs, klhs, pred, proj, map, AlgebraicValue::into_i16),
+            U32(map) => relate_loop(left, lhs, klhs, pred, proj, map, AlgebraicValue::into_u32),
+            I32(map) => relate_loop(left, lhs, klhs, pred, proj, map, AlgebraicValue::into_i32),
+            U64(map) => relate_loop(left, lhs, klhs, pred, proj, map, AlgebraicValue::into_u64),
+            I64(map) => relate_loop(left, lhs, klhs, pred, proj, map, AlgebraicValue::into_i64),
+            U128(map) => relate_loop(left, lhs, klhs, pred, proj, map, |k| k.into_u128().map(|k| k.0)),
+            I128(map) => relate_loop(left, lhs, klhs, pred, proj, map, |k| k.into_i128().map(|k| k.0)),
+            String(map) => relate_loop(left, lhs, klhs, pred, proj, map, AlgebraicValue::into_string),
+            AV(map) => relate_loop(left, lhs, klhs, pred, proj, map, |k| Ok(k)),
+            Empty => Ok(None),
         }
     }
 }
