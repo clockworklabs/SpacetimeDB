ab2cfb51c6e1a5238f72fc2a72a328e6ce06264a
Phoebe Goldman <phoebe@clockworklabs.io>
2024-06-04 12:29:25 -0400

    Zeke's nit: simplify control flow
    Co-authored-by: Zeke Foppa <196249+bfops@users.noreply.github.com>
    Signed-off-by: Phoebe Goldman <phoebe@goldman-tribe.org>

--- crates/fs-utils/src/lib.rs
+++ crates/fs-utils/src/lib.rs
@@ -19,5 +19,14 @@ pub fn create_parent_dir(file: &Path) -> Result<(), std::io::Error> {
         // If `parent` already exists as a directory, this is a no-op.
         std::fs::create_dir_all(parent)?;
     }
-    Ok(())
+    // If the `file` path is a relative path with no directory component,
+    // `parent` will be the empty path.
+    // In this case, do not attempt to create a directory.
+    if parent == Path::new("") {
+        return Ok(());
+    }
+    // If the `file` path has a directory component,
+    // do `create_dir_all` to ensure it exists.
+    // If `parent` already exists as a directory, this is a no-op.
+    std::fs::create_dir_all(parent)
 }
