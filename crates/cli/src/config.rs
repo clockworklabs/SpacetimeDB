bdab136a884c57c191d362349bafeaa0cd369f63
Zeke Foppa <github.com/bfops>
2024-06-05 15:20:35 -0700

    [bfops/fix-config-lock]: review

--- crates/cli/src/config.rs
+++ crates/cli/src/config.rs
@@ -858,12 +858,12 @@ impl Config {
         //
         // We used to use `Lockfile` to prevent this from happening, but we had other issues with
         // that approach (see https://github.com/clockworklabs/SpacetimeDB/issues/1339).
-        // We should eventually reintroduce `Lockfile`, with further fixes including OS locks (see
-        // the TODO in `lockfile.rs`).
+        // We should eventually reintroduce `Lockfile`, with further fixes (see the TODO in
+        // `lockfile.rs`).
         //
-        // (In a perfect world, we would also distinguish a read lock from a write lock, so that
+        // In a perfect world, we would also distinguish a read lock from a write lock, so that
         // multiple parallel processes could read the config file. The current `Lockfile` doesn't
-        // allow this).
+        // allow this, but it's not the end of the world.
         if let Err(e) = atomic_write(&home_path, config) {
             eprintln!("could not save config file: {e}")
         }
