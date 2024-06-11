535bdf56d65a34dfacf19c830145292e0cb8ae8a
joshua-spacetime <josh@clockworklabs.io>
2024-06-11 15:17:02 -0700

    chore: Remove max value metrics
    When a histogram's buckets are defined correctly,
    max values do not provide any additional insight over the p99.

--- crates/core/src/db/db_metrics/mod.rs
+++ crates/core/src/db/db_metrics/mod.rs
@@ -1,11 +1,9 @@
 use crate::execution_context::WorkloadType;
 use once_cell::sync::Lazy;
-use prometheus::{GaugeVec, HistogramVec, IntCounterVec, IntGaugeVec};
-use spacetimedb_data_structures::map::HashMap;
+use prometheus::{HistogramVec, IntCounterVec, IntGaugeVec};
 use spacetimedb_lib::Address;
 use spacetimedb_metrics::metrics_group;
 use spacetimedb_primitives::TableId;
-use std::sync::Mutex;
 
 metrics_group!(
     #[non_exhaustive]
@@ -61,11 +59,6 @@ metrics_group!(
         )]
         pub rdb_txn_cpu_time_sec: HistogramVec,
 
-        #[name = spacetime_txn_cpu_time_sec_max]
-        #[help = "The cpu time of the longest running transaction (in seconds)"]
-        #[labels(txn_type: WorkloadType, db: Address, reducer: str)]
-        pub rdb_txn_cpu_time_sec_max: GaugeVec,
-
         #[name = spacetime_message_log_size_bytes]
         #[help = "For a given database, the number of bytes occupied by its message log"]
         #[labels(db: Address)]
@@ -83,18 +76,7 @@ metrics_group!(
     }
 );
 
-type ReducerLabel = (Address, WorkloadType, String);
-type AddressLabel = (Address, WorkloadType);
-
-pub static MAX_TX_CPU_TIME: Lazy<Mutex<HashMap<ReducerLabel, f64>>> = Lazy::new(|| Mutex::new(HashMap::new()));
-pub static MAX_QUERY_COMPILE_TIME: Lazy<Mutex<HashMap<AddressLabel, f64>>> = Lazy::new(|| Mutex::new(HashMap::new()));
 pub static DB_METRICS: Lazy<DbMetrics> = Lazy::new(DbMetrics::new);
-
-pub fn reset_counters() {
-    // Reset max reducer durations
-    DB_METRICS.rdb_txn_cpu_time_sec_max.0.reset();
-    MAX_TX_CPU_TIME.lock().unwrap().clear();
-}
 
 /// Returns the number of committed rows in the table named by `table_name` and identified by `table_id` in the database `db_address`.
 pub fn table_num_rows(db_address: Address, table_id: TableId, table_name: &str) -> u64 {
