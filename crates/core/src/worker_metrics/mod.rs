9384e7cfbe2784cc8dd061de5113b5ac83343a31
joshua-spacetime <josh@clockworklabs.io>
2024-06-12 14:38:40 -0700

    chore: Remove already tracked subscription counter
    We already track subscription requests through the rdb_num_txns counter.

--- crates/core/src/worker_metrics/mod.rs
+++ crates/core/src/worker_metrics/mod.rs
@@ -56,11 +56,6 @@ metrics_group!(
         #[labels(identity: Identity, module_hash: Hash, database_address: Address, reducer_symbol: str)]
         pub wasm_instance_errors: IntCounterVec,
 
-        #[name = spacetime_initial_subscription_evals]
-        #[help = "The cumulative number of initial subscription evaluations"]
-        #[labels(database_address: Address)]
-        pub initial_subscription_evals: IntCounterVec,
-
         #[name = spacetime_active_queries]
         #[help = "The number of active subscription queries"]
         #[labels(database_address: Address)]
