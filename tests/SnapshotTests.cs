b7010946b18fb64323323d4e4d0769191ac8b1f4
Ingvar Stepanyan <me@rreverser.com>
2024-06-15 23:24:15 +0100

    Store test dump in a maintainable format

--- tests/SnapshotTests.cs
+++ tests/SnapshotTests.cs
@@ -1,5 +1,8 @@
 namespace SpacetimeDB.Tests;
 
+using System.IO.Compression;
+using System.Runtime.CompilerServices;
+using Argon;
 using Google.Protobuf;
 using SpacetimeDB.Types;
 using Xunit;
@@ -55,30 +58,62 @@ public class SnapshotTests
         }
     }
 
+    class ByteStringReaderConverter : JsonConverter<ByteString>
+    {
+        public override ByteString ReadJson(
+            JsonReader reader,
+            Type type,
+            ByteString? existingValue,
+            bool hasExisting,
+            JsonSerializer serializer
+        )
+        {
+            var s = reader.StringValue;
+            try
+            {
+                return ByteString.FromBase64(s);
+            }
+            catch
+            {
+                return ByteString.CopyFromUtf8(s);
+            }
+        }
+
+        public override void WriteJson(
+            JsonWriter writer,
+            ByteString value,
+            JsonSerializer serializer
+        )
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    private static string GetTestDir([CallerFilePath] string testFilePath = "") =>
+        Path.GetDirectoryName(testFilePath)!;
+
     [Fact]
     public async Task VerifyAllTablesParsed()
     {
-        // A dump of sample raw WebSocket messages.
-        var wsSamples = new[]
+        var settings = new JsonSerializerSettings
         {
-            "i6SAKscCCiCXSrMbUY9G0LWbCv7nir3y2/TfbmCjMtzpw3Ori+vOphKQAmV5SjBlWEFpT2lKS1YxUWlMQ0poYkdjaU9pSkZVekkxTmlKOS5leUpvWlhoZmFXUmxiblJwZEhraU9pSTVOelJoWWpNeFlqVXhPR1kwTm1Rd1lqVTVZakJoWm1WbE56aGhZbVJtTW1SaVpqUmtaalpsTmpCaE16TXlaR05sT1dNek56TmhZamhpWldKalpXRTJJaXdpYVdGMElqb3hOekUzTlRJek1qTXhMQ0psZUhBaU9tNTFiR3g5LjNRZEpkc2tLdTRlamdZMXVvQmk3TTBQN1QyaXpaSEhITXhDcEpTRWROU1dEaXZzZ0FFdkZJUmZxVUFKY0J0U0txaEZPaXRiZHhIUVJKd3VaQV8wejdRGhBgfJ3DgQmutLQUG2NU/BhfAw==",
-            "ix6AEjwKNQiBIBIEVXNlchoqCAEaJiAAAACXSrMbUY9G0LWbCv7nir3y2/TfbmCjMtzpw3Ori+vOpgEBEAEYuwgD",
-            "i2WAIskBCloIzIrmlIPehgMSIJdKsxtRj0bQtZsK/ueKvfLb9N9uYKMy3OnDc6uL686mGhMKCHNldF9uYW1lEgUBAAAAQRgBMI+eiQI4kgFCEGB8ncOBCa60tBQbY1T8GF8SawpkCIEgEgRVc2VyGigaJiAAAACXSrMbUY9G0LWbCv7nir3y2/TfbmCjMtzpw3Ori+vOpgEBGi8IARorIAAAAJdKsxtRj0bQtZsK/ueKvfLb9N9uYKMy3OnDc6uL686mAAEAAABBARABGLsQAw==",
-            "i2KAIsMBCmkI99mQmYPehgMSIJdKsxtRj0bQtZsK/ueKvfLb9N9uYKMy3OnDc6uL686mGiMKDHNlbmRfbWVzc2FnZRIRDQAAAEhlbGxvLCB3b3JsZCEYAjDfsrQBOCpCEGB8ncOBCa60tBQbY1T8GF8SVgpPCIIgEgdNZXNzYWdlGkEIARo9IAAAAJdKsxtRj0bQtZsK/ueKvfLb9N9uYKMy3OnDc6uL686m9ywkM/AaBgANAAAASGVsbG8sIHdvcmxkIRACGP8HAw==",
-            "C02AIpgBCl0I5obruIPehgMSIKtdmHT9AV3sDUOODydYrtVLR90/0mJ5TLJCwBmnpDdEGhgKFl9faWRlbnRpdHlfY29ubmVjdGVkX18w7713OEZCEJC8PImObEANVUiHlRorK9kSNwo1CIEgEgRVc2VyGioIARomIAAAAKtdmHT9AV3sDUOODydYrtVLR90/0mJ5TLJCwBmnpDdEAQED",
-            "i6SAKscCCiCrXZh0/QFd7A1Djg8nWK7VS0fdP9JieUyyQsAZp6Q3RBKQAmV5SjBlWEFpT2lKS1YxUWlMQ0poYkdjaU9pSkZVekkxTmlKOS5leUpvWlhoZmFXUmxiblJwZEhraU9pSmhZalZrT1RnM05HWmtNREUxWkdWak1HUTBNemhsTUdZeU56VTRZV1ZrTlRSaU5EZGtaRE5tWkRJMk1qYzVOR05pTWpReVl6QXhPV0UzWVRRek56UTBJaXdpYVdGMElqb3hOekU0TkRZNE9EYzVMQ0psZUhBaU9tNTFiR3g5LjNFWXQ1NkRpc2FBOUZfV0NOemhMSDU1LWh4TldNNEhQYjdQQi03OWx3Y2Ffcy1YSllBaXB2ZDlONmRXY2xYb3VmU0JyeXhuQlBkVTVsMHRoZHpoMmhBGhCQvDyJjmxADVVIh5UaKyvZAw==",
-            "C2CAEr4BCmYIgSASBFVzZXIaLwgBGisgAAAAl0qzG1GPRtC1mwr+54q98tv0325gozLc6cNzq4vrzqYAAQAAAEEBGioIARomIAAAAKtdmHT9AV3sDUOODydYrtVLR90/0mJ5TLJCwBmnpDdEAQEKTwiCIBIHTWVzc2FnZRpBCAEaPSAAAACXSrMbUY9G0LWbCv7nir3y2/TfbmCjMtzpw3Ori+vOpvcsJDPwGgYADQAAAEhlbGxvLCB3b3JsZCEQARipAgM=",
-            "C2WAIsgBClkIw8m6uoPehgMSIKtdmHT9AV3sDUOODydYrtVLR90/0mJ5TLJCwBmnpDdEGhMKCHNldF9uYW1lEgUBAAAAQhgBMMfEhAI4TUIQkLw8iY5sQA1VSIeVGisr2RJrCmQIgSASBFVzZXIaKBomIAAAAKtdmHT9AV3sDUOODydYrtVLR90/0mJ5TLJCwBmnpDdEAQEaLwgBGisgAAAAq12YdP0BXewNQ44PJ1iu1UtH3T/SYnlMskLAGaekN0QAAQAAAEIBEAEYqgYD",
-            "C2WAIsgBClkIw8m6uoPehgMSIKtdmHT9AV3sDUOODydYrtVLR90/0mJ5TLJCwBmnpDdEGhMKCHNldF9uYW1lEgUBAAAAQhgBMMfEhAI4TUIQkLw8iY5sQA1VSIeVGisr2RJrCmQIgSASBFVzZXIaKBomIAAAAKtdmHT9AV3sDUOODydYrtVLR90/0mJ5TLJCwBmnpDdEAQEaLwgBGisgAAAAq12YdP0BXewNQ44PJ1iu1UtH3T/SYnlMskLAGaekN0QAAQAAAEIBEAEYqwYD",
-            "i16AIrsBCmUI9Jv1u4PehgMSIKtdmHT9AV3sDUOODydYrtVLR90/0mJ5TLJCwBmnpDdEGh8KDHNlbmRfbWVzc2FnZRINCQAAAEhlbGxvLCBBIRgCMO+2owE4LkIQkLw8iY5sQA1VSIeVGisr2RJSCksIgiASB01lc3NhZ2UaPQgBGjkgAAAAq12YdP0BXewNQ44PJ1iu1UtH3T/SYnlMskLAGaekN0T0TX038BoGAAkAAABIZWxsbywgQSEQAhi1BgM=",
-            "i16AIrsBCmUI9Jv1u4PehgMSIKtdmHT9AV3sDUOODydYrtVLR90/0mJ5TLJCwBmnpDdEGh8KDHNlbmRfbWVzc2FnZRINCQAAAEhlbGxvLCBBIRgCMO+2owE4LkIQkLw8iY5sQA1VSIeVGisr2RJSCksIgiASB01lc3NhZ2UaPQgBGjkgAAAAq12YdP0BXewNQ44PJ1iu1UtH3T/SYnlMskLAGaekN0T0TX038BoGAAkAAABIZWxsbywgQSEQAhjEBgM=",
-            "i16AIrsBCmUIi7rVvYPehgMSIJdKsxtRj0bQtZsK/ueKvfLb9N9uYKMy3OnDc6uL686mGh8KDHNlbmRfbWVzc2FnZRINCQAAAEhlbGxvLCBCIRgDMO+2owE4HkIQYHydw4EJrrS0FBtjVPwYXxJSCksIgiASB01lc3NhZ2UaPQgBGjkgAAAAl0qzG1GPRtC1mwr+54q98tv0325gozLc6cNzq4vrzqYLXbU38BoGAAkAAABIZWxsbywgQiEQAxj6BQM=",
-            "i16AIrsBCmUIi7rVvYPehgMSIJdKsxtRj0bQtZsK/ueKvfLb9N9uYKMy3OnDc6uL686mGh8KDHNlbmRfbWVzc2FnZRINCQAAAEhlbGxvLCBCIRgDMO+2owE4HkIQYHydw4EJrrS0FBtjVPwYXxJSCksIgiASB01lc3NhZ2UaPQgBGjkgAAAAl0qzG1GPRtC1mwr+54q98tv0325gozLc6cNzq4vrzqYLXbU38BoGAAkAAABIZWxsbywgQiEQAxjpBQM=",
-            "i1yAIrcBCmMIyZLPwoPehgMSIJdKsxtRj0bQtZsK/ueKvfLb9N9uYKMy3OnDc6uL686mGh0KDHNlbmRfbWVzc2FnZRILBwAAAEdvb2RieWUYBDCnzqMBODhCEGB8ncOBCa60tBQbY1T8GF8SUApJCIIgEgdNZXNzYWdlGjsIARo3IAAAAJdKsxtRj0bQtZsK/ueKvfLb9N9uYKMy3OnDc6uL686mSclTOPAaBgAHAAAAR29vZGJ5ZRAEGNEJAw==",
-            "i1yAIrcBCmMIyZLPwoPehgMSIJdKsxtRj0bQtZsK/ueKvfLb9N9uYKMy3OnDc6uL686mGh0KDHNlbmRfbWVzc2FnZRILBwAAAEdvb2RieWUYBDCnzqMBODhCEGB8ncOBCa60tBQbY1T8GF8SUApJCIIgEgdNZXNzYWdlGjsIARo3IAAAAJdKsxtRj0bQtZsK/ueKvfLb9N9uYKMy3OnDc6uL686mSclTOPAaBgAHAAAAR29vZGJ5ZRAEGOEJAw==",
-            "C2kAAICqqqrqXxTcD+p6clDwg7vaxcCvDg7uYCe/Obgf/OA3B3A/qzmY6UHBAdQB3OziAH4xcAA/uB/saAcHPzmAXxzAL25+cQA/2d3B7ubgdnRw8JuBHx0cHHIuLebjATHF4GDMGUquqzEiY+xnabR8rnJUD/pnbeD34suW/F/rW5Z8U2Xujcc7co6bz/3ANFLhXNpCudKdc1s61lQpYbnC5ryZXhPUaneL5mIXeYVTGJY0a/grczohEns6zY8cMWN1VtMBINjlExAAdBBrYMSiDjA=",
-            "i1yAIrcBCmMIrM+DxYPehgMSIKtdmHT9AV3sDUOODydYrtVLR90/0mJ5TLJCwBmnpDdEGh0KDHNlbmRfbWVzc2FnZRILBwAAAEdvb2RieWUYAzCnzqMBOClCEJC8PImObEANVUiHlRorK9kSUApJCIIgEgdNZXNzYWdlGjsIARo3IAAAAKtdmHT9AV3sDUOODydYrtVLR90/0mJ5TLJCwBmnpDdErOegOPAaBgAHAAAAR29vZGJ5ZRADGIQGAw==",
+            Converters = [new ByteStringReaderConverter()]
         };
+
+        // We store the dump in JSON-NL format for simplicity (it's just `ClientApi.Message.toString()`) and readability.
+        var sampleDumpParsed = File.ReadLines(Path.Combine(GetTestDir(), "sample-dump.jsonl"))
+            .Select(line => JsonConvert.DeserializeObject<ClientApi.Message>(line, settings));
+
+        // But for proper testing we need to convert it back to raw binary messages as if it was received over network.
+        var sampleDumpBinary = sampleDumpParsed.Select(message =>
+        {
+            using var output = new MemoryStream();
+            using (var brotli = new BrotliStream(output, CompressionMode.Compress))
+            {
+                message.WriteTo(brotli);
+            }
+            return output.ToArray();
+        });
 
         var events = new Events();
 
@@ -86,10 +121,15 @@ public class SnapshotTests
 
         var client = SpacetimeDBClient.instance;
 
+        Identity? myIdentity = null;
+
         client.onBeforeSubscriptionApplied += () => events.Add("OnBeforeSubscriptionApplied");
         client.onEvent += (ev) => events.Add("OnEvent", ev);
         client.onIdentityReceived += (_authToken, identity, address) =>
+        {
+            myIdentity = identity;
             events.Add("OnIdentityReceived", new { identity, address });
+        };
         client.onSubscriptionApplied += () => events.Add("OnSubscriptionApplied");
         client.onUnhandledReducerError += (exception) =>
             events.Add("OnUnhandledReducerError", exception);
@@ -119,9 +159,9 @@ public class SnapshotTests
             events.Add("OnInsertMessage", new { message, reducerEvent });
 
         // Simulate receiving WebSocket messages.
-        foreach (var wsSample in wsSamples)
+        foreach (var sample in sampleDumpBinary)
         {
-            client.OnMessageReceived(Convert.FromBase64String(wsSample), DateTime.UtcNow);
+            client.OnMessageReceived(sample, DateTime.UtcNow);
             // Wait for this message to be picked up by the background thread, preprocessed and stored in the preprocessed queue.
             // Otherwise we'll get inconsistent output order between test reruns.
             while (!client.HasPreProcessedMessage) { }
@@ -141,16 +181,11 @@ public class SnapshotTests
                     }
                 }
             )
-            .AddExtraSettings(settings => settings.Converters.Add(new EventsConverter()))
-            .ScrubLinesWithReplace(s =>
-                s.Replace(
-                        "974AB31B518F46D0B59B0AFEE78ABDF2DBF4DF6E60A332DCE9C373AB8BEBCEA6",
-                        "(identity of A)"
-                    )
-                    .Replace(
-                        "AB5D9874FD015DEC0D438E0F2758AED54B47DD3FD262794CB242C019A7A43744",
-                        "(identity of B)"
-                    )
-            );
+            .AddExtraSettings(settings =>
+                settings.Converters.AddRange(
+                    [new EventsConverter(), new IdentityConverter(myIdentity)]
+                )
+            )
+            .ScrubMember<ClientApi.Event>(_ => _.CallerIdentity);
     }
 }
