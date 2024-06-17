7e0d958131a845db55c13fa63869db3f0d86812b
Ingvar Stepanyan <me@rreverser.com>
2024-06-17 11:50:42 +0100

    Move Verify converters around

--- tests/VerifyInit.cs
+++ tests/VerifyInit.cs
@@ -4,71 +4,78 @@ using System.Runtime.CompilerServices;
 using Google.Protobuf;
 using SpacetimeDB.Types;
 
-class ByteStringConverter : WriteOnlyJsonConverter<ByteString>
-{
-    public override void Write(VerifyJsonWriter writer, ByteString value)
-    {
-        writer.WriteValue(Convert.ToHexString(value.Span));
-    }
-}
-
-// A converter that scrubs identity to a stable string.
-class IdentityConverter : WriteOnlyJsonConverter<Identity>
-{
-    private static readonly List<Identity> seenIdentities = [];
-
-    public override void Write(VerifyJsonWriter writer, Identity value)
-    {
-        var index = seenIdentities.IndexOf(value);
-        if (index == -1)
-        {
-            index = seenIdentities.Count;
-            seenIdentities.Add(value);
-        }
-
-        writer.WriteValue($"Identity_{index + 1}");
-    }
-}
-
-class AddressConverter : WriteOnlyJsonConverter<Address>
-{
-    public override void Write(VerifyJsonWriter writer, Address value)
-    {
-        // Addresses are GUIDs, which Verify scrubs automatically.
-        writer.WriteValue(value.ToString());
-    }
-}
-
-class NetworkRequestTrackerConverter : WriteOnlyJsonConverter<NetworkRequestTracker>
-{
-    public override void Write(VerifyJsonWriter writer, NetworkRequestTracker value)
-    {
-        writer.WriteStartObject();
-
-        var sampleCount = value.GetSampleCount();
-        if (sampleCount > 0)
-        {
-            writer.WriteMember(value, sampleCount, nameof(sampleCount));
-        }
-
-        var requestsAwaitingResponse = value.GetRequestsAwaitingResponse();
-        if (requestsAwaitingResponse > 0)
-        {
-            writer.WriteMember(value, requestsAwaitingResponse, nameof(requestsAwaitingResponse));
-        }
-
-        if (value.GetMinMaxTimes(int.MaxValue) is { Min.Metadata: var Min, Max.Metadata: var Max })
-        {
-            writer.WriteMember(value, Min, nameof(Min));
-            writer.WriteMember(value, Max, nameof(Max));
-        }
-
-        writer.WriteEndObject();
-    }
-}
-
 static class VerifyInit
 {
+    class ByteStringConverter : WriteOnlyJsonConverter<ByteString>
+    {
+        public override void Write(VerifyJsonWriter writer, ByteString value)
+        {
+            writer.WriteValue(Convert.ToHexString(value.Span));
+        }
+    }
+
+    // A converter that scrubs identity to a stable string.
+    class IdentityConverter : WriteOnlyJsonConverter<Identity>
+    {
+        private static readonly List<Identity> seenIdentities = [];
+
+        public override void Write(VerifyJsonWriter writer, Identity value)
+        {
+            var index = seenIdentities.IndexOf(value);
+            if (index == -1)
+            {
+                index = seenIdentities.Count;
+                seenIdentities.Add(value);
+            }
+
+            writer.WriteValue($"Identity_{index + 1}");
+        }
+    }
+
+    class AddressConverter : WriteOnlyJsonConverter<Address>
+    {
+        public override void Write(VerifyJsonWriter writer, Address value)
+        {
+            // Addresses are GUIDs, which Verify scrubs automatically.
+            writer.WriteValue(value.ToString());
+        }
+    }
+
+    class NetworkRequestTrackerConverter : WriteOnlyJsonConverter<NetworkRequestTracker>
+    {
+        public override void Write(VerifyJsonWriter writer, NetworkRequestTracker value)
+        {
+            writer.WriteStartObject();
+
+            var sampleCount = value.GetSampleCount();
+            if (sampleCount > 0)
+            {
+                writer.WriteMember(value, sampleCount, nameof(sampleCount));
+            }
+
+            var requestsAwaitingResponse = value.GetRequestsAwaitingResponse();
+            if (requestsAwaitingResponse > 0)
+            {
+                writer.WriteMember(
+                    value,
+                    requestsAwaitingResponse,
+                    nameof(requestsAwaitingResponse)
+                );
+            }
+
+            if (
+                value.GetMinMaxTimes(int.MaxValue) is
+                { Min.Metadata: var Min, Max.Metadata: var Max }
+            )
+            {
+                writer.WriteMember(value, Min, nameof(Min));
+                writer.WriteMember(value, Max, nameof(Max));
+            }
+
+            writer.WriteEndObject();
+        }
+    }
+
     [ModuleInitializer]
     public static void Init()
     {
@@ -76,7 +83,12 @@ static class VerifyInit
 
         VerifierSettings.AddExtraSettings(settings =>
             settings.Converters.AddRange(
-                [new ByteStringConverter(), new IdentityConverter(), new AddressConverter()]
+                [
+                    new ByteStringConverter(),
+                    new IdentityConverter(),
+                    new AddressConverter(),
+                    new NetworkRequestTrackerConverter()
+                ]
             )
         );
 
