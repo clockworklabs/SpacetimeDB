60c438d1122d787f9ed83b2ec0af6af8a95ff9a2
Kim Altintop <kim@eagain.io>
2024-06-20 07:59:13 +0200

    Smoketest auto-disconnect after restart (#1367)

--- smoketests/tests/zz_docker.py
+++ smoketests/tests/zz_docker.py
@@ -1,5 +1,6 @@
+import time
 from .. import Smoketest, run_cmd, requires_docker
-from urllib.request import urlopen, URLError
+from urllib.request import urlopen
 
 
 def restart_docker():
@@ -23,16 +24,19 @@ def restart_docker():
 def ping():
     tries = 0
     host = "127.0.0.1:3000"
-    while tries < 5:
+    while tries < 10:
         tries += 1
         try:
+            print(f"Ping Server at {host}")
             urlopen(f"http://{host}/database/ping")
+            print("Server up")
             break
-        except URLError:
+        except Exception:
             print("Server down")
+            time.sleep(3)
     else:
         raise Exception(f"Server at {host} not responding")
-    print(f"Server up after {tries} try")
+    print(f"Server up after {tries} tries")
 
 
 @requires_docker
@@ -64,7 +68,7 @@ pub fn say_hello() {
     println!("Hello, World!");
 }
 """
-    
+
     def test_restart_module(self):
         """This tests to see if SpacetimeDB can be queried after a restart"""
 
@@ -111,7 +115,7 @@ pub fn say_hello() {
     println!("Hello, World!");
 }
 """
-    
+
     def test_restart_module(self):
         """This tests to see if SpacetimeDB can be queried after a restart"""
 
@@ -128,8 +132,62 @@ pub fn say_hello() {
         restart_docker()
 
         sql_out = self.spacetime("sql", self.address, "SELECT name FROM Person WHERE id = 3")
-        self.assertMultiLineEqual(sql_out, """\
- name     
-----------
- Samantha 
-""")
+        self.assertMultiLineEqual(sql_out, """ name       \n------------\n "Samantha" \n""")
+
+@requires_docker
+class DockerRestartAutoDisconnect(Smoketest):
+    MODULE_CODE = """
+use log::info;
+use spacetimedb::{spacetimedb, Address, Identity, ReducerContext, TableType};
+
+#[spacetimedb(table)]
+pub struct ConnectedClients {
+    identity: Identity,
+    address: Address,
+}
+
+#[spacetimedb(connect)]
+fn on_connect(ctx: ReducerContext) {
+    ConnectedClients::insert(ConnectedClients {
+        identity: ctx.sender,
+        address: ctx.address.expect("sender address unset"),
+    });
+}
+
+#[spacetimedb(disconnect)]
+fn on_disconnect(ctx: ReducerContext) {
+    let sender_identity = &ctx.sender;
+    let sender_address = ctx.address.as_ref().expect("sender address unset");
+    let match_client = |row: &ConnectedClients| {
+        &row.identity == sender_identity && &row.address == sender_address
+    };
+    if let Some(client) = ConnectedClients::iter().find(match_client) {
+        ConnectedClients::delete(&client);
+    }
+}
+
+#[spacetimedb(reducer)]
+fn print_num_connected() {
+    let n = ConnectedClients::iter().count();
+    info!("CONNECTED CLIENTS: {n}")
+}
+"""
+
+    def test_restart_disconnects(self):
+        """Tests if clients are automatically disconnected after a restart"""
+
+        # Start two subscribers
+        self.subscribe("SELECT * FROM ConnectedClients", n=2)
+        self.subscribe("SELECT * FROM ConnectedClients", n=2)
+
+        # Assert that we have two clients + the reducer call
+        self.call("print_num_connected")
+        logs = self.logs(10)
+        self.assertEqual("CONNECTED CLIENTS: 3", logs.pop())
+
+        restart_docker()
+
+        # After restart, only the current call should be connected
+        self.call("print_num_connected")
+        logs = self.logs(10)
+        self.assertEqual("CONNECTED CLIENTS: 1", logs.pop())
