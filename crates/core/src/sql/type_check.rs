dd7b16521240ab565223ae57b46d973cedb8c43e
Mario Alejandro Montoya Cortés <mamcx@elmalabarista.com>
2024-06-26 10:45:13 -0500

    Make error messages more readable & fix unsound type check

--- crates/core/src/sql/type_check.rs
+++ crates/core/src/sql/type_check.rs
@@ -2,17 +2,84 @@ use crate::error::PlanError;
 use crate::sql::ast::{From, Join};
 use crate::sql::ast::{Selection, SqlAst};
 use spacetimedb_lib::operator::OpQuery;
+use spacetimedb_sats::algebraic_type::fmt::fmt_algebraic_type;
 use spacetimedb_sats::db::def::ColumnSchema;
 use spacetimedb_sats::relation::FieldName;
-use spacetimedb_sats::AlgebraicType;
+use spacetimedb_sats::{AlgebraicType, AlgebraicValue};
 use spacetimedb_vm::errors::ErrorType;
 use spacetimedb_vm::expr::{FieldExpr, FieldOp};
+use std::fmt;
 
-fn find_field_name(from: &From, field: FieldName) -> Result<&ColumnSchema, PlanError> {
+fn find_field_name(from: &From, field: FieldName) -> Result<(Box<str>, &ColumnSchema), PlanError> {
     from.find_field_name(field).ok_or_else(|| PlanError::UnknownFieldName {
         field,
         tables: from.iter_tables().map(|t| t.table_name.clone()).collect(),
     })
+}
+
+#[derive(Debug)]
+pub enum Typed {
+    Field {
+        table: Box<str>,
+        field: Box<str>,
+        ty: Option<AlgebraicType>,
+    },
+    Value {
+        value: AlgebraicValue,
+        ty: Option<AlgebraicType>,
+    },
+    Cmp {
+        op: OpQuery,
+        lhs: Box<Typed>,
+        rhs: Box<Typed>,
+        ty: AlgebraicType,
+    },
+}
+
+impl Typed {
+    pub fn ty(&self) -> Option<&AlgebraicType> {
+        match self {
+            Typed::Field { ty, .. } => ty.as_ref(),
+            Typed::Value { ty, .. } => ty.as_ref(),
+            Typed::Cmp { ty, .. } => Some(ty),
+        }
+    }
+
+    pub fn set_ty(&mut self, ty: Option<AlgebraicType>) {
+        match self {
+            Typed::Field { ty: ty_lhs, .. } => {
+                *ty_lhs = ty;
+            }
+            Typed::Value { ty: ty_lhs, .. } => {
+                *ty_lhs = ty;
+            }
+            Typed::Cmp { .. } => {}
+        }
+    }
+}
+
+impl fmt::Display for Typed {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Typed::Field { table, field, ty } => {
+                if let Some(ty) = ty {
+                    write!(f, "{table}.{field}: {}", fmt_algebraic_type(ty))
+                } else {
+                    write!(f, "{table}.{field}: ?",)
+                }
+            }
+            Typed::Value { value, ty } => {
+                if let Some(ty) = ty {
+                    write!(f, "{value:?}: {}", fmt_algebraic_type(ty))
+                } else {
+                    write!(f, "{value:?}: ?")
+                }
+            }
+            Typed::Cmp { op, lhs, rhs, .. } => {
+                write!(f, "{lhs} {op} {rhs}")
+            }
+        }
+    }
 }
 
 struct QueryFragment<'a, T> {
@@ -26,52 +93,105 @@ pub(crate) trait TypeCheck {
     fn type_check(&self) -> Result<(), PlanError>;
 }
 
-fn type_check(of: &QueryFragment<FieldOp>) -> Result<Option<AlgebraicType>, PlanError> {
+fn resolve_type(field: &FieldExpr, ty: Option<AlgebraicType>) -> Result<Option<AlgebraicType>, PlanError> {
+    // The `SumType` returns `None` on `type_of` so we need to check against the value
+    if let Some(AlgebraicType::Sum(ty)) = &ty {
+        // We can use in `sql` coercion from string to sum type: `tag = 'name'`
+        if let FieldExpr::Value(val_rhs) = field {
+            if let Some(val_rhs) = val_rhs.as_string() {
+                if ty.get_variant_simple(val_rhs).is_some() {
+                    return Ok(Some(AlgebraicType::Sum(ty.clone())));
+                }
+            }
+            // or check it against a `SumValue` type: `tag = { tag: 0, value: 1 }`
+            if let Some(val_rhs) = val_rhs.as_sum() {
+                if ty.is_simple_enum() && ty.get_variant_by_tag(val_rhs.tag).is_some() {
+                    return Ok(Some(AlgebraicType::Sum(ty.clone())));
+                }
+            }
+        }
+    }
+
+    if let Some(AlgebraicType::Product(_)) = &ty {
+        if let FieldExpr::Value(val) = field {
+            if val.as_bytes().is_some() {
+                return Ok(Some(AlgebraicType::bytes()));
+            }
+        }
+    }
+    Ok(ty)
+}
+
+fn check_both(op: OpQuery, lhs: &Typed, rhs: &Typed) -> Result<(), PlanError> {
+    if lhs.ty() != rhs.ty() {
+        Err(match op {
+            OpQuery::Cmp(_) => ErrorType::TypeMismatch {
+                lhs: lhs.to_string(),
+                rhs: rhs.to_string(),
+            },
+            OpQuery::Logic(op) => ErrorType::TypeMismatchLogic {
+                lhs: lhs.to_string(),
+                rhs: rhs.to_string(),
+                op,
+                expected: fmt_algebraic_type(&AlgebraicType::Bool).to_string(),
+            },
+        }
+        .into())
+    } else {
+        Ok(())
+    }
+}
+
+fn patch_type(lhs: &FieldOp, ty_lhs: &mut Typed, ty_rhs: &Typed) -> Result<(), PlanError> {
+    if let FieldOp::Field(f) = lhs {
+        if let Some(ty) = ty_rhs.ty() {
+            if ty.is_sum() || ty.as_product().map_or(false, |x| x.is_special()) {
+                ty_lhs.set_ty(resolve_type(f, Some(ty.clone()))?);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn type_check(of: &QueryFragment<FieldOp>) -> Result<Typed, PlanError> {
     match of.q {
-        FieldOp::Field(x) => match x {
+        FieldOp::Field(expr) => match expr {
             FieldExpr::Name(x) => {
-                let col = find_field_name(of.from, *x)?;
+                let (table, col) = find_field_name(of.from, *x)?;
 
-                Ok(Some(col.col_type.clone()))
+                Ok(Typed::Field {
+                    table,
+                    field: col.col_name.clone(),
+                    ty: Some(col.col_type.clone()),
+                })
             }
-            FieldExpr::Value(x) => Ok(x.type_of()),
+            FieldExpr::Value(x) => Ok(Typed::Value {
+                value: x.clone(),
+                ty: x.type_of(),
+            }),
         },
-        FieldOp::Cmp { op, lhs, rhs } => match op {
-            OpQuery::Cmp(_) => {
-                let ty_lhs = type_check(&QueryFragment { from: of.from, q: lhs })?;
-                let ty_rhs = type_check(&QueryFragment { from: of.from, q: rhs })?;
+        FieldOp::Cmp { op, lhs, rhs } => {
+            let mut ty_lhs = type_check(&QueryFragment { from: of.from, q: lhs })?;
+            let mut ty_rhs = type_check(&QueryFragment { from: of.from, q: rhs })?;
 
-                // The `SumType` returns `None` on `type_of` so we need to check against the value
-                if let Some(AlgebraicType::Sum(ty_lhs)) = &ty_lhs {
-                    // We can use in `sql` coercion from string to sum type: `tag = 'name'`
-                    if let FieldOp::Field(FieldExpr::Value(x)) = rhs.as_ref() {
-                        if let Some(x) = x.as_string() {
-                            if ty_lhs.get_variant_simple(x).is_some() {
-                                return Ok(Some(AlgebraicType::Sum(ty_lhs.clone())));
-                            }
-                        }
-                        // or check it against a `SumValue` type: `tag = { tag: 0, value: 1 }`
-                        if let Some(x) = x.as_sum() {
-                            if ty_lhs.is_simple_enum() && ty_lhs.get_variant_by_tag(x.tag).is_some() {
-                                return Ok(Some(AlgebraicType::Sum(ty_lhs.clone())));
-                            }
-                        }
-                    }
-                }
-                if ty_lhs != ty_rhs {
-                    return Err(ErrorType::TypeMismatchCmp {
-                        lhs: lhs.as_ref().clone(),
-                        rhs: rhs.as_ref().clone(),
-                        expect: ty_lhs,
-                        given: ty_rhs,
-                    }
-                    .into());
-                } else {
-                    Ok(ty_lhs)
-                }
-            }
-            OpQuery::Logic(_) => Ok(Some(AlgebraicType::Bool)),
-        },
+            let op = match op {
+                OpQuery::Cmp(op) => (*op).into(),
+                OpQuery::Logic(op) => (*op).into(),
+            };
+
+            patch_type(lhs, &mut ty_lhs, &ty_rhs)?;
+            patch_type(rhs, &mut ty_rhs, &ty_lhs)?;
+
+            // If both sides are the same type, then return `Bool` to indicate a logical comparison
+            check_both(op, &ty_lhs, &ty_rhs)?;
+
+            Ok(Typed::Cmp {
+                op,
+                lhs: Box::new(ty_lhs),
+                rhs: Box::new(ty_rhs),
+                ty: AlgebraicType::Bool,
+            })
+        }
     }
 }
 
@@ -89,17 +209,26 @@ impl TypeCheck for QueryFragment<'_, ()> {
     fn type_check(&self) -> Result<(), PlanError> {
         for join in &self.from.joins {
             match join {
-                Join::Inner { rhs: table, on } => {
-                    let lhs = find_field_name(self.from, on.lhs)?;
-                    let rhs = find_field_name(self.from, on.rhs)?;
+                Join::Inner { rhs: _, on } => {
+                    let (table_lhs, lhs) = find_field_name(self.from, on.lhs)?;
+                    let (table_rhs, rhs) = find_field_name(self.from, on.rhs)?;
+                    let ty_lhs = resolve_type(&FieldExpr::Name(on.lhs), Some(lhs.col_type.clone()))?.unwrap();
+                    let ty_rhs = resolve_type(&FieldExpr::Name(on.rhs), Some(rhs.col_type.clone()))?.unwrap();
 
-                    if lhs.col_type != rhs.col_type {
+                    if ty_lhs != ty_rhs {
                         return Err(ErrorType::TypeMismatchJoin {
-                            table: table.table_name.clone(),
-                            lhs: lhs.col_name.clone(),
-                            rhs: rhs.col_name.clone(),
-                            expect: lhs.col_type.clone(),
-                            given: rhs.col_type.clone(),
+                            lhs: Typed::Field {
+                                table: table_lhs,
+                                field: lhs.col_name.clone(),
+                                ty: Some(ty_lhs),
+                            }
+                            .to_string(),
+                            rhs: Typed::Field {
+                                table: table_rhs,
+                                field: rhs.col_name.clone(),
+                                ty: Some(ty_rhs),
+                            }
+                            .to_string(),
                         }
                         .into());
                     }
