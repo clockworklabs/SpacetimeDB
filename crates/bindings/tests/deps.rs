de04726cf1392be0118ba76f354c8e1b8f738f5e
Mario Alejandro Montoya Cortés <mamcx@elmalabarista.com>
2024-07-09 08:25:36 -0500

    Filter extra deps on keccak on macOS that fails snap testing

--- crates/bindings/tests/deps.rs
+++ crates/bindings/tests/deps.rs
@@ -2,12 +2,41 @@
 //! to make sure we don't unknowningly add a bunch of dependencies here,
 //! slowing down compilation for every spacetime module.
 
+#[cfg(target_os = "macos")]
+/// On macOS, the `keccak` crate depends on `cpufeatures/libc`, which don't on Linux.
+fn filter_keccak(input: String) -> String {
+    let mut lines = input.lines();
+    let mut found_keccak = false;
+    let mut found_cpufeatures = false;
+    let mut result = Vec::new();
+
+    while let Some(line) = lines.next() {
+        if line.contains("keccak") {
+            found_keccak = true;
+            result.push(line.to_string());
+        } else if found_keccak && line.contains("cpufeatures") {
+            found_cpufeatures = true;
+        } else if found_cpufeatures && line.contains("libc") {
+        } else {
+            result.push(line.to_string());
+        }
+    }
+
+    result.join("\n")
+}
+
+#[cfg(not(target_os = "macos"))]
+fn filter_keccak(input: String) -> String {
+    input
+}
+
 #[test]
 fn deptree_snapshot() -> std::io::Result<()> {
     let cmd = "cargo tree -p spacetimedb -f {lib} -e no-dev";
-    let deps_tree = run_cmd(cmd);
+    let deps_tree = filter_keccak(run_cmd(cmd));
 
-    let all_deps = run_cmd("cargo tree -p spacetimedb -e no-dev --prefix none --no-dedupe");
+    let all_deps = filter_keccak(run_cmd("cargo tree -p spacetimedb -e no-dev --prefix none --no-dedupe"));
+
     let mut all_deps = all_deps.lines().collect::<Vec<_>>();
     all_deps.sort();
     all_deps.dedup();
