cf0db09156d24bb8fb08b006145792f6a24e2e76
James Gilles <jameshgilles@gmail.com>
2024-07-12 13:49:49 -0400

    Add some helper scripts

---
+++ tools/clippy.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+script_dir="$(readlink -f "$(dirname "$0")")"
+stdb_root="$(realpath "$script_dir/../")"
+
+set -euox pipefail
+
+cd "$stdb_root"
+cargo check --all --tests --benches
+cargo fmt --all -- --check
+cargo clippy --all --tests --benches -- -D warnings
+
