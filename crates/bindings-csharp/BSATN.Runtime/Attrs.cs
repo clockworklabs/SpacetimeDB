1e08fb47b881172e9c0f23a4a3444dcbee619507
Ingvar Stepanyan <me@rreverser.com>
2024-07-25 21:12:01 +0100

    Use latest C# in BSATN.Runtime
    Now that we include it by DLL rather than by source in the Unity SDK, we no longer need to limit ourselves to C# 9 - the only limitation is now the .NET Standard.
    
    This allows to slightly clean up the code by leveraging newer syntactic features.

--- crates/bindings-csharp/BSATN.Runtime/Attrs.cs
+++ crates/bindings-csharp/BSATN.Runtime/Attrs.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
-namespace SpacetimeDB
-{
-    [AttributeUsage(
-        AttributeTargets.Struct | AttributeTargets.Class | AttributeTargets.Enum,
-        Inherited = false,
-        AllowMultiple = false
-    )]
-    public sealed class TypeAttribute : Attribute { }
+namespace SpacetimeDB;
 
-    // This could be an interface, but using `record` forces C# to check that it can
-    // only be applied on types that are records themselves.
-    public abstract record TaggedEnum<Variants>
-        where Variants : struct, ITuple { }
-}
+[AttributeUsage(
+    AttributeTargets.Struct | AttributeTargets.Class | AttributeTargets.Enum,
+    Inherited = false,
+    AllowMultiple = false
+)]
+public sealed class TypeAttribute : Attribute { }
+
+// This could be an interface, but using `record` forces C# to check that it can
+// only be applied on types that are records themselves.
+public abstract record TaggedEnum<Variants>
+    where Variants : struct, ITuple { }
--- crates/bindings-csharp/BSATN.Runtime/Builtins.cs
+++ crates/bindings-csharp/BSATN.Runtime/Builtins.cs
@@ -1,134 +1,127 @@
-using System;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using SpacetimeDB.BSATN;
 using SpacetimeDB.Internal;
 
-namespace SpacetimeDB
+namespace SpacetimeDB;
+
+public readonly partial struct Unit
 {
-    public readonly partial struct Unit
+    // Custom BSATN that returns an inline empty product type that can be recognised by SpacetimeDB.
+    public readonly struct BSATN : IReadWrite<Unit>
     {
-        // Custom BSATN that returns an inline empty product type that can be recognised by SpacetimeDB.
-        public readonly struct BSATN : IReadWrite<Unit>
-        {
-            public Unit Read(BinaryReader reader) => default;
+        public Unit Read(BinaryReader reader) => default;
 
-            public void Write(BinaryWriter writer, Unit value) { }
+        public void Write(BinaryWriter writer, Unit value) { }
 
-            public AlgebraicType GetAlgebraicType(ITypeRegistrar registrar) =>
-                new AlgebraicType.Product(new AggregateElement[] { });
-        }
+        public AlgebraicType GetAlgebraicType(ITypeRegistrar registrar) =>
+            new AlgebraicType.Product([]);
+    }
+}
+
+// A helper for special wrappers around byte arrays like Identity and Address.
+// Makes them equatable, stringifiable, checks length, etc.
+public abstract record BytesWrapper
+{
+    protected abstract int SIZE { get; }
+
+    protected readonly byte[] bytes;
+
+    protected BytesWrapper()
+    {
+        bytes = new byte[SIZE];
     }
 
-    // A helper for special wrappers around byte arrays like Identity and Address.
-    // Makes them equatable, stringifiable, checks length, etc.
-    public abstract record BytesWrapper
+    // We can't hide the base class itself, but at least we can hide the constructor.
+    protected BytesWrapper(byte[] bytes)
     {
-        protected abstract int SIZE { get; }
-
-        protected readonly byte[] bytes;
-
-        protected BytesWrapper()
-        {
-            bytes = new byte[SIZE];
-        }
-
-        // We can't hide the base class itself, but at least we can hide the constructor.
-        protected BytesWrapper(byte[] bytes)
-        {
-            Debug.Assert(bytes.Length == SIZE);
-            this.bytes = bytes;
-        }
-
-        public virtual bool Equals(BytesWrapper? other) =>
-            ByteArrayComparer.Instance.Equals(bytes, other?.bytes);
-
-        public override int GetHashCode() => ByteArrayComparer.Instance.GetHashCode(bytes);
-
-        // Same as `Convert.ToHexString`, but that method is not available in .NET Standard
-        // which we need to target for Unity support.
-        public override string ToString() => BitConverter.ToString(bytes).Replace("-", "");
-
-        protected static byte[] ReadRaw(BinaryReader reader) => ByteArray.Instance.Read(reader);
-
-        protected void Write(BinaryWriter writer) => ByteArray.Instance.Write(writer, bytes);
-
-        // Custom BSATN that returns an inline product type with special property name that can be recognised by SpacetimeDB.
-        protected static AlgebraicType GetAlgebraicType(
-            ITypeRegistrar registrar,
-            string wrapperPropertyName
-        ) =>
-            new AlgebraicType.Product(
-                new AggregateElement[]
-                {
-                    new(wrapperPropertyName, ByteArray.Instance.GetAlgebraicType(registrar))
-                }
-            );
+        Debug.Assert(bytes.Length == SIZE);
+        this.bytes = bytes;
     }
 
-    public record Address : BytesWrapper
+    public virtual bool Equals(BytesWrapper? other) =>
+        ByteArrayComparer.Instance.Equals(bytes, other?.bytes);
+
+    public override int GetHashCode() => ByteArrayComparer.Instance.GetHashCode(bytes);
+
+    // Same as `Convert.ToHexString`, but that method is not available in .NET Standard
+    // which we need to target for Unity support.
+    public override string ToString() => BitConverter.ToString(bytes).Replace("-", "");
+
+    protected static byte[] ReadRaw(BinaryReader reader) => ByteArray.Instance.Read(reader);
+
+    protected void Write(BinaryWriter writer) => ByteArray.Instance.Write(writer, bytes);
+
+    // Custom BSATN that returns an inline product type with special property name that can be recognised by SpacetimeDB.
+    protected static AlgebraicType GetAlgebraicType(
+        ITypeRegistrar registrar,
+        string wrapperPropertyName
+    ) =>
+        new AlgebraicType.Product(
+            [new(wrapperPropertyName, ByteArray.Instance.GetAlgebraicType(registrar))]
+        );
+}
+
+public record Address : BytesWrapper
+{
+    protected override int SIZE => 16;
+
+    public Address() { }
+
+    private Address(byte[] bytes)
+        : base(bytes) { }
+
+    public static Address? From(byte[] bytes)
     {
-        protected override int SIZE => 16;
-
-        public Address() { }
-
-        private Address(byte[] bytes)
-            : base(bytes) { }
-
-        public static Address? From(byte[] bytes)
+        if (bytes.All(b => b == 0))
         {
-            if (bytes.All(b => b == 0))
-            {
-                return null;
-            }
-            return new(bytes);
+            return null;
         }
-
-        public static Address Random()
-        {
-            var random = new Random();
-            var addr = new Address();
-            random.NextBytes(addr.bytes);
-            return addr;
-        }
-
-        public readonly struct BSATN : IReadWrite<Address>
-        {
-            public Address Read(BinaryReader reader) => new(ReadRaw(reader));
-
-            public void Write(BinaryWriter writer, Address value) => value.Write(writer);
-
-            public AlgebraicType GetAlgebraicType(ITypeRegistrar registrar) =>
-                BytesWrapper.GetAlgebraicType(registrar, "__address_bytes");
-        }
-
-        // This must be explicitly forwarded to base, otherwise record will generate a new implementation.
-        public override string ToString() => base.ToString();
+        return new(bytes);
     }
 
-    public record Identity : BytesWrapper
+    public static Address Random()
     {
-        protected override int SIZE => 32;
-
-        public Identity() { }
-
-        public Identity(byte[] bytes)
-            : base(bytes) { }
-
-        public static Identity From(byte[] bytes) => new(bytes);
-
-        public readonly struct BSATN : IReadWrite<Identity>
-        {
-            public Identity Read(BinaryReader reader) => new(ReadRaw(reader));
-
-            public void Write(BinaryWriter writer, Identity value) => value.Write(writer);
-
-            public AlgebraicType GetAlgebraicType(ITypeRegistrar registrar) =>
-                BytesWrapper.GetAlgebraicType(registrar, "__identity_bytes");
-        }
-
-        // This must be explicitly forwarded to base, otherwise record will generate a new implementation.
-        public override string ToString() => base.ToString();
+        var random = new Random();
+        var addr = new Address();
+        random.NextBytes(addr.bytes);
+        return addr;
     }
+
+    public readonly struct BSATN : IReadWrite<Address>
+    {
+        public Address Read(BinaryReader reader) => new(ReadRaw(reader));
+
+        public void Write(BinaryWriter writer, Address value) => value.Write(writer);
+
+        public AlgebraicType GetAlgebraicType(ITypeRegistrar registrar) =>
+            BytesWrapper.GetAlgebraicType(registrar, "__address_bytes");
+    }
+
+    // This must be explicitly forwarded to base, otherwise record will generate a new implementation.
+    public override string ToString() => base.ToString();
+}
+
+public record Identity : BytesWrapper
+{
+    protected override int SIZE => 32;
+
+    public Identity() { }
+
+    public Identity(byte[] bytes)
+        : base(bytes) { }
+
+    public static Identity From(byte[] bytes) => new(bytes);
+
+    public readonly struct BSATN : IReadWrite<Identity>
+    {
+        public Identity Read(BinaryReader reader) => new(ReadRaw(reader));
+
+        public void Write(BinaryWriter writer, Identity value) => value.Write(writer);
+
+        public AlgebraicType GetAlgebraicType(ITypeRegistrar registrar) =>
+            BytesWrapper.GetAlgebraicType(registrar, "__identity_bytes");
+    }
+
+    // This must be explicitly forwarded to base, otherwise record will generate a new implementation.
+    public override string ToString() => base.ToString();
 }
