3451cab75d4e1c97c3d57dadd2c6e47b48e0c5a7
Ingvar Stepanyan <me@rreverser.com>
2024-07-26 20:49:28 +0100

    Use latest C# in BSATN.Runtime (#1548)

--- crates/bindings-csharp/BSATN.Runtime/BSATN/AlgebraicType.cs
+++ crates/bindings-csharp/BSATN.Runtime/BSATN/AlgebraicType.cs
@@ -1,77 +1,59 @@
-using System;
+namespace SpacetimeDB.BSATN;
 
-namespace SpacetimeDB.BSATN
+public interface ITypeRegistrar
 {
-    public interface ITypeRegistrar
-    {
-        AlgebraicType.Ref RegisterType<T>(Func<AlgebraicType.Ref, AlgebraicType> type);
-    }
+    AlgebraicType.Ref RegisterType<T>(Func<AlgebraicType.Ref, AlgebraicType> type);
+}
 
-    [SpacetimeDB.Type]
-    public partial struct AggregateElement
-    {
-        public string? Name;
-        public AlgebraicType AlgebraicType;
+[SpacetimeDB.Type]
+public partial struct AggregateElement(string? name, AlgebraicType algebraicType)
+{
+    public string? Name = name;
+    public AlgebraicType AlgebraicType = algebraicType;
+}
 
-        public AggregateElement(string? name, AlgebraicType algebraicType)
-        {
-            Name = name;
-            AlgebraicType = algebraicType;
-        }
-    }
+[SpacetimeDB.Type]
+public partial struct MapElement(AlgebraicType key, AlgebraicType value)
+{
+    public AlgebraicType Key = key;
+    public AlgebraicType Value = value;
+}
 
-    [SpacetimeDB.Type]
-    public partial struct MapElement
-    {
-        public AlgebraicType Key;
-        public AlgebraicType Value;
+[SpacetimeDB.Type]
+public partial record BuiltinType
+    : SpacetimeDB.TaggedEnum<(
+        Unit Bool,
+        Unit I8,
+        Unit U8,
+        Unit I16,
+        Unit U16,
+        Unit I32,
+        Unit U32,
+        Unit I64,
+        Unit U64,
+        Unit I128,
+        Unit U128,
+        Unit F32,
+        Unit F64,
+        Unit String,
+        AlgebraicType Array,
+        MapElement Map
+    )> { }
 
-        public MapElement(AlgebraicType key, AlgebraicType value)
-        {
-            Key = key;
-            Value = value;
-        }
-    }
+[SpacetimeDB.Type]
+public partial record AlgebraicType
+    : SpacetimeDB.TaggedEnum<(
+        AggregateElement[] Sum,
+        AggregateElement[] Product,
+        BuiltinType Builtin,
+        int Ref
+    )>
+{
+    public static implicit operator AlgebraicType(BuiltinType builtin) => new Builtin(builtin);
 
-    [SpacetimeDB.Type]
-    public partial record BuiltinType
-        : SpacetimeDB.TaggedEnum<(
-            Unit Bool,
-            Unit I8,
-            Unit U8,
-            Unit I16,
-            Unit U16,
-            Unit I32,
-            Unit U32,
-            Unit I64,
-            Unit U64,
-            Unit I128,
-            Unit U128,
-            Unit F32,
-            Unit F64,
-            Unit String,
-            AlgebraicType Array,
-            MapElement Map
-        )> { }
+    public static readonly AlgebraicType Unit = new Product([]);
 
-    [SpacetimeDB.Type]
-    public partial record AlgebraicType
-        : SpacetimeDB.TaggedEnum<(
-            AggregateElement[] Sum,
-            AggregateElement[] Product,
-            BuiltinType Builtin,
-            int Ref
-        )>
-    {
-        public static implicit operator AlgebraicType(BuiltinType builtin)
-        {
-            return new Builtin(builtin);
-        }
-
-        public static readonly AlgebraicType Unit = new Product(Array.Empty<AggregateElement>());
-
-        // Special AlgebraicType that can be recognised by the SpacetimeDB `generate` CLI as an Option<T>.
-        internal static AlgebraicType MakeOption(AlgebraicType someType) =>
-            new Sum(new AggregateElement[] { new("some", someType), new("none", Unit) });
-    }
+    // Special AlgebraicType that can be recognised by the SpacetimeDB `generate` CLI as an Option<T>.
+    internal static AlgebraicType MakeOption(AlgebraicType someType) =>
+        new Sum([new("some", someType), new("none", Unit)]);
 }
