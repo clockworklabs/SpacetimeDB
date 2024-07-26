769992c4c472cfbc10b3d5f97630fa2131b1581b
Shubham Mishra <shubham@clockworklabs.io>
2024-07-26 15:09:55 +0530

    broadcast schedule table deletion (#1547)

--- crates/core/src/host/scheduler.rs
+++ crates/core/src/host/scheduler.rs
@@ -5,6 +5,7 @@ use anyhow::anyhow;
 use futures::StreamExt;
 use rustc_hash::FxHashMap;
 use sled::transaction::TransactionError;
+use spacetimedb_client_api_messages::energy::EnergyQuanta;
 use spacetimedb_client_api_messages::timestamp::Timestamp;
 use spacetimedb_lib::bsatn::ser::BsatnError;
 use spacetimedb_lib::scheduler::ScheduleAt;
@@ -24,7 +25,10 @@ use crate::db::datastore::traits::IsolationLevel;
 use crate::db::relational_db::RelationalDB;
 use crate::execution_context::ExecutionContext;
 
+use super::module_host::ModuleEvent;
+use super::module_host::ModuleFunctionCall;
 use super::module_host::{CallReducerParams, WeakModuleHost};
+use super::module_host::{DatabaseUpdate, EventStatus};
 use super::{ModuleHost, ReducerArgs, ReducerCallError};
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash)]
@@ -286,6 +290,7 @@ impl SchedulerActor {
         };
 
         let db = module_host.dbic().relational_db.clone();
+        let module_host_clone = module_host.clone();
         let ctx = ExecutionContext::internal(db.address());
 
         let res = tokio::spawn(async move { module_host.call_scheduled_reducer(call_reducer_params).await }).await;
@@ -297,26 +302,8 @@ impl SchedulerActor {
 
             // delete the scheduled reducer row if its not repeated reducer
             Ok(_) | Err(_) => {
-                let mut tx = db.begin_mut_tx(IsolationLevel::Serializable);
-                match get_schedule_row_mut(&ctx, &tx, &db, id) {
-                    Ok(schedule_row) => {
-                        if let Ok(is_repeated) = self.handle_repeated_schedule(&tx, &db, id, &schedule_row) {
-                            if !is_repeated {
-                                let row_ptr = schedule_row.pointer();
-                                db.delete(&mut tx, id.table_id, [row_ptr]);
-                                tx.commit(&ctx);
-                            };
-                        }
-                    }
-                    Err(_) => {
-                        log::debug!(
-                            "table row corresponding to yeild scheduler id not found: tableid {}, schedulerId {}",
-                            id.table_id,
-                            id.schedule_id
-                        );
-                        return;
-                    }
-                };
+                self.delete_scheduled_reducer_row(&ctx, &db, id, module_host_clone)
+                    .await;
             }
         }
 
@@ -342,6 +329,63 @@ impl SchedulerActor {
         } else {
             Ok(false)
         }
+    }
+
+    async fn delete_scheduled_reducer_row(
+        &mut self,
+        ctx: &ExecutionContext,
+        db: &RelationalDB,
+        id: ScheduledReducerId,
+        module_host: ModuleHost,
+    ) {
+        let mut tx = db.begin_mut_tx(IsolationLevel::Serializable);
+
+        match get_schedule_row_mut(ctx, &tx, db, id) {
+            Ok(schedule_row) => {
+                if let Ok(is_repeated) = self.handle_repeated_schedule(&tx, db, id, &schedule_row) {
+                    if is_repeated {
+                        return; // Do not delete entry for repeated reducer
+                    }
+
+                    let row_ptr = schedule_row.pointer();
+                    db.delete(&mut tx, id.table_id, [row_ptr]);
+
+                    commit_and_broadcast_deletion_event(ctx, tx, module_host);
+                }
+            }
+            Err(_) => {
+                log::debug!(
+                    "Table row corresponding to yield scheduler ID not found: table_id {}, scheduler_id {}",
+                    id.table_id,
+                    id.schedule_id
+                );
+            }
+        }
+    }
+}
+
+fn commit_and_broadcast_deletion_event(ctx: &ExecutionContext, tx: MutTxId, module_host: ModuleHost) {
+    let caller_identity = module_host.info().identity;
+
+    let event = ModuleEvent {
+        timestamp: Timestamp::now(),
+        caller_identity,
+        caller_address: None,
+        function_call: ModuleFunctionCall::default(),
+        status: EventStatus::Committed(DatabaseUpdate::default()),
+        //Keeping them 0 as it is internal transaction, not by reducer
+        energy_quanta_used: EnergyQuanta { quanta: 0 },
+        host_execution_duration: Duration::from_millis(0),
+        request_id: None,
+        timer: None,
+    };
+
+    if let Err(e) = module_host
+        .info()
+        .subscriptions
+        .commit_and_broadcast_event(None, event, ctx, tx)
+    {
+        log::error!("Failed to broadcast deletion event: {e:#}");
     }
 }
 
---
+++ smoketests/tests/schedule_reducer.py
@@ -0,0 +1,112 @@
+from .. import Smoketest
+import time
+
+class CancelReducer(Smoketest):
+
+    MODULE_CODE = """
+    use spacetimedb::{duration, println, spacetimedb, spacetimedb_lib::ScheduleAt, ReducerContext};
+
+#[spacetimedb(init)]
+fn init() {
+    let schedule = ScheuledReducerArgs::insert(ScheuledReducerArgs {
+        num: 1,
+        scheduled_id: 0,
+        scheduled_at: duration!(100ms).into(),
+    });
+    ScheuledReducerArgs::delete_by_scheduled_id(&schedule.unwrap().scheduled_id);
+
+    let schedule = ScheuledReducerArgs::insert(ScheuledReducerArgs {
+         num: 2,
+         scheduled_id: 0,
+         scheduled_at: duration!(1000ms).into(),
+     });
+     do_cancel(schedule.unwrap().scheduled_id);
+}
+
+#[spacetimedb(table(public), scheduled(reducer))]
+pub struct ScheuledReducerArgs {
+    num: i32,
+}
+
+#[spacetimedb(reducer)]
+fn do_cancel(schedule_id: u64) {
+    ScheuledReducerArgs::delete_by_scheduled_id(&schedule_id);
+}
+
+#[spacetimedb(reducer)]
+fn reducer(_ctx: ReducerContext, args: ScheuledReducerArgs) {
+    println!("the reducer ran: {}", args.num);
+}
+"""
+
+    def test_cancel_reducer(self):
+        """Ensure cancelling a reducer works"""
+
+        time.sleep(2)
+        logs = "\n".join(self.logs(5))
+        self.assertNotIn("the reducer ran", logs)
+
+
+class SubscribeScheduledTable(Smoketest):
+    MODULE_CODE = """
+use spacetimedb::{println, duration, spacetimedb, Timestamp, spacetimedb_lib::ScheduleAt, ReducerContext};
+
+
+#[spacetimedb(table(public), scheduled(my_reducer))]
+pub struct ScheduledTable {
+    prev: Timestamp,
+}
+
+#[spacetimedb(reducer)]
+fn schedule_reducer() {
+    let _ = ScheduledTable::insert(ScheduledTable { prev: Timestamp::from_micros_since_epoch(0), scheduled_id: 2, scheduled_at: Timestamp::from_micros_since_epoch(0).into(), });
+}
+
+#[spacetimedb(reducer)]
+fn schedule_repeated_reducer() {
+    let _ = ScheduledTable::insert(ScheduledTable { prev: Timestamp::from_micros_since_epoch(0), scheduled_id: 1, scheduled_at: duration!(100ms).into(), });
+}
+
+#[spacetimedb(reducer)]
+pub fn my_reducer(_ctx: ReducerContext, arg: ScheduledTable) {
+    println!("Invoked: ts={:?}, delta={:?}", Timestamp::now(), arg.prev.elapsed());
+}
+"""
+    def test_scheduled_table_subscription(self):
+        """This test deploys a module with a scheduled reducer and check if client receives subscription update for scheduled table entry and deletion of reducer once it ran"""
+        # subscribe to empy ScheduledTable
+        sub = self.subscribe("SELECT * FROM ScheduledTable", n=2)
+        # call a reducer to schedule a reducer
+        self.call("schedule_reducer")
+
+        time.sleep(2)
+        lines = sum(1 for line in self.logs(100) if "Invoked:" in line)
+        # scheduled reducer should be ran by now
+        self.assertEqual(lines, 1)
+
+        row_entry = {'prev': 0, 'scheduled_id': 2, 'scheduled_at': {'Time': 0}}
+        # subscription should have 2 updates, first for row insert in scheduled table and second for row deletion.
+        self.assertEqual(sub(), [{'ScheduledTable': {'deletes': [], 'inserts': [row_entry]}}, {'ScheduledTable': {'deletes': [row_entry], 'inserts': []}}])
+
+
+
+    def test_scheduled_table_subscription_repeated_reducer(self):
+        """This test deploys a module with a  repeated reducer and check if client receives subscription update for scheduled table entry and no delete entry"""
+        # subscribe to emptry ScheduledTable
+        sub = self.subscribe("SELECT * FROM ScheduledTable", n=2)
+        # call a reducer to schedule a reducer
+        self.call("schedule_repeated_reducer")
+
+        time.sleep(2)
+        lines = sum(1 for line in self.logs(100) if "Invoked:" in line)
+        # repeated reducer should have run more than once.
+        self.assertLess(2, lines)
+
+        # scheduling repeated reducer again just to get 2nd subscription update.
+        self.call("schedule_reducer")
+
+        repeated_row_entry = {'prev': 0, 'scheduled_id': 1, 'scheduled_at': {'Interval': 100000}}
+        row_entry = {'prev': 0, 'scheduled_id': 2, 'scheduled_at': {'Time': 0}}
+
+        # subscription should have 2 updates and should not have any deletes
+        self.assertEqual(sub(), [{'ScheduledTable': {'deletes': [], 'inserts': [repeated_row_entry]}}, {'ScheduledTable': {'deletes': [], 'inserts': [row_entry]}}])
