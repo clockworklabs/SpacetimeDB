fa9316ea4662149a3790e38699dec1ed81f29a13
Ingvar Stepanyan <me@rreverser.com>
2024-08-06 14:32:11 +0100

    Enforce style for C# locally and on CI

--- crates/bindings-csharp/Codegen/Module.cs
+++ crates/bindings-csharp/Codegen/Module.cs
@@ -183,7 +183,7 @@ public class Module : IIncrementalGenerator
                 transform: (context, ct) =>
                 {
                     var tableSyntax = (TypeDeclarationSyntax)context.TargetNode;
-                    var table = context.SemanticModel.GetDeclaredSymbol(tableSyntax)!;
+                    var table = context.SemanticModel.GetDeclaredSymbol(tableSyntax, ct)!;
 
                     var fields = GetFields(tableSyntax, table)
                         .Select(f =>
@@ -337,8 +337,7 @@ public class Module : IIncrementalGenerator
                 transform: (context, ct) =>
                 {
                     var methodSyntax = (MethodDeclarationSyntax)context.TargetNode;
-
-                    var method = context.SemanticModel.GetDeclaredSymbol(methodSyntax)!;
+                    var method = context.SemanticModel.GetDeclaredSymbol(methodSyntax, ct)!;
 
                     if (!method.ReturnsVoid)
                     {
