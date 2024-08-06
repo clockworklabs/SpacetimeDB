ce878c805c2199dbf3d23a8b0b8c7767590ad93f
Mazdak Farrokhzad <twingoow@gmail.com>
2024-08-07 00:33:53 +0200

    sql smoketest: refactor (to restore sanity) + adjust for i/u256

--- smoketests/tests/sql.py
+++ smoketests/tests/sql.py
@@ -2,101 +2,130 @@ from .. import Smoketest
 
 class SqlFormat(Smoketest):
     MODULE_CODE = """
-use spacetimedb::{spacetimedb, Identity, sats::{i256, u256}};
+use spacetimedb::{spacetimedb, sats::{i256, u256}};
 
-#[derive(spacetimedb::SpacetimeType)]
-pub struct TupleType {
-    a_b: bool,
-    a_i8: i8,
-    a_i16: i16,
-    a_i32: i32,
-    a_i64: i64,
-    a_i128: i128,
-    a_i256: i256,
-    a_u8: u8,
-    a_u16: u16,
-    a_u32: u32,
-    a_u64: u64,
-    a_u128: u128,
-    a_u256: u256,
-    a_f32: f32,
-    a_f64: f64,
-    a_str: String,
-    a_bytes: Vec<u8>,
+#[derive(Copy, Clone)]
+#[spacetimedb(table)]
+pub struct TInts {
+    i8: i8,
+    i16: i16,
+    i32: i32,
+    i64: i64,
+    i128: i128,
+    i256: i256,
 }
 
 #[spacetimedb(table)]
-pub struct BuiltIn {
-    a_b: bool,
-    a_i8: i8,
-    a_i16: i16,
-    a_i32: i32,
-    a_i64: i64,
-    a_i128: i128,
-    a_i256: i256,
-    a_u8: u8,
-    a_u16: u16,
-    a_u32: u32,
-    a_u64: u64,
-    a_u128: u128,
-    a_u256: u256,
-    a_f32: f32,
-    a_f64: f64,
-    a_str: String,
-    a_bytes: Vec<u8>,
-    a_tuple: TupleType,
+pub struct TIntsTuple {
+    tuple: TInts,
+}
+
+#[derive(Copy, Clone)]
+#[spacetimedb(table)]
+pub struct TUints {
+    u8: u8,
+    u16: u16,
+    u32: u32,
+    u64: u64,
+    u128: u128,
+    u256: u256,
+}
+
+#[spacetimedb(table)]
+pub struct TUintsTuple {
+    tuple: TUints,
+}
+
+#[derive(Clone)]
+#[spacetimedb(table)]
+pub struct TOthers {
+    bool: bool,
+    f32: f32,
+    f64: f64,
+    str: String,
+    bytes: Vec<u8>,
+}
+
+#[spacetimedb(table)]
+pub struct TOthersTuple {
+    tuple: TOthers
 }
 
 #[spacetimedb(reducer)]
 pub fn test() {
-    BuiltIn::insert(BuiltIn {
-        a_b: true,
-        a_i8: -25,
-        a_i16: -3224,
-        a_i32: -23443,
-        a_i64: -2344353,
-        a_i128: -234434897853,
-        a_i256: (-234434897853i128).into(),
-        a_u8: 105,
-        a_u16: 1050,
-        a_u32: 83892,
-        a_u64: 48937498,
-        a_u128: 4378528978889,
-        a_u256: 4378528978889u128.into(),
-        a_f32: 594806.58906,
-        a_f64: -3454353.345389043278459,
-        a_str: "This is spacetimedb".to_string(),
-        a_bytes: vec!(1, 2, 3, 4, 5, 6, 7),
-        a_tuple: TupleType {
-            a_b: true,
-            a_i8: -25,
-            a_i16: -3224,
-            a_i32: -23443,
-            a_i64: -2344353,
-            a_i128: -234434897853,
-            a_i256: (-234434897853i128).into(),
-            a_u8: 105,
-            a_u16: 1050,
-            a_u32: 83892,
-            a_u64: 48937498,
-            a_u128: 4378528978889,
-            a_u256: 4378528978889u128.into(),
-            a_f32: 594806.58906,
-            a_f64: -3454353.345389043278459,
-            a_str: "This is spacetimedb".to_string(),
-            a_bytes: vec!(1, 2, 3, 4, 5, 6, 7),
-        },
-    });
+    let tuple = TInts {
+        i8: -25,
+        i16: -3224,
+        i32: -23443,
+        i64: -2344353,
+        i128: -234434897853,
+        i256: (-234434897853i128).into(),
+    };
+    TInts::insert(tuple);
+    TIntsTuple::insert(TIntsTuple { tuple });
+
+    let tuple = TUints {
+        u8: 105,
+        u16: 1050,
+        u32: 83892,
+        u64: 48937498,
+        u128: 4378528978889,
+        u256: 4378528978889u128.into(),
+    };
+    TUints::insert(tuple);
+    TUintsTuple::insert(TUintsTuple { tuple });
+
+    let tuple = TOthers {
+        bool: true,
+        f32: 594806.58906,
+        f64: -3454353.345389043278459,
+        str: "This is spacetimedb".to_string(),
+        bytes: vec!(1, 2, 3, 4, 5, 6, 7),
+    };
+    TOthers::insert(tuple.clone());
+    TOthersTuple::insert(TOthersTuple { tuple });
 }
 """
+
+    def assertSql(self, sql, expected):
+        self.maxDiff = None
+        sql_out = self.spacetime("sql", self.address, sql)
+        sql_out = "\n".join([line.rstrip() for line in sql_out.splitlines()])
+        expected = "\n".join([line.rstrip() for line in expected.splitlines()])
+        self.assertMultiLineEqual(sql_out, expected)
+
     def test_sql_format(self):
         """This test is designed to test the format of the output of sql queries"""
 
         self.call("test")
-        sql_out = self.spacetime("sql", self.address, "SELECT * FROM BuiltIn")
-        # The spaces after the last column are important
-        self.assertMultiLineEqual(sql_out, """\
- a_b  | a_i8 | a_i16 | a_i32  | a_i64    | a_i128        | a_i256        | a_u8 | a_u16 | a_u32 | a_u64    | a_u128        | a_u128        | a_f32     | a_f64              | a_str                 | a_bytes          | a_tuple
-------+------+-------+--------+----------+---------------+---------------+------+-------+-------+----------+---------------+---------------+-----------+--------------------+-----------------------+------------------+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
- true | -25  | -3224 | -23443 | -2344353 | -234434897853 | -234434897853 | 105  | 1050  | 83892 | 48937498 | 4378528978889 | 4378528978889 | 594806.56 | -3454353.345389043 | "This is spacetimedb" | 0x01020304050607 | (a_b = true, a_i8 = -25, a_i16 = -3224, a_i32 = -23443, a_i64 = -2344353, a_i128 = -234434897853, a_i256 = -234434897853, a_u8 = 105, a_u16 = 1050, a_u32 = 83892, a_u64 = 48937498, a_u128 = 4378528978889, a_u256 = 4378528978889, a_f32 = 594806.56, a_f64 = -3454353.345389043, a_str = "This is spacetimedb", a_bytes = 0x01020304050607)
+
+        self.assertSql("SELECT * FROM TInts", """\
+ i8  | i16   | i32    | i64      | i128          | i256          
+-----+-------+--------+----------+---------------+---------------
+ -25 | -3224 | -23443 | -2344353 | -234434897853 | -234434897853 
+""")
+        self.assertSql("SELECT * FROM TIntsTuple", """\
+ tuple                                                                                             
+---------------------------------------------------------------------------------------------------
+ (i8 = -25, i16 = -3224, i32 = -23443, i64 = -2344353, i128 = -234434897853, i256 = -234434897853)
+""")
+        self.assertSql("SELECT * FROM TUints", """\
+ u8  | u16  | u32   | u64      | u128          | u256          
+-----+------+-------+----------+---------------+---------------
+ 105 | 1050 | 83892 | 48937498 | 4378528978889 | 4378528978889 
+""")
+        self.assertSql("SELECT * FROM TUintsTuple", """\
+ tuple                                                                                           
+-------------------------------------------------------------------------------------------------
+ (u8 = 105, u16 = 1050, u32 = 83892, u64 = 48937498, u128 = 4378528978889, u256 = 4378528978889) 
+""")
+        self.assertSql("SELECT * FROM TOthers", """\
+ bool | f32       | f64                | str                   | bytes            
+------+-----------+--------------------+-----------------------+------------------
+ true | 594806.56 | -3454353.345389043 | "This is spacetimedb" | 0x01020304050607 
+""")
+        self.assertSql("SELECT * FROM TOthersTuple", """\
+ tuple                                                                                                           
+-----------------------------------------------------------------------------------------------------------------
+ (bool = true, f32 = 594806.56, f64 = -3454353.345389043, str = "This is spacetimedb", bytes = 0x01020304050607) 
 """)
