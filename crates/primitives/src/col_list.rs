9b4e9e50b23ee1a39d7e37ff7317691e10c1ae85
James Gilles <jameshgilles@gmail.com>
2024-08-13 16:48:08 -0400

    Remove debug prints

--- crates/primitives/src/col_list.rs
+++ crates/primitives/src/col_list.rs
@@ -646,10 +646,6 @@ mod tests {
 
     #[test]
     fn try_from_iter() {
-        for i in 0..0 {
-            println!("i: {}", i);
-        }
-        println!("{:?}", ColList::try_from_iter(0..0));
         assert!(ColList::try_from_iter(0..0).is_err());
         assert!(ColList::try_from_iter(0..5).is_ok());
         assert!(ColList::try_from_iter([4, 3, 2, 1, 0]).is_ok());
