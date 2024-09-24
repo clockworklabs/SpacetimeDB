747873735bfa43a874e9087e3dc99ce3296dff6b
Zeke Foppa <196249+bfops@users.noreply.github.com>
2024-09-24 14:13:34 -0400

    CLI - Exit when config is malformed (#1669)
    Co-authored-by: Zeke Foppa <bfops@users.noreply.github.com>

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -13,7 +13,7 @@ async fn main() -> Result<(), anyhow::Error> {
     let matches = get_command().get_matches();
     let (cmd, subcommand_args) = matches.subcommand().unwrap();
 
-    let config = Config::load();
+    let config = Config::load()?;
     // Save a default version to disk
     config.save();
 
