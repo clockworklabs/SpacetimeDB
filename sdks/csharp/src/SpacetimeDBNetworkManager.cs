2d20acd06e723575c7d198d93f360bd6267298bf
Ingvar Stepanyan <me@rreverser.com>
2024-10-04 01:35:51 +0100

    Fix UnityNetworkManager (#141)
    This got broken when we switched to a new DbConnection API.
    
    Keep track of all the active connections and update/destroy them from a
    singleton game object. Fixes #134.
    
    Marked as a draft for now, because it's untested and because we're not
    yet sure that singleton for all connections as requested is the right
    approach.
    
    ## Description of Changes
    *Describe what has been changed, any new features or bug fixes*
    
    ## API
    
     - [ ] This is an API breaking change to the SDK
    
    *If the API is breaking, please state below what will break*
    
    
    ## Requires SpacetimeDB PRs
    *List any PRs here that are required for this SDK change to work*
    
    ---------
    
    Co-authored-by: John Detter <4099508+jdetter@users.noreply.github.com>
    Co-authored-by: John Detter <no-reply@boppygames.gg>

---
+++ sdks/csharp/src/SpacetimeDBNetworkManager.cs
@@ -0,0 +1,43 @@
+#if UNITY_5_3_OR_NEWER
+using System;
+using System.Collections.Generic;
+using SpacetimeDB;
+using UnityEngine;
+
+namespace SpacetimeDB
+{
+	// This class is only used in Unity projects.
+	// Attach this to a gameobject in your scene to use SpacetimeDB.
+	public class SpacetimeDBNetworkManager : MonoBehaviour
+	{
+		private static bool _alreadyInitialized;
+
+		public void Awake()
+		{
+			// Ensure that users don't create several UnityNetworkManager instances.
+			// We're using a global (static) list of active connections and we don't want several instances to walk over it several times.
+			if (_alreadyInitialized)
+			{
+				throw new InvalidOperationException("UnityNetworkManager is a singleton and should only be attached once.");
+			}
+			else
+			{
+				_alreadyInitialized = true;
+			}
+		}
+
+		internal static HashSet<IDbConnection> ActiveConnections = new();
+
+		private void ForEachConnection(Action<IDbConnection> action)
+		{
+			foreach (var conn in ActiveConnections)
+			{
+				action(conn);
+			}
+		}
+
+		private void Update() => ForEachConnection(conn => conn.FrameTick());
+		private void OnDestroy() => ForEachConnection(conn => conn.Disconnect());
+	}
+}
+#endif
