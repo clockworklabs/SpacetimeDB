6f1b24adcbab6a5076d9197268ff533b382e2b99
Zeke Foppa <bfops@users.noreply.github.com>
2024-10-08 14:39:42 -0700

    reduce repro

--- demo/Blackholio/server/src/lib.rs
+++ demo/Blackholio/server/src/lib.rs
@@ -9,7 +9,6 @@ pub struct Entity {
     #[primary_key]
     pub id: u32,
     pub position: Vector2,
-    pub mass: u32,
 }
 
 #[spacetimedb::table(name = food, public)]
@@ -27,28 +26,19 @@ pub struct Vector2 {
 #[spacetimedb::table(name = spawn_food_timer, scheduled(spawn_food))]
 pub struct SpawnFoodTimer {}
 
-const FOOD_MASS_MIN: u32 = 2;
-const FOOD_MASS_MAX: u32 = 4;
-
-fn mass_to_radius(mass: u32) -> f32 {
-    (mass as f32).sqrt()
-}
-
 #[spacetimedb::reducer]
 pub fn spawn_food(ctx: &ReducerContext, _timer: SpawnFoodTimer) -> Result<(), String> {
     let mut food_count = ctx.db.food().count();
 
     while food_count < TARGET_FOOD_COUNT as u64 {
         let mut rng = ctx.rng();
-        let food_mass = rng.gen_range(FOOD_MASS_MIN..FOOD_MASS_MAX);
         let world_size = 1000;
-        let food_radius = mass_to_radius(food_mass);
+        let food_radius: f32 = 2.0;
         let x = rng.gen_range(food_radius..world_size as f32 - food_radius);
         let y = rng.gen_range(food_radius..world_size as f32 - food_radius);
         let entity = ctx.db.entity().try_insert(Entity {
             id: 0,
             position: Vector2 { x, y },
-            mass: food_mass
         })?;
         ctx.db.food().try_insert(Food { entity_id: entity.id })?;
         food_count += 1;
