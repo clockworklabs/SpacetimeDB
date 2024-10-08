32e1a6c89a3fcf4d3c0539879b469d01ea8dd69a
Mazdak Farrokhzad <twingoow@gmail.com>
2024-10-08 18:43:48 +0200

    add `ClientMessage::CallReducer.flags` with `NoSuccessNotify` as variant

--- crates/sdk/tests/test-client/src/module_bindings/insert_pk_identity_reducer.rs
+++ crates/sdk/tests/test-client/src/module_bindings/insert_pk_identity_reducer.rs
@@ -70,3 +70,25 @@ impl insert_pk_identity for super::RemoteReducers {
             .remove_on_reducer::<InsertPkIdentity>("insert_pk_identity", callback.0)
     }
 }
+
+#[allow(non_camel_case_types)]
+#[doc(hidden)]
+/// Extension trait for setting the call-flags for the reducer `insert_pk_identity`.
+///
+/// Implemented for [`super::SetReducerFlags`].
+///
+/// This type is currently unstable and may be removed without a major version bump.
+/// The ability to send over call-reducer flags in the protocol will however remain.
+pub trait set_flags_for_insert_pk_identity {
+    /// Set the call-reducer flags for the reducer `insert_pk_identity` to `flags`.
+    ///
+    /// This type is currently unstable and may be removed without a major version bump.
+    /// The ability to send over call-reducer flags in the protocol will however remain.
+    fn insert_pk_identity(&self, flags: __ws::CallReducerFlags);
+}
+
+impl set_flags_for_insert_pk_identity for super::SetReducerFlags {
+    fn insert_pk_identity(&self, flags: __ws::CallReducerFlags) {
+        self.imp.set_call_reducer_flags("insert_pk_identity", flags);
+    }
+}
--- crates/sdk/tests/test-client/src/module_bindings/update_pk_string_reducer.rs
+++ crates/sdk/tests/test-client/src/module_bindings/update_pk_string_reducer.rs
@@ -69,3 +69,25 @@ impl update_pk_string for super::RemoteReducers {
             .remove_on_reducer::<UpdatePkString>("update_pk_string", callback.0)
     }
 }
+
+#[allow(non_camel_case_types)]
+#[doc(hidden)]
+/// Extension trait for setting the call-flags for the reducer `update_pk_string`.
+///
+/// Implemented for [`super::SetReducerFlags`].
+///
+/// This type is currently unstable and may be removed without a major version bump.
+/// The ability to send over call-reducer flags in the protocol will however remain.
+pub trait set_flags_for_update_pk_string {
+    /// Set the call-reducer flags for the reducer `update_pk_string` to `flags`.
+    ///
+    /// This type is currently unstable and may be removed without a major version bump.
+    /// The ability to send over call-reducer flags in the protocol will however remain.
+    fn update_pk_string(&self, flags: __ws::CallReducerFlags);
+}
+
+impl set_flags_for_update_pk_string for super::SetReducerFlags {
+    fn update_pk_string(&self, flags: __ws::CallReducerFlags) {
+        self.imp.set_call_reducer_flags("update_pk_string", flags);
+    }
+}
