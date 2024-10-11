869e48070dc17ccfaacaae1827e3a6b0550baf53
Zeke Foppa <bfops@users.noreply.github.com>
2024-10-11 10:08:49 -0700

    [bfops/cli-server]: add smoketest

--- smoketests/tests/servers.py
+++ smoketests/tests/servers.py
@@ -23,3 +23,15 @@ class Servers(Smoketest):
 
         out = self.spacetime("server", "fingerprint", "localhost")
         self.assertIn("Fingerprint is unchanged for server localhost", out)
+
+    def edit_server(self):
+        """Verify that we can edit server configurations"""
+
+        out = self.spacetime("server", "add", "--url", "https://foo.com", "foo", "--no-fingerprint")
+        out = self.spacetime("server", "edit", "--url", "https://testnet.spacetimedb.com", "--new-name", "testnet", "--no-fingerprint")
+        self.assertEqual(extract_field(out, "Host:"), "testnet.spacetimedb.com")
+        self.assertEqual(extract_field(out, "Protocol:"), "https")
+
+        servers = self.spacetime("server", "list")
+        self.assertRegex(servers, re.compile(r"^\s*testnet\.spacetimedb\.com\s+https\s+testnet\s*$", re.M))
+        self.assertRegex(servers, re.compile(r"^\s*\*\*\*\s+127\.0\.0\.1:3000\s+http\s+localhost\s*$", re.M))
