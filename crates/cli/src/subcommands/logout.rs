88c7969213eb47276bd7105351b5c4f1b7236711
Zeke Foppa <bfops@users.noreply.github.com>
2024-10-31 10:08:31 -0700

    [bfops/cli-login-2]: logout

--- crates/cli/src/subcommands/logout.rs
+++ crates/cli/src/subcommands/logout.rs
@@ -1,12 +1,31 @@
 use crate::Config;
-use clap::{ArgMatches, Command};
+use clap::{Arg, ArgMatches, Command};
+use reqwest::Url;
 
 pub fn cli() -> Command {
-    Command::new("logout")
+    Command::new("logout").arg(
+        Arg::new("auth-host")
+            .long("auth-host")
+            .default_value("https://spacetimedb.com")
+            .help("Log out from a custom auth server"),
+    )
 }
 
-pub async fn exec(mut config: Config, _args: &ArgMatches) -> Result<(), anyhow::Error> {
+pub async fn exec(mut config: Config, args: &ArgMatches) -> Result<(), anyhow::Error> {
+    let host: &String = args.get_one("auth-host").unwrap();
+    let host = Url::parse(host)?;
+
+    if let Some(web_session_id) = config.web_session_id() {
+        let client = reqwest::Client::new();
+        client
+            .post(host.join("auth/cli/logout")?)
+            .header("Authorization", format!("Bearer {}", web_session_id))
+            .send()
+            .await?;
+    }
+
     config.clear_login_tokens();
     config.save();
+
     Ok(())
 }
