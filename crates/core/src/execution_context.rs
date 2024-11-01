3b2e9a48bc7509681bd6fcfeb3e0d8cb78efaf02
joshua-spacetime <josh@clockworklabs.io>
2024-11-01 12:47:10 -0700

    chore: remove dead metrics code

--- crates/core/src/execution_context.rs
+++ crates/core/src/execution_context.rs
@@ -1,100 +1,11 @@
 use std::sync::Arc;
 
-use crate::db::db_metrics::DB_METRICS;
 use bytes::Bytes;
 use derive_more::Display;
-use parking_lot::RwLock;
 use spacetimedb_client_api_messages::timestamp::Timestamp;
 use spacetimedb_commitlog::{payload::txdata, Varchar};
 use spacetimedb_lib::{Address, Identity};
-use spacetimedb_primitives::TableId;
 use spacetimedb_sats::bsatn;
-
-pub enum MetricType {
-    IndexSeeks,
-    KeysScanned,
-    RowsFetched,
-}
-
-#[derive(Default, Clone)]
-struct BufferMetric {
-    pub table_id: TableId,
-    pub index_seeks: u64,
-    pub keys_scanned: u64,
-    pub rows_fetched: u64,
-    pub cache_table_name: String,
-}
-
-impl BufferMetric {
-    pub fn inc_by(&mut self, ty: MetricType, val: u64) {
-        match ty {
-            MetricType::IndexSeeks => {
-                self.index_seeks += val;
-            }
-            MetricType::KeysScanned => {
-                self.keys_scanned += val;
-            }
-            MetricType::RowsFetched => {
-                self.rows_fetched += val;
-            }
-        }
-    }
-}
-
-impl BufferMetric {
-    pub fn new(table_id: TableId, table_name: String) -> Self {
-        Self {
-            table_id,
-            cache_table_name: table_name,
-            ..Default::default()
-        }
-    }
-}
-
-#[derive(Default, Clone)]
-pub struct Metrics(Vec<BufferMetric>);
-
-impl Metrics {
-    pub fn inc_by<F: FnOnce() -> String>(&mut self, table_id: TableId, ty: MetricType, val: u64, get_table_name: F) {
-        if let Some(metric) = self.0.iter_mut().find(|x| x.table_id == table_id) {
-            metric.inc_by(ty, val);
-        } else {
-            let table_name = get_table_name();
-            let mut metric = BufferMetric::new(table_id, table_name);
-            metric.inc_by(ty, val);
-            self.0.push(metric);
-        }
-    }
-
-    pub fn table_exists(&self, table_id: TableId) -> bool {
-        self.0.iter().any(|x| x.table_id == table_id)
-    }
-
-    #[allow(dead_code)]
-    fn flush(&mut self, workload: &WorkloadType, database_identity: &Identity, reducer: &str) {
-        macro_rules! flush_metric {
-            ($db_metric:expr, $metric:expr, $metric_field:ident) => {
-                if $metric.$metric_field > 0 {
-                    $db_metric
-                        .with_label_values(
-                            workload,
-                            database_identity,
-                            reducer,
-                            &$metric.table_id.0,
-                            &$metric.cache_table_name,
-                        )
-                        .inc_by($metric.$metric_field);
-                }
-            };
-        }
-
-        self.0.iter().for_each(|metric| {
-            flush_metric!(DB_METRICS.rdb_num_index_seeks, metric, index_seeks);
-            flush_metric!(DB_METRICS.rdb_num_keys_scanned, metric, keys_scanned);
-            flush_metric!(DB_METRICS.rdb_num_rows_fetched, metric, rows_fetched);
-        });
-    }
-}
 
 /// Represents the context under which a database runtime method is executed.
 /// In particular it provides details about the currently executing txn to runtime operations.
@@ -107,8 +18,6 @@ pub struct ExecutionContext {
     reducer: Option<ReducerContext>,
     /// The type of workload that is being executed.
     workload: WorkloadType,
-    /// The Metrics to be reported for this transaction.
-    pub metrics: Arc<RwLock<Metrics>>,
 }
 
 /// If an [`ExecutionContext`] is a reducer context, describes the reducer.
@@ -221,7 +130,6 @@ impl ExecutionContext {
             database_identity,
             reducer,
             workload,
-            metrics: <_>::default(),
         }
     }
 
@@ -291,14 +199,5 @@ impl ExecutionContext {
     #[inline]
     pub fn workload(&self) -> WorkloadType {
         self.workload
-    }
-}
-
-impl Drop for ExecutionContext {
-    fn drop(&mut self) {
-        let workload = self.workload;
-        let database = self.database_identity;
-        let reducer = self.reducer_name();
-        self.metrics.write().flush(&workload, &database, reducer);
     }
 }
