cdeeca7835701c58f4727e92b94203c5bb54cc45
Zeke Foppa <bfops@users.noreply.github.com>
2024-11-15 07:22:38 -0800

    repro script

--- demo/Blackholio/repro.sh
+++ demo/Blackholio/repro.sh
@@ -2,17 +2,19 @@
 
 set -ueo pipefail
 
+SERVER=${1-local}
+
 cd "$(readlink -f "$(dirname "$0")")"
 cd server
 
-echo >/dev/stderr "Publishing.."
-spacetime publish -s local circle-test --delete-data --yes
+#echo >/dev/stderr "Publishing.."
+#spacetime publish -s "${SERVER}" circle-test --delete-data --yes
 
 echo >/dev/stderr "Updating count.."
 sed -i'' -e 's/\<600\>/6000/' src/lib.rs
 
 echo >/dev/stderr "Republishing.."
-if spacetime publish -s local circle-test --build-options="--skip-println-checks"; then
+if spacetime publish -s "${SERVER}" circle-test --build-options="--skip-println-checks"; then
   echo "---- THIS DID NOT FAIL!!!!! ----"
 fi
 
