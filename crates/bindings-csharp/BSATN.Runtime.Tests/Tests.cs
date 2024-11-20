d17502cac5a3b7508e0a9a810faaf4a31cb2aeac
James Gilles <jameshgilles@gmail.com>
2024-11-20 13:06:42 -0500

    Final comment addressed

--- crates/bindings-csharp/BSATN.Runtime.Tests/Tests.cs
+++ crates/bindings-csharp/BSATN.Runtime.Tests/Tests.cs
@@ -14,25 +14,8 @@ public static class BSATNRuntimeTests
         Assert.NotNull(addr);
         Assert.Equal(addr.ToString(), str);
 
-        byte[] bytes =
-        [
-            0x00,
-            0x11,
-            0x22,
-            0x33,
-            0x44,
-            0x55,
-            0x66,
-            0x77,
-            0x88,
-            0x99,
-            0xaa,
-            0xbb,
-            0xcc,
-            0xdd,
-            0xee,
-            0xff,
-        ];
+        var bytes = Convert.FromHexString(str);
+
         var addr2 = Address.FromBigEndian(bytes);
         Assert.Equal(addr2, addr);
 
