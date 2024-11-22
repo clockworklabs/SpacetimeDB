62cbd0733790d8a03ca117078806aa0fb1c960ef
James Gilles <jameshgilles@gmail.com>
2024-11-22 15:36:44 -0500

    Update to comply with new requirements

--- modules/sdk-test/src/lib.rs
+++ modules/sdk-test/src/lib.rs
@@ -9,7 +9,7 @@
 use anyhow::{Context, Result};
 use spacetimedb::{
     sats::{i256, u256},
-    Address, Identity, ReducerContext, SpacetimeType, Table,
+    Address, Identity, ReducerContext, ScheduleAt, SpacetimeType, Table,
 };
 
 #[derive(SpacetimeType)]
@@ -650,6 +650,11 @@ pub struct ScheduledTable {
     #[scheduled_at]
     scheduled_at: spacetimedb::ScheduleAt,
     text: String,
+    #[primary_key]
+    #[auto_inc]
+    scheduled_id: u64,
+    #[scheduled_at]
+    scheduled_at: ScheduleAt,
 }
 
 #[spacetimedb::reducer]
