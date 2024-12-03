1f9572663444397abd468a00a21280c18859fc57
Zeke Foppa <bfops@users.noreply.github.com>
2024-12-03 11:02:31 -0800

    stopwatch

--- demo/Blackholio/server/src/lib.rs
+++ demo/Blackholio/server/src/lib.rs
@@ -125,7 +125,7 @@ fn elastic(v1: Vector2, v2: Vector2, m1: f32, m2: f32) -> (Vector2, Vector2) {
 
 #[spacetimedb::reducer]
 pub fn move_all_players(ctx: &ReducerContext, _timer: MoveAllPlayersTimer) -> Result<(), String> {
-//    let span = spacetimedb::log_stopwatch::LogStopwatch::new("tick");
+    let span = spacetimedb::log_stopwatch::LogStopwatch::new("tick");
     let world_size = ctx.db.config().id().find(0).ok_or("Config not found")?.world_size;
     for mut circle in ctx.db.circle().iter() {
         let Some(mut circle_entity) = ctx.db.entity().id().find(&circle.entity_id) else {
@@ -153,40 +153,42 @@ pub fn move_all_players(ctx: &ReducerContext, _timer: MoveAllPlayersTimer) -> Re
         ctx.db.entity().id().update(circle_entity);
     }
 
-    for (idx, mut circle) in ctx.db.circle().iter().enumerate() {
-        let Some(mut circle_entity) = ctx.db.entity().id().find(&circle.entity_id) else { continue; };
-        // Check collisions
-        // let span = spacetimedb::time_span::Span::start("collisions");
-        for mut other_circle in ctx.db.circle().iter().skip(idx+1) {
-            let Some(mut other_entity) = ctx.db.entity().id().find(&other_circle.entity_id) else { continue; };
-            let overlap_vector = Vector2 {
-                x: circle_entity.position.x - other_entity.position.x,
-                y: circle_entity.position.y - other_entity.position.y,
-            };
-            let overlap_length = mass_to_radius(circle_entity.mass) + mass_to_radius(other_entity.mass) - (overlap_vector.x*overlap_vector.x + overlap_vector.y*overlap_vector.y).sqrt();
-            if overlap_length <= 0.0 {
-                continue;
+    {
+        let span = spacetimedb::log_stopwatch::LogStopwatch::new("collisions");
+        for (idx, mut circle) in ctx.db.circle().iter().enumerate() {
+            let Some(mut circle_entity) = ctx.db.entity().id().find(&circle.entity_id) else { continue; };
+            // Check collisions
+            for mut other_circle in ctx.db.circle().iter().skip(idx+1) {
+                let Some(mut other_entity) = ctx.db.entity().id().find(&other_circle.entity_id) else { continue; };
+                let overlap_vector = Vector2 {
+                    x: circle_entity.position.x - other_entity.position.x,
+                    y: circle_entity.position.y - other_entity.position.y,
+                };
+                let overlap_length = mass_to_radius(circle_entity.mass) + mass_to_radius(other_entity.mass) - (overlap_vector.x*overlap_vector.x + overlap_vector.y*overlap_vector.y).sqrt();
+                if overlap_length <= 0.0 {
+                    continue;
+                }
+                let overlap_vector = overlap_vector.normalize();
+
+                circle_entity.position.x += overlap_vector.x * overlap_length / 2.0;
+                circle_entity.position.y += overlap_vector.y * overlap_length / 2.0;
+                other_entity.position.x -= overlap_vector.x * overlap_length / 2.0;
+                other_entity.position.y -= overlap_vector.y * overlap_length / 2.0;
+
+                let (circle_velocity, other_circle_velocity) = elastic(circle.velocity, other_circle.velocity, circle_entity.mass as f32, other_entity.mass as f32);
+                circle.velocity = circle_velocity;
+                other_circle.velocity = other_circle_velocity;
+
+                ctx.db.circle().entity_id().update(other_circle);
+                ctx.db.entity().id().update(other_entity);
+
             }
-            let overlap_vector = overlap_vector.normalize();
-
-            circle_entity.position.x += overlap_vector.x * overlap_length / 2.0;
-            circle_entity.position.y += overlap_vector.y * overlap_length / 2.0;
-            other_entity.position.x -= overlap_vector.x * overlap_length / 2.0;
-            other_entity.position.y -= overlap_vector.y * overlap_length / 2.0;
-
-            let (circle_velocity, other_circle_velocity) = elastic(circle.velocity, other_circle.velocity, circle_entity.mass as f32, other_entity.mass as f32);
-            circle.velocity = circle_velocity;
-            other_circle.velocity = other_circle_velocity;
-
-            ctx.db.circle().entity_id().update(other_circle);
-            ctx.db.entity().id().update(other_entity);
-
+            ctx.db.circle().entity_id().update(circle);
+            ctx.db.entity().id().update(circle_entity);
         }
-        ctx.db.circle().entity_id().update(circle);
-        ctx.db.entity().id().update(circle_entity);
-        // span.end();
+        span.end();
     }
 
-//    span.end();
+    span.end();
     Ok(())
 }
