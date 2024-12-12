47681f010463a9df5ce19e635f999bd0b242ab21
James Gilles <jameshgilles@gmail.com>
2024-12-12 17:34:48 -0500

    Move comments

--- crates/sats/src/de.rs
+++ crates/sats/src/de.rs
@@ -14,8 +14,7 @@ use core::marker::PhantomData;
 use smallvec::SmallVec;
 use std::borrow::Borrow;
 
-/// A **data format** that can deserialize any data structure supported by
-/// the SpacetimeDB Algebraic Type System.
+/// A **data format** that can deserialize any data structure supported by SATS.
 ///
 /// The `Deserializer` trait in SATS performs the same function as [`serde::Deserializer`] in [`serde`].
 /// See the documentation of [`serde::Deserializer`] for more information of the data model.
@@ -27,9 +26,6 @@ use std::borrow::Borrow;
 /// but rather, this functionality is split up into more targeted traits such as `SumVisitor<'de>`.
 ///
 /// The lifetime `'de` allows us to deserialize lifetime-generic types in a zero-copy fashion.
-///
-/// Do not manually implement this trait unless you know what you are doing.
-/// Incorrect implementations are safe, but can result in data loss.
 ///
 /// [`serde::Deserializer`]: ::serde::Deserializer
 /// [`serde`]: https://crates.io/crates/serde
@@ -546,7 +542,7 @@ pub trait DeserializeSeed<'de> {
 use crate::de::impls::BorrowedSliceVisitor;
 pub use spacetimedb_bindings_macro::Deserialize;
 
-/// A **datastructure** that can be deserialized from any data format supported by SATS.
+/// A **data structure** that can be deserialized from any data format supported by the SpacetimeDB Algebraic Type System.
 ///
 /// In most cases, implementations of `Deserialize` may be `#[derive(Deserialize)]`d.
 ///
@@ -554,6 +550,9 @@ pub use spacetimedb_bindings_macro::Deserialize;
 /// See the documentation of [`serde::Deserialize`] for more information of the data model.
 ///
 /// The lifetime `'de` allows us to deserialize lifetime-generic types in a zero-copy fashion.
+///
+/// Do not manually implement this trait unless you know what you are doing.
+/// Incorrect implementations are safe, but can result in data loss.
 ///
 /// [`serde::Deserialize`]: ::serde::Deserialize
 /// [`serde`]: https://crates.io/crates/serde
