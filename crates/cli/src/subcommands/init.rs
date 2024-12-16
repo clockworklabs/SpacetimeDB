f2b347ae647bf498c4e98d6c9b402d2030475c7b
Ingvar Stepanyan <me@rreverser.com>
2024-12-16 13:51:24 +0000

    Don't reuse global.json

--- crates/cli/src/subcommands/init.rs
+++ crates/cli/src/subcommands/init.rs
@@ -178,8 +178,7 @@ pub async fn exec_init_csharp(args: &ArgMatches) -> anyhow::Result<()> {
         (include_str!("project/csharp/StdbModule._csproj"), "StdbModule.csproj"),
         (include_str!("project/csharp/Lib._cs"), "Lib.cs"),
         (include_str!("project/csharp/_gitignore"), ".gitignore"),
-        // Reuse global.json from modules instead of a separate template to ensure that we stay in sync.
-        (include_str!("../../../../modules/global.json"), "global.json"),
+        (include_str!("project/csharp/global._json"), "global.json"),
     ];
 
     // Check all dependencies
