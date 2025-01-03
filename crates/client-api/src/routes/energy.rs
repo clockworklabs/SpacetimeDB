d852428af2da2e104c4654f33e72a73be0bfc3c9
Noa <coolreader18@gmail.com>
2025-01-03 15:23:36 -0600

    Update axum

--- crates/client-api/src/routes/energy.rs
+++ crates/client-api/src/routes/energy.rs
@@ -131,7 +131,7 @@ where
 {
     use axum::routing::get;
     axum::Router::new().route(
-        "/:identity",
+        "/{identity}",
         get(get_energy_balance::<S>)
             .put(set_energy_balance::<S>)
             .post(add_energy::<S>),
