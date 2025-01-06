012b69e31a174bda101590e58ecd11bc30f1b734
Zeke Foppa <bfops@users.noreply.github.com>
2025-01-06 09:12:52 -0800

    [bfops/cli-fix-energy-balance]: fix smoketest

--- smoketests/tests/energy.py
+++ smoketests/tests/energy.py
@@ -3,11 +3,11 @@ import time
 
 class EnergyFlow(Smoketest):
 
-    def test_new_user_flow(self):
+    def test_energy_balance(self):
         """Test getting energy balance."""
 
         self.new_identity()
         self.publish_module()
 
         out = self.spacetime("energy", "balance")
-        self.assertEqual(out, '{"balance":"1"}')
+        self.assertIn('{"balance":"', out)
