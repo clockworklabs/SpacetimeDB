9ddd85f2eb99b9e74f2d9da27e7896f6aca4e98d
Tyler Cloutier <cloutiertyler@aol.com>
2025-01-14 23:40:35 -0500

    Fixed the tsconfig issue (it was not actually using the tsconfig)

--- docs/docs/nav.js
+++ docs/docs/nav.js
@@ -1,23 +1,10 @@
-"use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-Object.defineProperty(exports, "__esModule", { value: true });
 function page(title, slug, path, props) {
-    return __assign({ type: 'page', path: path, slug: slug, title: title }, props);
+    return { type: 'page', path, slug, title, ...props };
 }
 function section(title) {
-    return { type: 'section', title: title };
+    return { type: 'section', title };
 }
-var nav = {
+const nav = {
     items: [
         section('Intro'),
         page('Overview', 'index', 'index.md'), // TODO(BREAKING): For consistency & clarity, 'index' slug should be renamed 'intro'?
@@ -62,4 +49,4 @@ var nav = {
         page('SQL Reference', 'sql', 'sql/index.md'),
     ],
 };
-exports.default = nav;
+export default nav;
