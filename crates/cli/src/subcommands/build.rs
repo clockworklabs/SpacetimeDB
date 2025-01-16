92f14a1692317a614811f62a87451fb09edb56ea
Zeke Foppa <bfops@users.noreply.github.com>
2025-01-16 10:27:46 -0800

    [bfops/replace-clippy]: make --lint-dir '' work

--- crates/cli/src/subcommands/build.rs
+++ crates/cli/src/subcommands/build.rs
@@ -1,6 +1,7 @@
 use crate::Config;
 use clap::ArgAction::SetTrue;
 use clap::{Arg, ArgMatches};
+use std::ffi::OsString;
 use std::path::{Path, PathBuf};
 
 pub fn cli() -> clap::Command {
@@ -17,7 +18,7 @@ pub fn cli() -> clap::Command {
         .arg(
             Arg::new("lint_dir")
                 .long("lint-dir")
-                .value_parser(clap::value_parser!(PathBuf))
+                .value_parser(clap::value_parser!(OsString))
                 .default_value("src")
                 .help("The directory to lint for nonfunctional print statements. If set to the empty string, skips linting.")
         )
@@ -32,7 +33,12 @@ pub fn cli() -> clap::Command {
 
 pub async fn exec(_config: Config, args: &ArgMatches) -> Result<PathBuf, anyhow::Error> {
     let project_path = args.get_one::<PathBuf>("project_path").unwrap();
-    let lint_dir = args.get_one::<PathBuf>("lint_dir").unwrap();
+    let lint_dir = args.get_one::<OsString>("lint_dir").unwrap();
+    let lint_dir = if lint_dir.is_empty() {
+        None
+    } else {
+        Some(PathBuf::from(lint_dir))
+    };
     let build_debug = args.get_flag("debug");
 
     // Create the project path, or make sure the target project path is empty.
@@ -50,12 +56,7 @@ pub async fn exec(_config: Config, args: &ArgMatches) -> Result<PathBuf, anyhow:
         ));
     }
 
-    let lint_dir = if lint_dir.as_os_str().is_empty() {
-        None
-    } else {
-        Some(lint_dir.as_path())
-    };
-    let bin_path = crate::tasks::build(project_path, lint_dir, build_debug)?;
+    let bin_path = crate::tasks::build(project_path, lint_dir.as_ref().map(|p| p.as_path()), build_debug)?;
     println!("Build finished successfully.");
 
     Ok(bin_path)
