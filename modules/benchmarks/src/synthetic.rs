c6a5f78f9bbe063cc63c2609f3277b510f1c0110
Mario Alejandro Montoya Cortés <mamcx@elmalabarista.com>
2025-01-21 15:09:50 -0500

    Generate fake data for further analysis

--- modules/benchmarks/src/synthetic.rs
+++ modules/benchmarks/src/synthetic.rs
@@ -23,9 +23,16 @@
 //! Obviously more could be added...
 #![allow(non_camel_case_types)]
 #![allow(clippy::too_many_arguments)]
-use spacetimedb::{log, ReducerContext, Table};
+use fake::faker::address::raw::{CityName, CountryName, StateName, ZipCode};
+use fake::faker::internet::raw::{Password, SafeEmail};
+use fake::faker::lorem::raw::{Paragraph, Words};
+use fake::faker::name::raw::*;
+use fake::faker::phone_number::raw::CellNumber;
+use fake::locales::EN;
+use fake::{Fake, Faker};
+use spacetimedb::rand::Rng;
+use spacetimedb::{log, Address, Identity, ReducerContext, SpacetimeType, StdbRng, Table};
 use std::hint::black_box;
-
 // ---------- schemas ----------
 
 #[spacetimedb::table(name = unique_0_u32_u64_str)]
@@ -76,6 +83,151 @@ pub struct btree_each_column_u32_u64_u64_t {
     x: u64,
     #[index(btree)]
     y: u64,
+}
+
+// Tables for data generation loading tests
+
+#[derive(SpacetimeType)]
+pub enum Load {
+    Tiny,
+    Small,
+    Medium,
+    Large,
+}
+
+#[derive(SpacetimeType)]
+pub enum Index {
+    One,
+    Many,
+}
+
+#[spacetimedb::table(name = tiny_rows)]
+pub struct tiny_rows_t {
+    #[index(btree)]
+    id: u8,
+}
+
+#[spacetimedb::table(name = small_rows)]
+pub struct small_rows_t {
+    #[index(btree)]
+    id: u64,
+    x: u64,
+    y: u64,
+}
+
+#[spacetimedb::table(name = small_btree_each_column_rows)]
+pub struct small_rows_btree_each_column_t {
+    #[index(btree)]
+    id: u64,
+    #[index(btree)]
+    x: u64,
+    #[index(btree)]
+    y: u64,
+}
+
+#[spacetimedb::table(name = medium_var_rows)]
+pub struct medium_var_rows_t {
+    #[index(btree)]
+    id: u64,
+    name: String,
+    email: String,
+    password: String,
+    identity: Identity,
+    address: Address,
+    pos: Vec<u64>,
+}
+
+#[spacetimedb::table(name = medium_var_rows_btree_each_column)]
+pub struct medium_var_rows_btree_each_column_t {
+    #[index(btree)]
+    id: u64,
+    #[index(btree)]
+    name: String,
+    #[index(btree)]
+    email: String,
+    #[index(btree)]
+    password: String,
+    #[index(btree)]
+    identity: Identity,
+    #[index(btree)]
+    address: Address,
+    #[index(btree)]
+    pos: Vec<u64>,
+}
+
+#[spacetimedb::table(name = large_var_rows)]
+pub struct large_var_rows_t {
+    #[index(btree)]
+    id: u128,
+    invoice_code: String,
+    status: String,
+    customer: Identity,
+    company: Address,
+    user_name: String,
+
+    price: f64,
+    cost: f64,
+    discount: f64,
+    taxes: Vec<f64>,
+    tax_total: f64,
+    sub_total: f64,
+    total: f64,
+
+    country: String,
+    state: String,
+    city: String,
+    zip_code: Option<String>,
+    phone: String,
+
+    notes: String,
+    tags: Option<Vec<String>>,
+}
+
+#[spacetimedb::table(name = large_var_rows_btree_each_column)]
+pub struct large_var_rows_btree_each_column_t {
+    #[index(btree)]
+    id: u128,
+    #[index(btree)]
+    invoice_code: String,
+    #[index(btree)]
+    status: String,
+    #[index(btree)]
+    customer: Identity,
+    #[index(btree)]
+    company: Address,
+    #[index(btree)]
+    user_name: String,
+
+    #[index(btree)]
+    price: f64,
+    #[index(btree)]
+    cost: f64,
+    #[index(btree)]
+    discount: f64,
+    #[index(btree)]
+    taxes: Vec<f64>,
+    #[index(btree)]
+    tax_total: f64,
+    #[index(btree)]
+    sub_total: f64,
+    #[index(btree)]
+    total: f64,
+
+    #[index(btree)]
+    country: String,
+    #[index(btree)]
+    state: String,
+    #[index(btree)]
+    city: String,
+    #[index(btree)]
+    zip_code: Option<String>,
+    #[index(btree)]
+    phone: String,
+
+    #[index(btree)]
+    notes: String,
+    #[index(btree)]
+    tags: Option<Vec<String>>,
 }
 
 // ---------- empty ----------
@@ -170,6 +322,197 @@ pub fn insert_bulk_btree_each_column_u32_u64_str(ctx: &ReducerContext, people: V
     }
 }
 
+fn rand_address(rng: &mut &StdbRng) -> Address {
+    Address::from(Faker.fake_with_rng::<u128, _>(rng))
+}
+
+fn rand_identity(rng: &mut &StdbRng) -> Identity {
+    Identity::from_u256(Faker.fake_with_rng::<u128, _>(rng).into())
+}
+
+#[spacetimedb::reducer]
+pub fn insert_bulk_tiny_rows(ctx: &ReducerContext, rows: u8) {
+    for id in 0..rows {
+        ctx.db.tiny_rows().insert(tiny_rows_t { id });
+    }
+    log::info!("Inserted on tiny_rows: {} rows", rows);
+}
+
+#[spacetimedb::reducer]
+pub fn insert_bulk_small_rows(ctx: &ReducerContext, rows: u64) {
+    let mut rng = ctx.rng();
+    for id in 0..rows {
+        ctx.db.small_rows().insert(small_rows_t {
+            id,
+            x: rng.gen(),
+            y: rng.gen(),
+        });
+    }
+    log::info!("Inserted on small_rows: {} rows", rows);
+}
+
+#[spacetimedb::reducer]
+pub fn insert_bulk_small_btree_each_column_rows(ctx: &ReducerContext, rows: u64) {
+    let mut rng = ctx.rng();
+    for id in 0..rows {
+        ctx.db
+            .small_btree_each_column_rows()
+            .insert(small_rows_btree_each_column_t {
+                id,
+                x: rng.gen(),
+                y: rng.gen(),
+            });
+    }
+    log::info!("Inserted on small_btree_each_column_rows: {} rows", rows);
+}
+
+#[spacetimedb::reducer]
+pub fn insert_bulk_medium_var_rows(ctx: &ReducerContext, rows: u64) {
+    let mut rng = ctx.rng();
+    for id in 0..rows {
+        ctx.db.medium_var_rows().insert(medium_var_rows_t {
+            id,
+            name: Name(EN).fake_with_rng(&mut rng),
+            email: SafeEmail(EN).fake_with_rng(&mut rng),
+            password: Password(EN, 6..10).fake_with_rng(&mut rng),
+            identity: rand_identity(&mut rng),
+            address: rand_address(&mut rng),
+            pos: Faker.fake_with_rng(&mut rng),
+        });
+    }
+    log::info!("Inserted on medium_var_rows: {} rows", rows);
+}
+
+#[spacetimedb::reducer]
+pub fn insert_bulk_medium_var_rows_btree_each_column(ctx: &ReducerContext, rows: u64) {
+    let mut rng = ctx.rng();
+    for id in 0..rows {
+        ctx.db
+            .medium_var_rows_btree_each_column()
+            .insert(medium_var_rows_btree_each_column_t {
+                id,
+                name: Name(EN).fake_with_rng(&mut rng),
+                email: SafeEmail(EN).fake_with_rng(&mut rng),
+                password: Password(EN, 6..10).fake_with_rng(&mut rng),
+                identity: rand_identity(&mut rng),
+                address: rand_address(&mut rng),
+                pos: Faker.fake_with_rng(&mut rng),
+            });
+    }
+    log::info!("Inserted on medium_var_rows_btree_each_column: {} rows", rows);
+}
+
+#[spacetimedb::reducer]
+pub fn insert_bulk_large_var_rows(ctx: &ReducerContext, rows: u64) {
+    let mut rng = ctx.rng();
+    for id in 0..(rows as u128) {
+        ctx.db.large_var_rows().insert(large_var_rows_t {
+            id,
+            invoice_code: Faker.fake_with_rng(&mut rng),
+            status: Faker.fake_with_rng(&mut rng),
+            customer: rand_identity(&mut rng),
+            company: rand_address(&mut rng),
+            user_name: Faker.fake_with_rng(&mut rng),
+
+            price: Faker.fake_with_rng(&mut rng),
+            cost: Faker.fake_with_rng(&mut rng),
+            discount: Faker.fake_with_rng(&mut rng),
+            taxes: Faker.fake_with_rng(&mut rng),
+            tax_total: Faker.fake_with_rng(&mut rng),
+            sub_total: Faker.fake_with_rng(&mut rng),
+            total: Faker.fake_with_rng(&mut rng),
+
+            country: CountryName(EN).fake_with_rng(&mut rng),
+            state: StateName(EN).fake_with_rng(&mut rng),
+            city: CityName(EN).fake_with_rng(&mut rng),
+            zip_code: ZipCode(EN).fake_with_rng(&mut rng),
+            phone: CellNumber(EN).fake_with_rng(&mut rng),
+
+            notes: Paragraph(EN, 0..3).fake_with_rng(&mut rng),
+            tags: Words(EN, 0..3).fake_with_rng(&mut rng),
+        });
+    }
+    log::info!("Inserted on large_var_rows: {} rows", rows);
+}
+
+#[spacetimedb::reducer]
+pub fn insert_bulk_large_var_rows_btree_each_column(ctx: &ReducerContext, rows: u64) {
+    let mut rng = ctx.rng();
+    for id in 0..(rows as u128) {
+        ctx.db
+            .large_var_rows_btree_each_column()
+            .insert(large_var_rows_btree_each_column_t {
+                id,
+                invoice_code: Faker.fake_with_rng(&mut rng),
+                status: Faker.fake_with_rng(&mut rng),
+                customer: rand_identity(&mut rng),
+                company: rand_address(&mut rng),
+                user_name: Faker.fake_with_rng(&mut rng),
+
+                price: Faker.fake_with_rng(&mut rng),
+                cost: Faker.fake_with_rng(&mut rng),
+                discount: Faker.fake_with_rng(&mut rng),
+                taxes: Faker.fake_with_rng(&mut rng),
+                tax_total: Faker.fake_with_rng(&mut rng),
+                sub_total: Faker.fake_with_rng(&mut rng),
+                total: Faker.fake_with_rng(&mut rng),
+
+                country: CountryName(EN).fake_with_rng(&mut rng),
+                state: StateName(EN).fake_with_rng(&mut rng),
+                city: CityName(EN).fake_with_rng(&mut rng),
+                zip_code: ZipCode(EN).fake_with_rng(&mut rng),
+                phone: CellNumber(EN).fake_with_rng(&mut rng),
+
+                notes: Paragraph(EN, 0..3).fake_with_rng(&mut rng),
+                tags: Words(EN, 0..3).fake_with_rng(&mut rng),
+            });
+    }
+    log::info!("Inserted on large_var_rows_btree_each_column: {} rows", rows);
+}
+
+/// This reducer is used to load synthetic data into the database for benchmarking purposes.
+///
+/// The input is a string with the following format:
+///
+/// `load_type`: [`Load`], `index_type`: [`Index`], `row_count`: `u32`
+#[spacetimedb::reducer]
+pub fn load(ctx: &ReducerContext, input: String) -> Result<(), String> {
+    let args = input.split(',').map(|x| x.trim().to_lowercase()).collect::<Vec<_>>();
+    if args.len() != 3 {
+        return Err(format!("Expected 3 arguments, got {}", args.len()));
+    }
+    let load = match args[0].as_str() {
+        "tiny" => Load::Tiny,
+        "small" => Load::Small,
+        "medium" => Load::Medium,
+        "large" => Load::Large,
+        x => {
+            return Err(format!(
+                "Invalid load type: '{x}', expected: tiny, small, medium, or large"
+            ))
+        }
+    };
+    let index = match args[1].as_str() {
+        "one" => Index::One,
+        "many" => Index::Many,
+        x => return Err(format!("Invalid index type: '{x}', expected: one, or many")),
+    };
+    let rows = args[2]
+        .parse::<u64>()
+        .map_err(|e| format!("Invalid row count: {}", e))?;
+
+    match (load, index) {
+        (Load::Tiny, Index::One | Index::Many) => insert_bulk_tiny_rows(ctx, rows as u8),
+        (Load::Small, Index::One) => insert_bulk_small_rows(ctx, rows),
+        (Load::Small, Index::Many) => insert_bulk_small_btree_each_column_rows(ctx, rows),
+        (Load::Medium, Index::One) => insert_bulk_medium_var_rows(ctx, rows),
+        (Load::Medium, Index::Many) => insert_bulk_medium_var_rows_btree_each_column(ctx, rows),
+        (Load::Large, Index::One) => insert_bulk_large_var_rows(ctx, rows),
+        (Load::Large, Index::Many) => insert_bulk_large_var_rows_btree_each_column(ctx, rows),
+    }
+
+    Ok(())
+}
 // ---------- update ----------
 
 #[spacetimedb::reducer]
