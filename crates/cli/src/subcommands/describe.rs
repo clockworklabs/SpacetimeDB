2cde4448d985051ee404a9b709fc7901cb667a26
Noa <coolreader18@gmail.com>
2025-01-27 15:45:13 -0600

    Remove janky schema from http API

--- crates/cli/src/subcommands/describe.rs
+++ crates/cli/src/subcommands/describe.rs
@@ -1,7 +1,9 @@
 use crate::common_args;
 use crate::config::Config;
-use crate::util::{add_auth_header_opt, database_identity, get_auth_header};
+use crate::util::{database_identity, fetch_raw_module_schema};
+use anyhow::Context;
 use clap::{Arg, ArgMatches};
+use spacetimedb_lib::sats;
 
 pub fn cli() -> clap::Command {
     clap::Command::new("describe")
@@ -13,7 +15,7 @@ pub fn cli() -> clap::Command {
         )
         .arg(
             Arg::new("entity_type")
-                .value_parser(["reducer", "table"])
+                .value_parser(clap::value_parser!(EntityType))
                 .help("Whether to describe a reducer or table"),
         )
         .arg(
@@ -26,31 +28,55 @@ pub fn cli() -> clap::Command {
         .after_help("Run `spacetime help describe` for more detailed information.\n")
 }
 
+#[derive(clap::ValueEnum, Clone, Copy)]
+enum EntityType {
+    Reducer,
+    Table,
+}
+
 pub async fn exec(config: Config, args: &ArgMatches) -> Result<(), anyhow::Error> {
     let database = args.get_one::<String>("database").unwrap();
     let entity_name = args.get_one::<String>("entity_name");
-    let entity_type = args.get_one::<String>("entity_type");
+    let entity_type = args.get_one::<EntityType>("entity_type");
+    let entity = entity_type.zip(entity_name);
     let server = args.get_one::<String>("server").map(|s| s.as_ref());
 
     let anon_identity = args.get_flag("anon_identity");
 
     let database_identity = database_identity(&config, database, server).await?;
 
-    let builder = reqwest::Client::new().get(match entity_name {
-        None => format!("{}/database/schema/{}", config.get_host_url(server)?, database_identity),
-        Some(entity_name) => format!(
-            "{}/database/schema/{}/{}/{}",
-            config.get_host_url(server)?,
-            database_identity,
-            entity_type.unwrap(),
-            entity_name
-        ),
-    });
-    let auth_header = get_auth_header(&config, anon_identity)?;
-    let builder = add_auth_header_opt(builder, &auth_header);
+    let module_def = fetch_raw_module_schema(
+        &reqwest::Client::new(),
+        &config,
+        database_identity,
+        server,
+        anon_identity,
+    )
+    .await?;
+    fn sats_to_json<T: sats::Serialize>(v: &T) -> serde_json::Result<String> {
+        serde_json::to_string_pretty(sats::serde::SerdeWrapper::from_ref(v))
+    }
+    let json = match entity {
+        Some((EntityType::Reducer, reducer_name)) => {
+            let reducer = module_def
+                .reducers
+                .iter()
+                .find(|r| *r.name == **reducer_name)
+                .context("no such reducer")?;
+            sats_to_json(reducer)?
+        }
+        Some((EntityType::Table, table_name)) => {
+            let table = module_def
+                .tables
+                .iter()
+                .find(|t| *t.name == **table_name)
+                .context("no such table")?;
+            sats_to_json(table)?
+        }
+        None => sats_to_json(&module_def)?,
+    };
 
-    let descr = builder.send().await?.error_for_status()?.text().await?;
-    println!("{}", descr);
+    println!("{json}");
 
     Ok(())
 }
