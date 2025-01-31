c37a46f8d42584d8be632b57893b4f00b3f1d59b
Zeke Foppa <bfops@users.noreply.github.com>
2025-01-31 09:15:56 -0800

    [bfops/anonymous-cli]: clarity tweaks, comments

--- crates/cli/src/subcommands/publish.rs
+++ crates/cli/src/subcommands/publish.rs
@@ -9,7 +9,7 @@ use std::fs;
 use std::path::PathBuf;
 
 use crate::config::Config;
-use crate::util::{add_auth_header_opt, get_auth_header, get_login_token_or_log_in};
+use crate::util::{add_auth_header_opt, get_auth_header};
 use crate::util::{decode_identity, unauth_error_context, y_or_n};
 use crate::{build, common_args};
 
@@ -159,7 +159,8 @@ pub async fn exec(mut config: Config, args: &ArgMatches) -> Result<(), anyhow::E
 
     let res = builder.body(program_bytes).send().await?;
     if res.status() == StatusCode::UNAUTHORIZED && !anon_identity {
-        let token = get_login_token_or_log_in(&mut config, server, !force).await?;
+        // If we're not in the `anon_identity` case, then we have already forced the user to log in above (using `get_auth_header`), so this should be safe to unwrap.
+        let token = config.spacetimedb_token().unwrap();
         let identity = decode_identity(&token)?;
         let err = res.text().await?;
         return unauth_error_context(
@@ -206,7 +207,7 @@ pub async fn exec(mut config: Config, args: &ArgMatches) -> Result<(), anyhow::E
                     domain.tld()
                 );
             }
-            // If we're not in the `anon_identity` case, then we have already forced the user to log in above (using `get_login_token_or_log_in`), so this should be safe to unwrap.
+            // If we're not in the `anon_identity` case, then we have already forced the user to log in above (using `get_auth_header`), so this should be safe to unwrap.
             let token = config.spacetimedb_token().unwrap();
             let identity = decode_identity(token)?;
             //TODO(jdetter): Have a nice name generator here, instead of using some abstract characters
