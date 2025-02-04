127cc3a8d5235f887fccb6b93e15931039fffa22
Jeffrey Dallatezza <jeffreydallatezza@gmail.com>
2025-02-04 13:47:48 -0800

    Add context to error

--- crates/core/src/host/host_controller.rs
+++ crates/core/src/host/host_controller.rs
@@ -730,7 +730,7 @@ impl Host {
                     history,
                     Some(durability),
                     Some(snapshot_repo),
-                )?
+                ).with_context(|| format!("Error opening relational db for {}", db_identity))?
             }
         };
         let (program, program_needs_init) = match db.program()? {
