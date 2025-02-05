0283279c720a39cdc657beba0df655e3070052d3
Mario Alejandro Montoya Cortés <mamcx@elmalabarista.com>
2025-02-05 13:06:05 -0500

    Printer for the plan, ie: EXPLAIN
    * Improve output of plan
    * Fix print of alias and the output of joins
    * Allow the test utils of the plan to be used elsewhere
    * Use the arrow only for nodes, make timing optional
    * Apply new clippy hints and remove old test logic

--- crates/expr/src/lib.rs
+++ crates/expr/src/lib.rs
@@ -347,6 +347,7 @@ pub(crate) fn parse(value: &str, ty: &AlgebraicType) -> anyhow::Result<Algebraic
 }
 
 /// The source of a statement
+#[derive(Debug, Clone, Copy)]
 pub enum StatementSource {
     Subscription,
     Query,
@@ -359,4 +360,5 @@ pub struct StatementCtx<'a> {
     pub statement: Statement,
     pub sql: &'a str,
     pub source: StatementSource,
+    pub planning_time: Option<std::time::Duration>,
 }
