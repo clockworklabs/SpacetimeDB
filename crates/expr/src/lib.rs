7ff9b453486217d3b2f7e45ca5f7650bcda0b46a
Mario Alejandro Montoya Cortés <mamcx@elmalabarista.com>
2025-02-05 13:06:05 -0500

    Printer for the plan, ie: EXPLAIN
    * Improve output of plan
    * Fix print of alias and the output of joins
    * Allow the test utils of the plan to be used elsewhere
    * Use the arrow only for nodes, make timing optional
    * Apply new clippy hints and remove old test logic
    * `EXPLAIN` for `dml`
    * `EXPLAIN` for `COUNT` and `LIMIT`
    * Change the output so we show the node info before the sub nodes
    * Index scans for ranges

--- crates/expr/src/lib.rs
+++ crates/expr/src/lib.rs
@@ -57,9 +57,11 @@ pub(crate) fn type_proj(input: RelExpr, proj: ast::Project, vars: &Relvars) -> T
             Ok(ProjectList::Name(vec![ProjectName::Some(input, var)]))
         }
         ast::Project::Star(Some(SqlIdent(var))) => Err(Unresolved::var(&var).into()),
-        ast::Project::Count(SqlIdent(alias)) => {
-            Ok(ProjectList::Agg(vec![input], AggType::Count, alias, AlgebraicType::U64))
-        }
+        ast::Project::Count(SqlIdent(alias)) => Ok(ProjectList::Agg(
+            vec![input],
+            AggType::Count { alias },
+            AlgebraicType::U64,
+        )),
         ast::Project::Exprs(elems) => {
             let mut projections = vec![];
             let mut names = HashSet::new();
@@ -359,6 +361,7 @@ pub(crate) fn parse(value: &str, ty: &AlgebraicType) -> anyhow::Result<Algebraic
 }
 
 /// The source of a statement
+#[derive(Debug, Clone, Copy)]
 pub enum StatementSource {
     Subscription,
     Query,
@@ -371,4 +374,5 @@ pub struct StatementCtx<'a> {
     pub statement: Statement,
     pub sql: &'a str,
     pub source: StatementSource,
+    pub planning_time: Option<std::time::Duration>,
 }
