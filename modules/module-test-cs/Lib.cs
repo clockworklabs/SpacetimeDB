aad7648d4d7d9aa997e40f6bc34beed8d6cdb977
Phoebe Goldman <phoebe@goldman-tribe.org>
2025-02-09 18:39:23 -0500

    `CallerIdentity` -> `Sender` as well

--- modules/module-test-cs/Lib.cs
+++ modules/module-test-cs/Lib.cs
@@ -251,7 +251,7 @@ static partial class Module
     public static void test(ReducerContext ctx, TestAlias arg, TestB arg2, TestC arg3, TestF arg4)
     {
         Log.Info("BEGIN");
-        Log.Info($"sender: {ctx.CallerIdentity}");
+        Log.Info($"sender: {ctx.Sender}");
         Log.Info($"timestamp: {ctx.Timestamp}");
         Log.Info($"bar: {arg2.foo}");
 
@@ -436,7 +436,7 @@ static partial class Module
     [Reducer]
     public static void assert_caller_identity_is_module_identity(ReducerContext ctx)
     {
-        var caller = ctx.CallerIdentity;
+        var caller = ctx.Sender;
         var owner = ctx.Identity;
         if (!caller.Equals(owner))
         {
