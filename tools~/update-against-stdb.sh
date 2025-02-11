510d8583e1fc296b73d8fa0a863ccce05d5567c7
james gilles <jameshgilles@gmail.com>
2025-02-11 13:21:06 -0500

    Emit Event.UnknownTransaction rather than throwing on unknown reducer (#244)
    ## Description of Changes
    as described
    
    ## API
    
    no breaks
    
    ## Requires SpacetimeDB PRs
    https://github.com/clockworklabs/SpacetimeDB/pull/2241
    
    ## Testsuite
    
    SpacetimeDB branch name: jgilles/unknown_reducer
    
    ## Testing
    
    see SpacetimeDB PR

---
+++ tools~/update-against-stdb.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Do everything needed to update this repo from an upstream STDB clone.
+
+set -ueox pipefail
+
+STDB_PATH="$1"
+SDK_PATH="$(dirname "$0")/.."
+SDK_PATH="$(realpath "$SDK_PATH")"
+
+"$SDK_PATH/tools~/write-nuget-config.sh" "$STDB_PATH"
+"$SDK_PATH/tools~/gen-client-api.sh" "$STDB_PATH"
+"$SDK_PATH/tools~/gen-quickstart.sh" "$STDB_PATH"
+dotnet nuget locals all --clear
+dotnet pack "$STDB_PATH/crates/bindings-csharp"
+rm -rf "$SDK_PATH/packages"
+dotnet pack
+dotnet test
+pushd "$SDK_PATH"; git checkout -- 'packages/*.meta' 'packages/**/*.meta' packages/.gitignore; popd
+
+
