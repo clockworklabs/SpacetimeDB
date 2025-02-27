c09e2138e0fa7644db001e846befe26069cf7f04
Phoebe Goldman <phoebe@goldman-tribe.org>
2025-02-27 10:19:49 -0500

    Also add comments to `connect_to_db`

--- crates/sdk/examples/quickstart-chat/main.rs
+++ crates/sdk/examples/quickstart-chat/main.rs
@@ -35,12 +35,21 @@ const DB_NAME: &str = "quickstart-chat";
 /// Load credentials from a file and connect to the database.
 fn connect_to_db() -> DbConnection {
     DbConnection::builder()
+        // Register our `on_connect` callback, which will save our auth token.
         .on_connect(on_connected)
+        // Register our `on_connect_error` callback, which will print a message, then exit the process.
         .on_connect_error(on_connect_error)
+        // Our `on_disconnect` callback, which will print a message, then exit the process.
         .on_disconnect(on_disconnected)
+        // If the user has previously connected, we'll have saved a token in the `on_connect` callback.
+        // In that case, we'll load it and pass it to `with_token`,
+        // so we can re-authenticate as the same `Identity`.
         .with_token(creds_store().load().expect("Error loading credentials"))
+        // Set the database name we chose when we called `spacetime publish`.
         .with_module_name(DB_NAME)
+        // Set the URI of the SpacetimeDB host that's running our database.
         .with_uri(HOST)
+        // Finalize configuration and connect!
         .build()
         .expect("Failed to connect")
 }
