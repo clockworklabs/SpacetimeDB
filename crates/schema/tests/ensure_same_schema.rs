4b79aa6d2e2cda793e29e12a3773bb3e155dccbe
James Gilles <jameshgilles@gmail.com>
2025-03-19 13:33:05 -0400

    Update schema tests now that CVF is implemented

--- crates/schema/tests/ensure_same_schema.rs
+++ crates/schema/tests/ensure_same_schema.rs
@@ -19,11 +19,6 @@ fn assert_identical_modules(module_name_prefix: &str) {
         .expect("could not compute a diff between Rust and C#")
         .steps;
 
-    // Ignore RLS steps for now, as they are not yet implemented in C#.
-    // TODO: remove this when C#-friendly RLS is implemented.
-    let mut diff = diff;
-    diff.retain(|step| !matches!(step, AutoMigrateStep::AddRowLevelSecurity(_)));
-
     assert!(
         diff.is_empty(),
         "Rust and C# modules are not identical. Here are the steps to migrate from C# to Rust: {diff:#?}"
