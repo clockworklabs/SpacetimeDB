9daf3992b204703241d916e8b796853d064bd475
Phoebe Goldman <phoebe@goldman-tribe.org>
2025-03-28 10:19:26 -0400

    Add smoketest that `st_client` rows get cleaned up after `client_disconnected` error
    they don't, so this test fails...

--- smoketests/tests/client_connected_error_rejects_connection.py
+++ smoketests/tests/client_connected_error_rejects_connection.py
@@ -1,7 +1,6 @@
 from .. import Smoketest
 
-class ClientConnectedErrorRejectsConnection(Smoketest):
-    MODULE_CODE = """
+MODULE_HEADER = """
 use spacetimedb::{ReducerContext, Table};
 
 #[spacetimedb::table(name = all_u8s, public)]
@@ -16,6 +15,10 @@ pub fn init(ctx: &ReducerContext) {
         ctx.db.all_u8s().insert(AllU8s { number: i });
     }
 }
+"""
+
+class ClientConnectedErrorRejectsConnection(Smoketest):
+    MODULE_CODE = MODULE_HEADER + """
 
 #[spacetimedb::reducer(client_connected)]
 pub fn identity_connected(ctx: &ReducerContext) -> Result<(), String> {
@@ -35,3 +38,29 @@ pub fn identity_disconnected(_ctx: &ReducerContext) {
         logs = self.logs(100)
         self.assertIn('Rejecting connection from client', logs)
         self.assertNotIn('This should never be called, since we reject all connections!', logs)
+
+class ClientDisconnectedErrorStillDeletesStClient(Smoketest):
+    MODULE_CODE = MODULE_HEADER + """
+#[spacetimedb::reducer(client_connected)]
+pub fn identity_connected(_ctx: &ReducerContext) -> Result<(), String> {
+     Ok(())
+}
+
+#[spacetimedb::reducer(client_disconnected)]
+pub fn identity_disconnected(_ctx: &ReducerContext) {
+    panic!("This should be called, but the `st_client` row should still be deleted")
+}
+"""
+
+    def test_client_disconnected_error_still_deletes_st_client(self):
+        self.subscribe("select * from all_u8s", n = 0)()
+
+        logs = self.logs(100)
+        self.assertIn('This should be called, but the `st_client` row should still be deleted', logs)
+
+        sql_out = self.spacetime("sql", self.database_identity, "select * from st_client")
+
+        self.assertMultiLineEqual(sql_out, """
+ identity | connection_id 
+----------+---------------
+""")
