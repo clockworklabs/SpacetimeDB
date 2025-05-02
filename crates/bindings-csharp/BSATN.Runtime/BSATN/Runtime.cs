06db18de1a2d62517e91ef19eced0d042fe23a1d
James Gilles <jameshgilles@gmail.com>
2025-05-02 12:35:46 -0400

    Fix comment

--- crates/bindings-csharp/BSATN.Runtime/BSATN/Runtime.cs
+++ crates/bindings-csharp/BSATN.Runtime/BSATN/Runtime.cs
@@ -407,7 +407,7 @@ public readonly struct Array<Element, ElementRW> : IReadWrite<Element[]>
 
     public Element[] Read(BinaryReader reader)
     {
-        // Don't use IEnumerable here: save an allocation and pre-allocate the output.
+        // Don't use Enumerable here: save an allocation and pre-allocate the output.
         var count = reader.ReadInt32();
         var result = new Element[count];
         for (var i = 0; i < count; i++)
@@ -461,7 +461,7 @@ public readonly struct List<Element, ElementRW> : IReadWrite<List<Element>>
 
     public List<Element> Read(BinaryReader reader)
     {
-        // Don't use IEnumerable here: save an allocation and pre-allocate the output.
+        // Don't use Enumerable here: save an allocation and pre-allocate the output.
         var count = reader.ReadInt32();
         var result = new List<Element>(count);
         for (var i = 0; i < count; i++)
