cbbbd89ab03906ba9c56b8ce9165d6221713d4d4
James Gilles <jameshgilles@gmail.com>
2025-05-07 15:00:05 -0400

    Fix a few comments

--- crates/bindings-csharp/Codegen.Tests/Tests.cs
+++ crates/bindings-csharp/Codegen.Tests/Tests.cs
@@ -123,7 +123,7 @@ public static class GeneratorSnapshotTests
         return compilation
             .Emit(Stream.Null)
             .Diagnostics.Where(diag => diag.Severity != DiagnosticSeverity.Hidden)
-            // The order of diagnostics is not predictable, sort them by location & content to make the test deterministic.
+            // The order of diagnostics is not predictable, sort them by location to make the test deterministic.
             .OrderBy(diag => diag.Location.SourceSpan.ToString());
     }
 
