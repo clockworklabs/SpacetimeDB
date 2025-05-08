75b0c28be3a35efa07089f4f8314479e8e66df2b
joshua-spacetime <josh@clockworklabs.io>
2025-05-08 13:15:05 -0700

    Avoid parsing to generate query hashes altogether

--- crates/core/src/subscription/query.rs
+++ crates/core/src/subscription/query.rs
@@ -12,11 +12,22 @@ use spacetimedb_vm::expr::{self, Crud, CrudExpr, QueryExpr};
 use super::execution_unit::QueryHash;
 use super::module_subscription_manager::Plan;
 
-pub(crate) static WHITESPACE: Lazy<Regex> = Lazy::new(|| Regex::new(r"\s+").unwrap());
-pub const SUBSCRIBE_TO_ALL_QUERY: &str = "SELECT * FROM *";
+static WHITESPACE: Lazy<Regex> = Lazy::new(|| Regex::new(r"^\s*$").unwrap());
+static SUBSCRIBE_TO_ALL_TABLES_REGEX: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r"^\s*(?i)\bSELECT\s+\*\s+FROM\s+\*\s*$").unwrap());
+
+/// Is this string all whitespace?
+pub fn is_whitespace_or_empty(sql: &str) -> bool {
+    WHITESPACE.is_match_at(sql, 0)
+}
+
+/// Is this a `SELECT * FROM *` query?
+pub fn is_subscribe_to_all_tables(sql: &str) -> bool {
+    SUBSCRIBE_TO_ALL_TABLES_REGEX.is_match_at(sql, 0)
+}
 
 // TODO: Remove this after the SubscribeSingle migration.
-// TODO: It's semantically wrong to `SUBSCRIBE_TO_ALL_QUERY`
+// TODO: It's semantically wrong to `SELECT * FROM *`
 // as it can only return back the changes valid for the tables in scope *right now*
 // instead of **continuously updating** the db changes
 // with system table modifications (add/remove tables, indexes, ...).
@@ -70,19 +81,36 @@ pub fn compile_read_only_queryset(
 /// Compile a string into a single read-only query.
 /// This returns an error if the string has multiple queries or mutations.
 pub fn compile_read_only_query(auth: &AuthCtx, tx: &Tx, input: &str) -> Result<Plan, DBError> {
-    let input = input.trim();
-    if input.is_empty() {
+    if is_whitespace_or_empty(input) {
         return Err(SubscriptionError::Empty.into());
     }
 
-    // Remove redundant whitespace, and in particular newlines, for debug info.
-    let input = WHITESPACE.replace_all(input, " ");
+    let tx = SchemaViewer::new(tx, auth);
+    let (plans, has_param) = SubscriptionPlan::compile(input, &tx, auth)?;
+    let hash = QueryHash::from_string(input, auth.caller, has_param);
+    Ok(Plan::new(plans, hash, input.to_owned()))
+}
+
+/// Compile a string into a single read-only query.
+/// This returns an error if the string has multiple queries or mutations.
+pub fn compile_query_with_hashes(
+    auth: &AuthCtx,
+    tx: &Tx,
+    input: &str,
+    hash: QueryHash,
+    hash_with_param: QueryHash,
+) -> Result<Plan, DBError> {
+    if WHITESPACE.is_match_at(input, 0) {
+        return Err(SubscriptionError::Empty.into());
+    }
 
     let tx = SchemaViewer::new(tx, auth);
-    let (plans, has_param) = SubscriptionPlan::compile(&input, &tx, auth)?;
-    let hash = QueryHash::from_string(&input, auth.caller, has_param);
+    let (plans, has_param) = SubscriptionPlan::compile(input, &tx, auth)?;
 
-    Ok(Plan::new(plans, hash, input.into_owned()))
+    if has_param {
+        return Ok(Plan::new(plans, hash_with_param, input.to_owned()));
+    }
+    Ok(Plan::new(plans, hash, input.to_owned()))
 }
 
 /// The kind of [`QueryExpr`] currently supported for incremental evaluation.
@@ -349,6 +377,25 @@ mod tests {
 
     fn singleton_execution_set(expr: QueryExpr, sql: String) -> ResultTest<ExecutionSet> {
         Ok(ExecutionSet::from_iter([SupportedQuery::try_from((expr, sql))?]))
+    }
+
+    #[test]
+    fn test_whitespace_regex() -> ResultTest<()> {
+        assert!(is_whitespace_or_empty(""));
+        assert!(is_whitespace_or_empty(" "));
+        assert!(is_whitespace_or_empty("\n \t"));
+        assert!(!is_whitespace_or_empty(" a"));
+        Ok(())
+    }
+
+    #[test]
+    fn test_subscribe_to_all_tables_regex() -> ResultTest<()> {
+        assert!(is_subscribe_to_all_tables("SELECT * FROM *"));
+        assert!(is_subscribe_to_all_tables("Select * From *"));
+        assert!(is_subscribe_to_all_tables("select * from *"));
+        assert!(is_subscribe_to_all_tables("\nselect *\nfrom * "));
+        assert!(!is_subscribe_to_all_tables("select * from * where"));
+        Ok(())
     }
 
     #[test]
