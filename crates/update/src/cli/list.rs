9c09db121f1e99fef286d068c83cfa941e668b40
Zeke Foppa <bfops@users.noreply.github.com>
2025-05-16 13:06:19 -0700

    [bfops/fix-current-version]: Noa's version

--- crates/update/src/cli/list.rs
+++ crates/update/src/cli/list.rs
@@ -1,5 +1,4 @@
 use spacetimedb_paths::SpacetimePaths;
-use std::path::Path;
 
 /// List installed SpacetimeDB versions.
 #[derive(clap::Args)]
@@ -11,16 +10,7 @@ pub(super) struct List {
 
 impl List {
     pub(super) fn exec(self, paths: &SpacetimePaths) -> anyhow::Result<()> {
-        let current = match paths.cli_bin_dir.current_version()? {
-            None => None,
-            Some(path_str) => {
-                let file_name = Path::new(&path_str)
-                    .file_name()
-                    .and_then(|f| f.to_str())
-                    .ok_or(anyhow::anyhow!("Could not extract current version"))?;
-                Some(file_name.to_string())
-            }
-        };
+        let current = paths.cli_bin_dir.current_version()?;
         let versions = if self.all {
             let client = super::reqwest_client()?;
             super::tokio_block_on(super::install::available_releases(&client))??
