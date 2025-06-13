898a71986c4372f32eec01de72488925d16dfffd
Phoebe Goldman <phoebe@goldman-tribe.org>
2025-06-13 12:13:10 -0400

    Add comments to smoketest

--- smoketests/tests/timestamp_route.py
+++ smoketests/tests/timestamp_route.py
@@ -11,21 +11,24 @@ class TimestampRoute(Smoketest):
     def test_timestamp_route(self):
         name = random_string()
 
+        # A request for the timestamp at a non-existent database is an error...
         with self.assertRaises(Exception) as err:
             self.api_call(
                 "GET",
-                f"/unstable/database/{name}/timestamp",
+                f"/database/{name}/timestamp",
             )
-
+        # ... with code 404.
         self.assertEqual(err.exception.args[0].status, 404)
 
         self.publish_module(name)
 
+        # A request for the timestamp at an extant database is a success...
         resp = self.api_call(
             "GET",
-            f"/unstable/database/{name}/timestamp",
+            f"/database/{name}/timestamp",
         )
 
+        # ... and the response body is a SATS-JSON encoded `Timestamp`.
         timestamp = json.load(io.BytesIO(resp))
         self.assertIsInstance(timestamp, dict)
         self.assertIn(TIMESTAMP_TAG, timestamp)
