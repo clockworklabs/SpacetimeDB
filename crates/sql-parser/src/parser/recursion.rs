cbeabbc6f5f240296b080a75d180f311013b3960
Mario Alejandro Montoya Cortes <mamcx@elmalabarista.com>
2025-07-14 10:15:38 -0500

    Apply suggestion from PR, and increase the recursion limit thanks to reduction on `Err` type

--- crates/sql-parser/src/parser/recursion.rs
+++ crates/sql-parser/src/parser/recursion.rs
@@ -1,33 +1,25 @@
-//! A utility for guarding against excessive recursion depth in the SQL parser.
+//! A utility for guarding against stack overflows in the SQL parser.
 //!
-//! Different parts of the parser may have different recursion limits.
-//!
-//! Removing one could allow the others to be higher, but depending on how the `SQL` is structured, it could lead to a `stack overflow`
-//! if is not guarded against, so is incorrect to assume that a limit is sufficient for the next part of the parser.
+//! Different parts of the parser may have different recursion limits, based in the size of the structures they parse.
+
 use crate::parser::errors::{RecursionError, SqlParseError};
-use std::fmt::Display;
 
 /// A conservative limit for recursion depth on `parse_expr`.
-pub const MAX_RECURSION_EXPR: usize = 700;
+pub const MAX_RECURSION_EXPR: usize = 1_600;
 /// A conservative limit for recursion depth on `type_expr`.
-pub const MAX_RECURSION_TYP_EXPR: usize = 5_000;
+pub const MAX_RECURSION_TYP_EXPR: usize = 2_500;
 
-/// A utility for guarding against excessive recursion depth.
+/// A utility for guarding against stack overflows in the SQL parser.
 ///
 /// **Usage:**
 /// ```
 /// use spacetimedb_sql_parser::parser::recursion;
 /// let mut depth = 0;
-/// assert!(recursion::guard(&mut depth, 10, "test").is_ok());
+/// assert!(recursion::guard(depth, 10, "test").is_ok());
 /// ```
-pub fn guard(depth: &mut usize, limit: usize, msg: impl Display) -> Result<(), SqlParseError> {
-    *depth += 1;
-    if *depth > limit {
-        Err(RecursionError {
-            limit,
-            message: msg.to_string(),
-        }
-        .into())
+pub fn guard(depth: usize, limit: usize, source: &'static str) -> Result<(), SqlParseError> {
+    if depth > limit {
+        Err(RecursionError { source_: source }.into())
     } else {
         Ok(())
     }
