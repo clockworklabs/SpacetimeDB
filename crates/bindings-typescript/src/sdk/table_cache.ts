241ca66b78dca5dd23b5f30e829291e333336fa9
Egor Vorontsov <sdoregor@sdore.me>
2025-07-22 05:45:03 +0300

    Implemented async `.remoteQuery()` for tables in TypeScript SDK.

--- crates/bindings-typescript/src/sdk/table_cache.ts
+++ crates/bindings-typescript/src/sdk/table_cache.ts
@@ -1,3 +1,8 @@
+import {
+  BinaryWriter,
+  type EventContextInterface,
+} from './db_connection_impl.ts';
+import type { DbContext } from './db_context.ts';
 import { EventEmitter } from './event_emitter.ts';
 import type { TableRuntimeTypeInfo } from './spacetime_module.ts';
 
@@ -68,6 +73,17 @@ export class TableCache<
    */
   iter(): RowType[] {
     return Array.from(this.rows.values()).map(([row]) => row);
+  }
+
+  remoteQuery(ctx: DbContext, filters: string): Promise<RowType[]> {
+    return new Promise((resolve, reject) => {
+      const name = this.name();
+
+      ctx.queryBuilder()
+        .onResolved((ctx, tables) => resolve(tables.get(name)?.iter()))
+        .onError((ctx, error) => reject(error))
+        .query(`SELECT ${name}.* FROM ${name} ${filters}`);
+    });
   }
 
   applyOperations = (
