956436a6090a6ff9731aea7f8c44813c15bdc4de
Tyler Cloutier <cloutiertyler@aol.com>
2025-07-23 23:54:28 -0400

    Formatting

--- sdks/typescript/packages/sdk/src/schedule_at.ts
+++ sdks/typescript/packages/sdk/src/schedule_at.ts
@@ -9,25 +9,42 @@ export namespace ScheduleAt {
     ]);
   }
 
-  export type Interval = { tag: 'Interval'; value: { __time_duration_micros__: BigInt } };
+  export type Interval = {
+    tag: 'Interval';
+    value: { __time_duration_micros__: BigInt };
+  };
   export const Interval = (value: BigInt): Interval => ({
     tag: 'Interval',
     value: { __time_duration_micros__: value },
   });
-  export type Time = { tag: 'Time'; value: { __timestamp_micros_since_unix_epoch__: BigInt } };
-  export const Time = (value: BigInt): Time => ({ tag: 'Time', value: { __timestamp_micros_since_unix_epoch__: value } });
+  export type Time = {
+    tag: 'Time';
+    value: { __timestamp_micros_since_unix_epoch__: BigInt };
+  };
+  export const Time = (value: BigInt): Time => ({
+    tag: 'Time',
+    value: { __timestamp_micros_since_unix_epoch__: value },
+  });
 
   export function fromValue(value: AlgebraicValue): ScheduleAt {
     let sumValue = value.asSumValue();
     switch (sumValue.tag) {
       case 0:
-        return { tag: 'Interval', value: {
-          __time_duration_micros__: sumValue.value.asProductValue().elements[0].asBigInt()
-        }};
+        return {
+          tag: 'Interval',
+          value: {
+            __time_duration_micros__: sumValue.value
+              .asProductValue()
+              .elements[0].asBigInt(),
+          },
+        };
       case 1:
-        return { tag: 'Time', value: {
-          __timestamp_micros_since_unix_epoch__: sumValue.value.asBigInt()
-        }};
+        return {
+          tag: 'Time',
+          value: {
+            __timestamp_micros_since_unix_epoch__: sumValue.value.asBigInt(),
+          },
+        };
       default:
         throw 'unreachable';
     }
