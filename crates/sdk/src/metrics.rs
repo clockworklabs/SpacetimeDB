8abb95ceaba950b6b2de350a5f1586e89a9e071b
Jeffrey Dallatezza <jeffreydallatezza@gmail.com>
2025-07-30 09:07:23 -0700

    Dont generate connection_ids in the rust client.

--- crates/sdk/src/metrics.rs
+++ crates/sdk/src/metrics.rs
@@ -1,18 +1,17 @@
 use once_cell::sync::Lazy;
 use prometheus::{HistogramVec, IntCounterVec};
-use spacetimedb_lib::ConnectionId;
 use spacetimedb_metrics::metrics_group;
 
 metrics_group!(
     pub struct ClientMetrics {
         #[name = spacetime_client_received_total]
         #[help = "The cumulative number of received websocket messages"]
-        #[labels(db: Box<str>, connection_id: ConnectionId)]
+        #[labels(db: Box<str>)]
         pub websocket_received: IntCounterVec,
 
         #[name = spacetime_client_received_msg_size]
         #[help = "The size of received websocket messages"]
-        #[labels(db: Box<str>, connection_id: ConnectionId)]
+        #[labels(db: Box<str>)]
         pub websocket_received_msg_size: HistogramVec,
     }
 );
