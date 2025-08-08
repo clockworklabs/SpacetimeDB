a0bd5f90a0c23e03b909b2e2586b839ffd43b987
Egor Vorontsov <sdoregor@sdore.me>
2025-08-09 01:29:09 +0300

    Use 32-bit `messageId`s for one-off queries.

--- sdks/typescript/packages/sdk/src/db_connection_impl.ts
+++ sdks/typescript/packages/sdk/src/db_connection_impl.ts
@@ -276,7 +276,7 @@ export class DbConnectionImpl<
     this.#sendMessage(
       ClientMessage.OneOffQuery({
         queryString: querySql,
-        messageId: new Uint8Array([queryId]),
+        messageId: new Uint8Array(new Uint32Array([queryId]).buffer),
       })
     );
     return queryId;
@@ -754,11 +754,19 @@ export class DbConnectionImpl<
         break;
       }
       case 'QueryResolved': {
-        const query = this.#queryManager.queries.get(message.messageId[0]);
+        if (message.messageId?.length != 4) {
+          stdbLogger(
+            'error',
+            `Received QueryResolved with invalid messageId ${message.messageId}.`
+          );
+          break;
+        }
+        const queryId = new DataView(message.messageId.buffer, message.messageId.byteOffset, 4).getUint32(0, true);
+        const query = this.#queryManager.queries.get(queryId);
         if (query === undefined) {
           stdbLogger(
             'error',
-            `Received QueryResolved for unknown messageId ${message.messageId}.`
+            `Received QueryResolved for unknown queryId ${queryId}.`
           );
           break;
         }
@@ -773,12 +781,8 @@ export class DbConnectionImpl<
             ...eventContext,
             event: error,
           };
-          if (message.messageId === undefined) {
-            console.error('Received an error message without a messageId: ', error);
-            break;
-          }
           this.#queryManager.queries
-            .get(message.messageId[0])
+            .get(queryId)
             ?.emitter.emit(
               'error',
               errorContext,
@@ -801,7 +805,7 @@ export class DbConnectionImpl<
             message.totalHostExecutionDuration
           );
         }
-        this.#queryManager.queries.delete(message.messageId[0]);
+        this.#queryManager.queries.delete(queryId);
         break;
       }
       case 'SubscribeApplied': {
