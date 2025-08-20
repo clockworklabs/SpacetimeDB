67ca3b1152aab18bd82093f5758af40e152ba146
Tyler Cloutier <cloutiertyler@aol.com>
2025-08-20 12:50:16 -0400

    Ran formatter

--- sdks/typescript/packages/sdk/vitest.config.ts
+++ sdks/typescript/packages/sdk/vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
@@ -12,4 +12,4 @@ export default defineConfig({
     preserveSymlinks: true,
     extensions: ['.ts', '.tsx', '.mjs', '.js', '.json'],
   },
-})
+});
