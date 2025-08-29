2af41120080cad0d79aff7ed0da739584db1119a
Jeffrey Dallatezza <jeffreydallatezza@gmail.com>
2025-08-29 14:00:55 -0400

    Regen csharp example and update the script for regenerating

--- sdks/csharp/tools~/gen-quickstart.sh
+++ sdks/csharp/tools~/gen-quickstart.sh
@@ -2,9 +2,13 @@
 
 set -ueo pipefail
 
-STDB_PATH="$1"
-SDK_PATH="$(dirname "$0")/.."
-SDK_PATH="$(realpath "$SDK_PATH")"
+# Move to the location of this script.
+cd "$(dirname "$0")"
+
+# This script is in "sdks/csharp/tools", so we need to go up 3 levels for the root of the repo.
+STDB_PATH="${1:-../../../}"
+# One level up gets us to the root of the csharp sdk.
+SDK_PATH="$(realpath "..")"
 
 cargo build --manifest-path "$STDB_PATH/crates/standalone/Cargo.toml"
 cargo run --manifest-path "$STDB_PATH/crates/cli/Cargo.toml" -- generate -y -l csharp -o "$SDK_PATH/examples~/quickstart-chat/client/module_bindings" --project-path "$STDB_PATH/modules/quickstart-chat"
