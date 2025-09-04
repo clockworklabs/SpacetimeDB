71cb462395f7c847ef258fc4b7d2a3edd67fb009
Tyler Cloutier <cloutiertyler@aol.com>
2025-09-04 15:21:41 -0400

    fixed errors with running tests and generating

--- crates/bindings-typescript/src/server/type_builders.ts
+++ crates/bindings-typescript/src/server/type_builders.ts
@@ -67,7 +67,7 @@ interface IntoColumnBuilder<Type, SpacetimeType extends AlgebraicType> {
 /**
  * Helper type to extract the TypeScript type from a TypeBuilder
  */
-type InferTypeOfTypeBuilder<T> =
+export type InferTypeOfTypeBuilder<T> =
   T extends TypeBuilder<infer U, any> ? U : never;
 
 /**
@@ -78,7 +78,7 @@ export type Infer<T> = InferTypeOfTypeBuilder<T>;
 /**
  * Helper type to extract the type of a row from an object.
  */
-type InferTypeOfRow<T> =
+export type InferTypeOfRow<T> =
   T extends Record<string, ColumnBuilder<any, any, any> | TypeBuilder<any, any>>
     ? {
         [K in keyof T]: T[K] extends ColumnBuilder<infer V, any, any>
@@ -844,92 +844,3 @@ const t = {
   },
 } as const;
 export default t;
-
-type MustBeNever<T> = [T] extends [never]
-  ? true
-  : ['Error: Type must be never', T];
-
-// Test type inference on a row
-// i.e. a Record<string, TypeBuilder | ColumnBuilder> type
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const row = {
-  foo: t.string(),
-  bar: t.i32().primaryKey(),
-  idx: t.i64().index('btree').unique(),
-};
-type Row = InferTypeOfRow<typeof row>;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const _row: Row = {
-  foo: 'hello',
-  bar: 42,
-  idx: 100n,
-};
-
-// Test that a row must not allow non-TypeBuilder or ColumnBuilder values
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const row2 = {
-  foo: {
-    // bar is not a TypeBuilder or ColumnBuilder, so this should fail
-    bar: t.string(),
-  },
-  bar: t.i32().primaryKey(),
-  idx: t.i64().index('btree').unique(),
-};
-type Row2 = InferTypeOfRow<typeof row2>;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-type _ = MustBeNever<Row2>;
-
-// Test type inference on a type with a nested object
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const point = t.object({
-  x: t.i32(),
-  y: t.f64(),
-  z: t.object({
-    foo: t.string(),
-  }),
-});
-type Point = InferTypeOfTypeBuilder<typeof point>;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const _point: Point = {
-  x: 1.0,
-  y: 2.0,
-  z: {
-    foo: 'bar',
-  },
-};
-
-// Test type inference on an enum
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const e = t.enum({
-  A: t.string(),
-  B: t.number(),
-});
-type E = InferTypeOfTypeBuilder<typeof e>;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const _e: E = { tag: 'A', value: 'hello' };
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const _e2: E = { tag: 'B', value: 42 };
-
-// Test that the type of a row includes the correct ColumnBuilder types
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const _row3: {
-  foo: TypeBuilder<string, AlgebraicTypeVariants.String>;
-  bar: I32ColumnBuilder<{
-    isPrimaryKey: true;
-    isUnique: false;
-    isAutoIncrement: false;
-    isScheduleAt: false;
-    indexType: undefined;
-  }>;
-  idx: I64ColumnBuilder<{
-    isPrimaryKey: false;
-    isUnique: true;
-    isAutoIncrement: false;
-    isScheduleAt: false;
-    indexType: 'btree';
-  }>;
-} = {
-  foo: t.string(),
-  bar: t.i32().primaryKey(),
-  idx: t.i64().index('btree').unique(),
-};
