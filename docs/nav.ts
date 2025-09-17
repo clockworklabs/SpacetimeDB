625dd335f006d5398b86a1673736b38cc8c80020
Jason Larabie <jason@clockworklabs.io>
2025-09-17 13:01:35 -0700

    Update navigation for new pages

--- docs/nav.ts
+++ docs/nav.ts
@@ -48,6 +48,14 @@ const nav: Nav = {
     page('3 - Gameplay', 'unity/part-3', 'unity/part-3.md'),
     page('4 - Moving and Colliding', 'unity/part-4', 'unity/part-4.md'),
 
+    section('Unreal Tutorial - Basic Multiplayer'),
+    page('Overview', 'unreal', 'unreal/index.md'),
+    page('1 - Setup', 'unreal/part-1', 'unreal/part-1.md'),
+    page('2 - Connecting to SpacetimeDB', 'unreal/part-2', 'unreal/part-2.md'),
+    page('3 - Gameplay', 'unreal/part-3', 'unreal/part-3.md'),
+    page('4 - Moving and Colliding', 'unreal/part-4', 'unreal/part-4.md'),
+	page('Reference', 'unreal/part-4', 'unreal/part-4.md'),
+
     section('CLI Reference'),
     page('CLI Reference', 'cli-reference', 'cli-reference.md'),
     page(
