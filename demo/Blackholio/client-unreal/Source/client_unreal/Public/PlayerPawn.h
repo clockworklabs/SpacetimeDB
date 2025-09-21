9530e08476d2c3176447cb9e5de1c0df6c96c0cd
Jason Larabie <jason@clockworklabs.io>
2025-09-21 16:21:43 -0700

    Add Unreal Blackholio

---
+++ demo/Blackholio/client-unreal/Source/client_unreal/Public/PlayerPawn.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerPawn.generated.h"
+
+class ACircle;
+struct FPlayerType;
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class CLIENT_UNREAL_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	APlayerPawn();
+	void Initialize(FPlayerType Player);
+
+	uint32 PlayerId = 0;
+	UPROPERTY(BlueprintReadWrite, Category="BH|Player")
+	bool bIsLocalPlayer = false;
+	
+	UPROPERTY()
+	TArray<TWeakObjectPtr<ACircle>> OwnedCircles;
+
+	UFUNCTION()
+	FString GetUsername() const;
+	UFUNCTION()
+	void OnCircleSpawned(ACircle* Circle);
+	UFUNCTION()
+	void OnCircleDeleted(ACircle* Circle);
+
+	UFUNCTION(BlueprintCallable, Category="BH|Input")
+	void Split();
+	UFUNCTION(BlueprintCallable, Category="BH|Input")
+	void Suicide();
+	
+	uint32 TotalMass() const;
+	UFUNCTION(BlueprintPure, Category="BH|Player")
+	FVector CenterOfMass() const;
+
+protected:
+	virtual void Destroyed() override;
+	UPROPERTY(EditDefaultsOnly, Category="BH|Camera")
+	float BaseSize = 50.f;
+	UPROPERTY(EditDefaultsOnly, Category="BH|Camera")
+	float MassToSizeDivisor = 5.f;
+	UPROPERTY(EditDefaultsOnly, Category="BH|Camera")
+	float MaxMassBonus = 50.f;
+	UPROPERTY(EditDefaultsOnly, Category="BH|Camera")
+	float SplitBonus = 30.f;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<USpringArmComponent> SpringArm;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UCameraComponent> Camera;
+public:
+	virtual void Tick(float DeltaTime) override;
+};
