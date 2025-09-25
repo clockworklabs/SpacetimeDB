fd79d2f476ba398d36d81c6237efbfcde47d9aa8
Kim Altintop <kim@eagain.io>
2025-09-25 14:21:18 +0200

    WIP: snapshot metrics

--- crates/core/src/db/snapshot.rs
+++ crates/core/src/db/snapshot.rs
@@ -4,12 +4,13 @@ use std::{
 };
 
 use futures::{channel::mpsc, StreamExt as _};
+use log::{error, info, warn};
 use parking_lot::RwLock;
-use prometheus::Histogram;
+use prometheus::{Histogram, IntGauge};
 use spacetimedb_datastore::locking_tx_datastore::{committed_state::CommittedState, datastore::Locking};
 use spacetimedb_durability::TxOffset;
 use spacetimedb_lib::Identity;
-use spacetimedb_snapshot::SnapshotRepository;
+use spacetimedb_snapshot::{CompressionStats, SnapshotRepository};
 use tokio::sync::watch;
 
 use crate::{util::asyncify, worker_metrics::WORKER_METRICS};
@@ -61,11 +62,12 @@ impl SnapshotWorker {
         let (request_tx, request_rx) = mpsc::unbounded();
         let metrics = ActorMetrics::new(self.snapshot_repository.database_identity());
         let actor = SnapshotWorkerActor {
-            trigger: request_rx,
-            committed_state: state,
-            repo: self.snapshot_repository.clone(),
-            notify_tx: self.snapshot_created.clone(),
+            snapshot_requests: request_rx,
+            database_state: state,
+            snapshot_repo: self.snapshot_repository.clone(),
+            snapshot_created: self.snapshot_created.clone(),
             metrics,
+            compression_stats: <_>::default(),
         };
         tokio::spawn(actor.run());
         self.request_snapshot
@@ -103,6 +105,11 @@ struct ActorMetrics {
     snapshot_timing_inner: Histogram,
     compression_timing_total: Histogram,
     compression_timing_inner: Histogram,
+    compression_timing_single: Histogram,
+    compression_skipped: IntGauge,
+    compression_compressed: IntGauge,
+    compression_objects_compressed: IntGauge,
+    compression_objects_hardlinked: IntGauge,
 }
 
 impl ActorMetrics {
@@ -112,81 +119,124 @@ impl ActorMetrics {
             snapshot_timing_inner: WORKER_METRICS.snapshot_creation_time_inner.with_label_values(&db),
             compression_timing_total: WORKER_METRICS.snapshot_compression_time_total.with_label_values(&db),
             compression_timing_inner: WORKER_METRICS.snapshot_compression_time_inner.with_label_values(&db),
+            compression_timing_single: WORKER_METRICS.snapshot_compression_time_single.with_label_values(&db),
+            compression_skipped: WORKER_METRICS.snapshot_compression_skipped.with_label_values(&db),
+            compression_compressed: WORKER_METRICS.snapshot_compression_compressed.with_label_values(&db),
+            compression_objects_compressed: WORKER_METRICS
+                .snapshot_compression_objects_compressed
+                .with_label_values(&db),
+            compression_objects_hardlinked: WORKER_METRICS
+                .snapshot_compression_objects_hardlinked
+                .with_label_values(&db),
         }
+    }
+
+    fn report_and_reset(
+        &self,
+        CompressionStats {
+            skipped,
+            compression_timings: compress_time,
+            objects,
+            // Don't reset `last_compressed`, we need it for the next run.
+            last_compressed: _,
+        }: &mut CompressionStats,
+    ) {
+        self.compression_skipped.set(*skipped as _);
+        *skipped = 0;
+
+        self.compression_compressed.set(compress_time.len() as _);
+        for duration in compress_time.drain(..) {
+            self.compression_timing_single.observe(duration.as_secs_f64());
+        }
+
+        self.compression_objects_compressed.set(objects.compressed as _);
+        self.compression_objects_hardlinked.set(objects.hardlinked as _);
+        objects.reset();
     }
 }
 
 struct SnapshotWorkerActor {
-    trigger: mpsc::UnboundedReceiver<()>,
-    committed_state: Arc<RwLock<CommittedState>>,
-    repo: Arc<SnapshotRepository>,
-    notify_tx: watch::Sender<TxOffset>,
+    snapshot_requests: mpsc::UnboundedReceiver<()>,
+    database_state: SnapshotDatabaseState,
+    snapshot_repo: Arc<SnapshotRepository>,
+    snapshot_created: watch::Sender<TxOffset>,
     metrics: ActorMetrics,
+    compression_stats: Option<CompressionStats>,
 }
 
 impl SnapshotWorkerActor {
     /// The snapshot loop takes a snapshot after each `trigger` message received.
     async fn run(mut self) {
-        while let Some(()) = self.trigger.next().await {
+        while let Some(()) = self.snapshot_requests.next().await {
             self.take_snapshot().await
         }
     }
 
-    async fn take_snapshot(&self) {
+    async fn take_snapshot(&mut self) {
         let timer = self.metrics.snapshot_timing_total.start_timer();
-        let committed_state = self.committed_state.clone();
-        let snapshot_repo = self.repo.clone();
-        let res = asyncify({
-            let inner_timer = self.metrics.snapshot_timing_inner.clone();
+        let inner_timer = self.metrics.snapshot_timing_inner.clone();
+
+        let committed_state = self.database_state.clone();
+        let snapshot_repo = self.snapshot_repo.clone();
+
+        let database_identity = self.snapshot_repo.database_identity();
+
+        let res = asyncify(move || {
+            let _timer = inner_timer.start_timer();
+            Locking::take_snapshot_internal(&committed_state, &snapshot_repo)
+        })
+        .await;
+
+        match res {
+            Err(e) => error!("Error capturing snapshot of database {database_identity}: {e:#}"),
+            Ok(None) => warn!("SnapshotWorker::take_snapshot: refusing to take snapshot of database {database_identity} at TX offset -1"),
+
+            Ok(Some((tx_offset, _path))) => {
+                let elapsed = Duration::from_secs_f64(timer.stop_and_record());
+                info!("Captured snapshot of database {database_identity} at TX offset {tx_offset} in {elapsed:?}");
+                self.snapshot_created.send_replace(tx_offset);
+                self.compress_snapshot_repo(tx_offset).await
+            }
+        }
+    }
+
+    async fn compress_snapshot_repo(&mut self, latest_snapshot: TxOffset) {
+        let timer = self.metrics.compression_timing_total.start_timer();
+        let inner_timer = self.metrics.compression_timing_inner.clone();
+
+        let database_identity = self.snapshot_repo.database_identity();
+
+        let snapshot_repo = self.snapshot_repo.clone();
+        // If we ran before, start at the last compressed snapshot,
+        // otherwise inspect all snapshots.
+        let last_compressed = self
+            .compression_stats
+            .as_ref()
+            .and_then(|stats| stats.last_compressed)
+            .unwrap_or_default();
+        let range = (last_compressed + 1)..latest_snapshot;
+        let mut stats = self.compression_stats.take().unwrap_or_default();
+
+        let (mut stats, res) = asyncify({
+            let range = range.clone();
             move || {
                 let _timer = inner_timer.start_timer();
-                Locking::take_snapshot_internal(&committed_state, &snapshot_repo)
+                let res = snapshot_repo.compress_snapshots(&mut stats, range);
+                (stats, res)
             }
         })
         .await;
-        match res {
-            Err(e) => {
-                log::error!(
-                    "Error capturing snapshot of database {:?}: {e:?}",
-                    self.repo.database_identity()
-                );
-            }
+        let elapsed = Duration::from_secs_f64(timer.stop_and_record());
+        self.metrics.report_and_reset(&mut stats);
+        // Store stats for reuse.
+        // `stats.last_compressed` is unchanged,
+        // we'll use it as the range start in the next invocation.
+        self.compression_stats = Some(stats);
 
-            Ok(None) => {
-                log::warn!(
-                    "SnapshotWorker::take_snapshot: refusing to take snapshot of database {} at TX offset -1",
-                    self.repo.database_identity()
-                );
-            }
-
-            Ok(Some((tx_offset, _path))) => {
-                let elapsed_secs = timer.stop_and_record();
-                log::info!(
-                    "Captured snapshot of database {:?} at TX offset {} in {:?}",
-                    self.repo.database_identity(),
-                    tx_offset,
-                    Duration::from_secs_f64(elapsed_secs),
-                );
-                self.notify_tx.send_replace(tx_offset);
-
-                let timer = self.metrics.compression_timing_total.start_timer();
-                let snapshot_repo = self.repo.clone();
-                asyncify({
-                    let inner_timer = self.metrics.compression_timing_inner.clone();
-                    move || {
-                        let _timer = inner_timer.start_timer();
-                        Locking::compress_older_snapshot_internal(&snapshot_repo, tx_offset)
-                    }
-                })
-                .await;
-                let elapsed_secs = timer.stop_and_record();
-                log::info!(
-                    "Compressed snapshots of database {} before offset {} in {:?}",
-                    self.repo.database_identity(),
-                    tx_offset,
-                    Duration::from_secs_f64(elapsed_secs),
-                );
-            }
+        if let Err(e) = res {
+            warn!("Error compressing snapshot range {range:?} of database {database_identity}: {e:#}");
+        } else {
+            info!("Compressed snapshot range {range:?} of database {database_identity} in {elapsed:?}");
         }
     }
 }
