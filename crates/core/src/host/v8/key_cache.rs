b1030f6a1c23c23303ecc0b6d6a456b30a21bfb8
Mazdak Farrokhzad <twingoow@gmail.com>
2025-10-03 12:04:48 +0200

    v8 -> 140.2, s/HandleScope/PinScope/g

--- crates/core/src/host/v8/key_cache.rs
+++ crates/core/src/host/v8/key_cache.rs
@@ -1,12 +1,12 @@
 use super::de::v8_interned_string;
 use core::cell::RefCell;
 use std::rc::Rc;
-use v8::{Global, HandleScope, Local};
+use v8::{Global, Local, PinScope};
 
 /// Returns a `KeyCache` for the current `scope`.
 ///
 /// Creates the cache in the scope if it doesn't exist yet.
-pub(super) fn get_or_create_key_cache(scope: &mut HandleScope<'_>) -> Rc<RefCell<KeyCache>> {
+pub(super) fn get_or_create_key_cache(scope: &PinScope<'_, '_>) -> Rc<RefCell<KeyCache>> {
     let context = scope.get_current_context();
     context.get_slot::<RefCell<KeyCache>>().unwrap_or_else(|| {
         let cache = Rc::default();
@@ -30,29 +30,29 @@ pub(super) struct KeyCache {
 
 impl KeyCache {
     /// Returns the `tag` property name.
-    pub(super) fn tag<'scope>(&mut self, scope: &mut HandleScope<'scope>) -> Local<'scope, v8::String> {
+    pub(super) fn tag<'scope>(&mut self, scope: &PinScope<'scope, '_>) -> Local<'scope, v8::String> {
         Self::get_or_create_key(scope, &mut self.tag, "tag")
     }
 
     /// Returns the `value` property name.
-    pub(super) fn value<'scope>(&mut self, scope: &mut HandleScope<'scope>) -> Local<'scope, v8::String> {
+    pub(super) fn value<'scope>(&mut self, scope: &PinScope<'scope, '_>) -> Local<'scope, v8::String> {
         Self::get_or_create_key(scope, &mut self.value, "value")
     }
 
     /// Returns the `__describe_module__` property name.
-    pub(super) fn describe_module<'scope>(&mut self, scope: &mut HandleScope<'scope>) -> Local<'scope, v8::String> {
+    pub(super) fn describe_module<'scope>(&mut self, scope: &PinScope<'scope, '_>) -> Local<'scope, v8::String> {
         Self::get_or_create_key(scope, &mut self.describe_module, "__describe_module__")
     }
 
     /// Returns the `__call_reducer__` property name.
-    pub(super) fn call_reducer<'scope>(&mut self, scope: &mut HandleScope<'scope>) -> Local<'scope, v8::String> {
+    pub(super) fn call_reducer<'scope>(&mut self, scope: &PinScope<'scope, '_>) -> Local<'scope, v8::String> {
         Self::get_or_create_key(scope, &mut self.call_reducer, "__call_reducer__")
     }
 
     /// Returns an interned string corresponding to `string`
     /// and memoizes the creation on the v8 side.
     fn get_or_create_key<'scope>(
-        scope: &mut HandleScope<'scope>,
+        scope: &PinScope<'scope, '_>,
         slot: &mut Option<Global<v8::String>>,
         string: &str,
     ) -> Local<'scope, v8::String> {
