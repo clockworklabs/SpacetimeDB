dd8c89bdf782c93f6e41db1a7cae232c99b1d508
Mazdak Farrokhzad <twingoow@gmail.com>
2025-10-03 09:25:56 +0200

    v8: improve some TODOs

--- crates/core/src/host/v8/mod.rs
+++ crates/core/src/host/v8/mod.rs
@@ -156,7 +156,7 @@ impl ModuleInstance for JsInstance {
             params,
             log_traceback,
             |tx, op, budget| {
-                // TODO(centril): snapshots, module->host calls
+                // TODO(v8): snapshots, module->host calls
                 // Setup V8 scope.
                 let mut isolate: v8::OwnedIsolate = Isolate::new(<_>::default());
                 let isolate_handle = isolate.thread_safe_handle();
@@ -179,7 +179,7 @@ impl ModuleInstance for JsInstance {
                 let energy = EnergyStats { budget, remaining };
                 let timings = ExecutionTimings {
                     total_duration,
-                    // TODO(centril): call times.
+                    // TODO(v8): call times.
                     wasm_instance_env_call_times: CallTimes::new(),
                 };
 
@@ -206,7 +206,7 @@ fn run_reducer_timeout(isolate_handle: IsolateHandle, budget: ReducerBudget) -> 
     let execution_done_flag2 = execution_done_flag.clone();
     let timeout = budget_to_duration(budget);
 
-    // TODO(centril): Using an OS thread is a bit heavy handed...?
+    // TODO(v8): Using an OS thread is a bit heavy handed...?
     thread::spawn(move || {
         // Sleep until the timeout.
         thread::sleep(timeout);
@@ -225,13 +225,13 @@ fn run_reducer_timeout(isolate_handle: IsolateHandle, budget: ReducerBudget) -> 
 }
 
 fn budget_to_duration(_budget: ReducerBudget) -> Duration {
-    // TODO(centril): This is fake logic that allows a maximum timeout.
+    // TODO(v8): This is fake logic that allows a maximum timeout.
     // Replace with sensible math.
     Duration::MAX
 }
 
 fn duration_to_budget(_duration: Duration) -> ReducerBudget {
-    // TODO(centril): This is fake logic that allows minimum energy usage.
+    // TODO(v8): This is fake logic that allows minimum energy usage.
     // Replace with sensible math.
     ReducerBudget::ZERO
 }
