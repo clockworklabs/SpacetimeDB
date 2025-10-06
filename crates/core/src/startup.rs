7034073735f7ff5f6036144124361f8796bc62dd
Phoebe Goldman <phoebe@goldman-tribe.org>
2025-10-06 12:06:41 -0400

    console_layer before fmt_layer

--- crates/core/src/startup.rs
+++ crates/core/src/startup.rs
@@ -94,9 +94,9 @@ pub fn configure_tracing(opts: TracingOptions) {
     // Is important for `tracy_layer` to be before `fmt_layer` to not print ascii codes...
     let subscriber = tracing_subscriber::Registry::default()
         .with(tracy_layer)
+        .with(console_layer)
         .with(fmt_layer)
-        .with(flame_layer)
-        .with(console_layer);
+        .with(flame_layer);
 
     if let Some(conf_file) = opts.reload_config {
         let (reload_layer, reload_handle) = tracing_subscriber::reload::Layer::new(env_filter_layer);
