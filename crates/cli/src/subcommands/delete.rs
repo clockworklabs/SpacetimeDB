1db2a4bfb715d3b95bf6ac7b1fd957345ce53799
Kim Altintop <kim@eagain.io>
2025-10-07 13:36:50 +0200

    cli: Handle delete confirmation

--- crates/cli/src/subcommands/delete.rs
+++ crates/cli/src/subcommands/delete.rs
@@ -1,7 +1,13 @@
+use std::collections::{BTreeMap, BTreeSet};
+use std::io;
+
 use crate::common_args;
 use crate::config::Config;
-use crate::util::{add_auth_header_opt, database_identity, get_auth_header};
+use crate::util::{add_auth_header_opt, database_identity, get_auth_header, y_or_n, AuthHeader};
 use clap::{Arg, ArgMatches};
+use http::StatusCode;
+use reqwest::Response;
+use spacetimedb_lib::{Hash, Identity};
 
 pub fn cli() -> clap::Command {
     clap::Command::new("delete")
@@ -22,11 +28,93 @@ pub async fn exec(mut config: Config, args: &ArgMatches) -> Result<(), anyhow::E
     let force = args.get_flag("force");
 
     let identity = database_identity(&config, database, server).await?;
-
-    let builder = reqwest::Client::new().delete(format!("{}/v1/database/{}", config.get_host_url(server)?, identity));
+    let host_url = config.get_host_url(server)?;
+    let request_path = format!("{host_url}/v1/database/{identity}");
     let auth_header = get_auth_header(&mut config, false, server, !force).await?;
-    let builder = add_auth_header_opt(builder, &auth_header);
-    builder.send().await?.error_for_status()?;
+    let client = reqwest::Client::new();
 
-    Ok(())
+    let response = send_request(&client, &request_path, &auth_header, None).await?;
+    match response.status() {
+        StatusCode::PRECONDITION_REQUIRED => {
+            let confirm = response.json::<ConfirmationResponse>().await?;
+            println!("WARNING: Deleting the database {identity} will also delete its children:");
+            confirm.print_database_tree_info(io::stdout())?;
+            if y_or_n(force, "Do you want to proceed deleting above databases?")? {
+                send_request(&client, &request_path, &auth_header, Some(confirm.token))
+                    .await?
+                    .error_for_status()?;
+            } else {
+                println!("Aborting");
+            }
+
+            Ok(())
+        }
+        StatusCode::OK => Ok(()),
+        _ => response.error_for_status().map(drop).map_err(Into::into),
+    }
+}
+
+async fn send_request(
+    client: &reqwest::Client,
+    request_path: &str,
+    auth: &AuthHeader,
+    confirmation_token: Option<Hash>,
+) -> Result<Response, reqwest::Error> {
+    let mut builder = client.delete(request_path);
+    builder = add_auth_header_opt(builder, auth);
+    if let Some(token) = confirmation_token {
+        builder = builder.query(&[("token", token.to_string())]);
+    }
+    builder.send().await
+}
+
+#[derive(serde::Deserialize)]
+struct ConfirmationResponse {
+    database_tree: DatabaseTreeInfo,
+    token: Hash,
+}
+
+impl ConfirmationResponse {
+    pub fn print_database_tree_info(&self, mut out: impl io::Write) -> anyhow::Result<()> {
+        let fmt_names = |names: &BTreeSet<String>| match names.len() {
+            0 => <_>::default(),
+            1 => format!(": {}", names.first().unwrap()),
+            _ => format!(": {names:?}"),
+        };
+
+        let tree_info = &self.database_tree;
+
+        write!(out, "{}{}", tree_info.root.identity, fmt_names(&tree_info.root.names))?;
+        for (identity, info) in &tree_info.children {
+            let names = fmt_names(&info.names);
+            let parent = info
+                .parent
+                .map(|parent| format!(" (parent: {parent})"))
+                .unwrap_or_default();
+
+            write!(out, "{identity}{parent}{names}")?;
+        }
+
+        Ok(())
+    }
+}
+
+// TODO: Should below types be in client-api?
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseTreeInfo {
+    root: RootDatabase,
+    children: BTreeMap<Identity, DatabaseInfo>,
+}
+
+#[derive(serde::Deserialize)]
+pub struct RootDatabase {
+    identity: Identity,
+    names: BTreeSet<String>,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseInfo {
+    names: BTreeSet<String>,
+    parent: Option<Identity>,
 }
