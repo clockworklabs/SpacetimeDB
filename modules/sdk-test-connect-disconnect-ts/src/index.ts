9d95af71e9716db9f6f7a1651475a7efc28f13cb
Noa <coolreader18@gmail.com>
2025-10-12 17:44:53 -0500

    Add sdk-test-connect-disconnect-ts

---
+++ modules/sdk-test-connect-disconnect-ts/src/index.ts
@@ -0,0 +1,27 @@
+// ─────────────────────────────────────────────────────────────────────────────
+// IMPORTS
+// ─────────────────────────────────────────────────────────────────────────────
+import { schema, t, table } from 'spacetimedb/server';
+
+// TODO: Remove
+export { __call_reducer__, __describe_module__ } from 'spacetimedb/server';
+
+const Connected = table(
+  { name: 'connected', public: true },
+  { identity: t.identity() }
+);
+
+const Disconnected = table(
+  { name: 'disconnected', public: true },
+  { identity: t.identity() }
+);
+
+const spacetimedb = schema(Connected, Disconnected);
+
+spacetimedb.reducer('identity_connected', ctx => {
+  ctx.db.connected.insert({ identity: ctx.sender });
+});
+
+spacetimedb.reducer('identity_disconnected', ctx => {
+  ctx.db.disconnected.insert({ identity: ctx.sender });
+});
