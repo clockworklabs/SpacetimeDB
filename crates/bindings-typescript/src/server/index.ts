42966387561e4d4570eabe8c291b71d4b793ab58
Mazdak Farrokhzad <twingoow@gmail.com>
2025-10-16 21:07:02 +0200

    use `export type`

--- crates/bindings-typescript/src/server/index.ts
+++ crates/bindings-typescript/src/server/index.ts
@@ -3,7 +3,7 @@ export { schema, type InferSchema } from './schema';
 export { table } from './table';
 export * as errors from './errors';
 export { SenderError } from './errors';
-export { Reducer, ReducerCtx } from './reducers';
+export { type Reducer, type ReducerCtx } from './reducers';
 
 import './polyfills'; // Ensure polyfills are loaded
 import './register_hooks'; // Ensure module hooks are registered
