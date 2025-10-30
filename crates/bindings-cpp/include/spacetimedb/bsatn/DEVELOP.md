ab3bdd36cc9d3708c6a5ac07e2b6dd906c967fb3
Jason Larabie <jason@clockworklabs.io>
2025-10-30 10:41:50 -0700

    Initial commit for bindings-cpp, module-test-cpp, sdk-test-cpp, and benchmarks-cpp

---
+++ crates/bindings-cpp/include/spacetimedb/bsatn/DEVELOP.md
@@ -0,0 +1,7 @@
+# Core bsatn library
+
+These files here are being used by both the Unreal C++ Client SDK and the Module Library.
+These files are the main files. The SDK do not use these files directly becouse a user might move the SDK plugin to the unreal engine plugin direcory or the unreal project directory.
+All changed made in one of the files should copied over to the SDK.
+
+SDK bsatn path: "crates\sdk-unreal\SpacetimeDbSdk\Source\SpacetimeDbSdk\Public\BSATN"
---
+++ crates/bindings-cpp/tests/type-isolation-test/test_modules/debug_large_struct.cpp
@@ -0,0 +1,46 @@
+#include <spacetimedb.h>
+#include <optional>
+
+using namespace SpacetimeDb;
+
+// ISOLATION TEST: Large struct causing client codegen issues
+// Test if EveryPrimitiveStruct alone causes the "non-special product or sum type" error
+
+// Recreate the problematic large struct
+struct EveryPrimitiveStruct {
+    uint8_t a;
+    uint16_t b;
+    uint32_t c;
+    uint64_t d;
+    u128 e;
+    u256 f;
+    int8_t g;
+    int16_t h;
+    int32_t i;
+    int64_t j;
+    i128 k;
+    i256 l;
+    bool m;
+    float n;
+    double o;
+    std::string p;
+    Identity q;
+    ConnectionId r;
+    Timestamp s;
+    TimeDuration t;
+};
+SPACETIMEDB_STRUCT(EveryPrimitiveStruct, a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t)
+
+// Simple table using the large struct
+struct TestTable { 
+    EveryPrimitiveStruct s; 
+    int32_t id;
+};
+SPACETIMEDB_STRUCT(TestTable, s, id)
+SPACETIMEDB_TABLE(TestTable, test_table, Public)
+
+// Simple reducer without problematic parameters
+SPACETIMEDB_REDUCER(test_basic, ReducerContext ctx)
+{
+    LOG_INFO("Basic reducer called");
+}
