39bbd45f14ec9dc5db88100a0c20a1088036b554
Jeffrey Dallatezza <jeffreydallatezza@gmail.com>
2025-11-06 14:20:37 -0800

    remove index stuff for now

--- crates/bindings-typescript/src/server/query.test-d.ts
+++ crates/bindings-typescript/src/server/query.test-d.ts
@@ -1,6 +1,10 @@
 import type { U32 } from '../lib/autogen/algebraic_type_variants';
 import type { Indexes } from './indexes';
-import type { ColumnExpr, IndexExprs, IndexNameUnion, RowExpr, TableSchemaAsTableDef } from './query';
+import type {
+  ColumnExpr,
+  RowExpr,
+  TableSchemaAsTableDef,
+} from './query';
 import { schema } from './schema';
 import { table, type TableIndexes } from './table';
 import t from './type_builders';
@@ -44,7 +48,7 @@ type PersonDef = {
 const tableDef = {
   name: person.tableName,
   columns: person.rowType.row,
-  indexes: person.idxs,          // keep the typed, literal tuples here
+  indexes: person.idxs, // keep the typed, literal tuples here
 } as TableSchemaAsTableDef<typeof person>;
 
 type PersonDef = typeof tableDef;
@@ -52,22 +56,12 @@ type PersonDef = typeof tableDef;
 declare const row: RowExpr<typeof tableDef>;
 const x: U32 = row.age.spacetimeType;
 
-declare const idxs: IndexExprs<PersonDef>;
-idxs.name_idx
-
-declare const xyz: IndexNameUnion<PersonDef>;
-
-declare const xx: TableIndexes<PersonDef>;
-
-//idxs.
-declare const idxs2: Indexes<PersonDef, TableIndexes<PersonDef>>;
-idxs2.id_name_idx
 //idxs2.
 
-spacetimedb.init((ctx) => {
+spacetimedb.init(ctx => {
   // ctx.db.person.
   //ctx.db.person.
   //ctx.db
   // ctx.db.person.id_name_idx.find
-  ctx.db.person.id
-})
+  ctx.db.person.id;
+});
