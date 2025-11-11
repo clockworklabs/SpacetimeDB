f7d70270113141c3a2b688fd875c5ac344ec4bf2
Zeke Foppa <bfops@users.noreply.github.com>
2025-11-11 11:22:52 -0800

    [bfops/bump-version-test]: comment

--- smoketests/tests/quickstart.py
+++ smoketests/tests/quickstart.py
@@ -361,8 +361,9 @@ class TypeScript(Rust):
 
     def server_postprocess(self, server_path: Path):
         build_typescript_sdk()
-        # We already have spacetimedb installed somehow, but it's not the right version.
-        # If we don't uninstall before installing, pnpm can panic because it can't find the specified version on npm.
+        # We already have spacetimedb as a depencency, but it's expecting to fetch from npm.
+        # If we don't uninstall before installing, pnpm can panic because it can't find
+        # the specified version on npm (even though we're about to override it anyway).
         pnpm("uninstall", 'spacetimedb', cwd=server_path)
         pnpm("install", TYPESCRIPT_BINDINGS_PATH, cwd=server_path)
 
