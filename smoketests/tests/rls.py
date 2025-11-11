a9f60ea7eac8b1307ef8653229f1e480997aec86
joshua-spacetime <josh@clockworklabs.io>
2025-11-10 22:06:58 -0800

    Apply suggestion from @joshua-spacetime
    Signed-off-by: joshua-spacetime <josh@clockworklabs.io>

--- smoketests/tests/rls.py
+++ smoketests/tests/rls.py
@@ -97,8 +97,6 @@ pub struct Users {
 pub fn add_user(ctx: &ReducerContext, name: String) {
     ctx.db.users().insert(Users { name, identity: ctx.sender });
 }
-
-// RLS
 """
     
     ADD_RLS = """ 
