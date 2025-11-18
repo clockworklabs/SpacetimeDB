9c594af5768649d8e0e4ca6ba27e8e4b0d503b44
Zeke Foppa <bfops@users.noreply.github.com>
2025-11-18 15:02:29 -0800

    [bfops/run-dotnet-format]: comment

--- crates/cli/src/tasks/csharp.rs
+++ crates/cli/src/tasks/csharp.rs
@@ -110,6 +110,8 @@ pub(crate) fn build_csharp(project_path: &Path, build_debug: bool) -> anyhow::Re
 
 pub(crate) fn dotnet_format(project_dir: &PathBuf, files: impl IntoIterator<Item = PathBuf>) -> anyhow::Result<()> {
     let cwd = std::env::current_dir().unwrap_or_else(|_| project_dir.clone());
+    // Resolve absolute paths for all of the files, because we receive them as relative paths to cwd, but
+    // `dotnet format` will interpret those paths relative to `project_dir`.
     let files: Vec<OsString> = files
         .into_iter()
         .map(|f| {
