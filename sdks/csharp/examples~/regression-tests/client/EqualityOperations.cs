42b113ed45e3ec022322943c1fa5d80db0eb4c90
Jason Larabie <jason@clockworklabs.io>
2025-11-20 20:38:31 -0800

    Fixed up the Equality Operations

--- sdks/csharp/examples~/regression-tests/client/EqualityOperations.cs
+++ sdks/csharp/examples~/regression-tests/client/EqualityOperations.cs
@@ -22,10 +22,4 @@ public static class EqualityOperations
                lh.Name == rh.Name &&
                lh.Level == rh.Level;
     }
-
-    public static bool Equals(this MyTable lh, MyTable rh)
-    {
-        return lh.Field.A == rh.Field.A &&
-               lh.Field.B == rh.Field.B;
-    }
 }
