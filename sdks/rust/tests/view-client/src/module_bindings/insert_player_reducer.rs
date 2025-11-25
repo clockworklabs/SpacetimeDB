6935a4a354d3f31abd7d0b9e0bda3f9fdc211f93
Phoebe Goldman <phoebe@goldman-tribe.org>
2025-11-25 17:29:01 -0500

    Update view_client bindings

--- sdks/rust/tests/view-client/src/module_bindings/insert_player_reducer.rs
+++ sdks/rust/tests/view-client/src/module_bindings/insert_player_reducer.rs
@@ -65,6 +65,7 @@ impl insert_player for super::RemoteReducers {
         InsertPlayerCallbackId(self.imp.on_reducer(
             "insert_player",
             Box::new(move |ctx: &super::ReducerEventContext| {
+                #[allow(irrefutable_let_patterns)]
                 let super::ReducerEventContext {
                     event:
                         __sdk::ReducerEvent {
