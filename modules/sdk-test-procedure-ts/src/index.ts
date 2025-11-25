d19fd5fcf38844be6b1470e557a04c97f6c0304b
Mazdak Farrokhzad <twingoow@gmail.com>
2025-11-25 12:24:05 +0100

    scheduling fix test module for ts

--- modules/sdk-test-procedure-ts/src/index.ts
+++ modules/sdk-test-procedure-ts/src/index.ts
@@ -3,6 +3,7 @@
 // ─────────────────────────────────────────────────────────────────────────────
 import {
   errors,
+  ScheduleAt,
   schema,
   t,
   table,
@@ -25,7 +26,30 @@ const MyTable = table(
   { field: ReturnStruct }
 );
 
-const spacetimedb = schema(MyTable);
+const ScheduledProcTable = t.row({
+  scheduled_id: t.u64().primaryKey().autoInc(),
+  scheduled_at: t.scheduleAt(),
+  reducer_ts: t.timestamp(),
+  x: t.u8(),
+  y: t.u8(),
+});
+const ScheduledProcTableTable = table(
+  { name: 'scheduled_proc_table', scheduled: 'scheduled_proc' },
+  ScheduledProcTable
+);
+
+const ProcInsertsInto = t.row({
+  reducer_ts: t.timestamp(),
+  procedure_ts: t.timestamp(),
+  x: t.u8(),
+  y: t.u8(),
+});
+const ProcInsertsIntoTable = table(
+  { name: 'proc_inserts_into', public: true },
+  ProcInsertsInto
+);
+
+const spacetimedb = schema(MyTable, ScheduledProcTableTable, ProcInsertsIntoTable);
 
 spacetimedb.procedure(
   'return_primitive',
@@ -117,5 +141,31 @@ spacetimedb.procedure('insert_with_tx_rollback', t.unit(), ctx => {
     if (e !== error) throw e;
   }
   assertRowCount(ctx, 0);
+  return {};
+});
+
+spacetimedb.reducer('schedule_proc', {}, ctx => {
+  ctx.db.scheduled_proc_table.insert({
+    scheduled_id: 0n,
+    scheduled_at: ScheduleAt.interval(1000000n),
+    reducer_ts: ctx.timestamp,
+    x: 42,
+    y: 24,
+  })
+});
+
+spacetimedb.procedure('scheduled_proc', { data: ScheduledProcTable }, t.unit(), (ctx, { data }) => {
+  const reducer_ts = data.reducer_ts;
+  const x = data.x;
+  const y = data.y;
+  const procedure_ts = ctx.timestamp;
+  ctx.withTx(ctx => {
+    ctx.db.proc_inserts_into.insert({
+      reducer_ts,
+      procedure_ts,
+      x,
+      y
+    });
+  });
   return {};
 });
