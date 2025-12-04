07129b5665cde21853382da058ae87e599b6d1b1
Jeffrey Dallatezza <jeffreydallatezza@gmail.com>
2025-12-04 09:18:31 -0800

    Add some query builder type tests to the view test file

--- crates/bindings-typescript/src/lib/view.test-d.ts
+++ crates/bindings-typescript/src/lib/view.test-d.ts
@@ -1,13 +1,12 @@
+import { literal } from '../server';
 import { schema } from './schema';
 import { table } from './table';
-import { type ViewFn } from './views';
 import t from './type_builders';
-import { type RowTypedQuery } from '../server/query';
 
 const person = table(
   { name: 'person' },
   {
-    id: t.u32(),
+    id: t.u32().primaryKey(),
     name: t.string(),
   }
 );
@@ -24,7 +23,7 @@ const personWithExtra = table(
 const order = table(
   { name: 'order' },
   {
-    id: t.u32(),
+    id: t.u32().primaryKey(),
     name2: t.string(),
     person_id: t.u32(),
   }
@@ -34,12 +33,12 @@ const spacetime = schema(person, order, personWithExtra);
 
 const arrayRetValue = t.array(person.rowType);
 
-spacetime.anonymousView({ name: 'idk', public: true }, arrayRetValue, ctx => {
+spacetime.anonymousView({ name: 'v1', public: true }, arrayRetValue, ctx => {
   return ctx.from.person.build();
 });
 
 spacetime.anonymousView(
-  { name: 'idk', public: true },
+  { name: 'v2', public: true },
   arrayRetValue,
   // @ts-expect-error returns a query of the wrong type.
   ctx => {
@@ -47,6 +46,20 @@ spacetime.anonymousView(
   }
 );
 
-spacetime.anonymousView({ name: 'idk', public: true }, arrayRetValue, ctx => {
+// We should eventually make this fail.
+spacetime.anonymousView({ name: 'v3', public: true }, arrayRetValue, ctx => {
   return ctx.from.personWithExtra.build();
+});
+
+spacetime.anonymousView({ name: 'v4', public: true }, arrayRetValue, ctx => {
+  // @ts-expect-error this uses a literal of the wrong type.
+  const x = ctx.from.person.where(row => row.id.eq(literal('string'))).build();
+  return ctx.from.person.where(row => row.id.eq(literal(5))).build();
+});
+
+spacetime.anonymousView({ name: 'v5', public: true }, arrayRetValue, ctx => {
+  return ctx.from.person
+    .where(row => row.id.eq(literal(5)))
+    .semijoinLeft(ctx.from.order, (p, o) => p.id.eq(o.id))
+    .build();
 });
