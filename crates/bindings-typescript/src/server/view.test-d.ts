9a1209206bf81ccbd5c6b8e2b0ee5068e33373c0
Jeffrey Dallatezza <jeffreydallatezza@gmail.com>
2025-12-04 09:22:05 -0800

    move the view type tests to server

--- crates/bindings-typescript/src/server/view.test-d.ts
+++ crates/bindings-typescript/src/server/view.test-d.ts
@@ -1,7 +1,7 @@
-import { literal } from '../server';
-import { schema } from './schema';
-import { table } from './table';
-import t from './type_builders';
+import { literal } from '.';
+import { schema } from '../lib/schema';
+import { table } from '../lib/table';
+import t from '../lib/type_builders';
 
 const person = table(
   { name: 'person' },
