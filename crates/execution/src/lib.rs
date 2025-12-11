5bb1c0a0c994e19d1f2c6f61d013bd9e57838c34
Egor Vorontsov <sdoregor@sdore.me>
2025-12-11 22:35:46 +0300

    Fixed for updated Views.

--- crates/execution/src/lib.rs
+++ crates/execution/src/lib.rs
@@ -121,7 +121,7 @@ impl Hash for Row<'_> {
 impl Row<'_> {
     pub fn to_product_value(&self) -> ProductValue {
         match self {
-            Self::Null => ProductValue { elements: Box::new([]) },
+            Self::Null => [].into(),
             Self::Ptr(ptr) => ptr.to_product_value(),
             Self::Ref(val) => (*val).clone(),
         }
@@ -129,6 +129,7 @@ impl Row<'_> {
 
     pub fn project_product(self, cols: &ColList) -> Result<ProductValue, InvalidFieldError> {
         match self {
+            Self::Null => Ok([].into()),
             Self::Ptr(ptr) => ptr.project_product(cols),
             Self::Ref(val) => val.project_product(cols),
         }
