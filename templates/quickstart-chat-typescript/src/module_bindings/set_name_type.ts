a50ee6f1fb18a971a937ff1e85a302477cc080f7
Piotr Sarnacki <drogus@gmail.com>
2026-01-07 15:18:20 +0100

    Module bindings

--- templates/quickstart-chat-typescript/src/module_bindings/set_name_type.ts
+++ templates/quickstart-chat-typescript/src/module_bindings/set_name_type.ts
@@ -8,8 +8,10 @@ import {
   t as __t,
   type AlgebraicTypeType as __AlgebraicTypeType,
   type Infer as __Infer,
-} from 'spacetimedb';
+} from "spacetimedb";
 
-export default __t.object('SetName', {
+export default __t.object("SetName", {
   name: __t.string(),
 });
+
+
