b1fb0b73ca62d4f3113f78bb842f8011b148d9ce
= <cloutiertyler@gmail.com>
2026-01-16 16:27:17 -0500

    Fix C# template table names to use PascalCase

--- templates/basic-c-sharp/spacetimedb/Lib.cs
+++ templates/basic-c-sharp/spacetimedb/Lib.cs
@@ -2,7 +2,7 @@ using SpacetimeDB;
 
 public static partial class Module
 {
-    [SpacetimeDB.Table(Name = "person", Public = true)]
+    [SpacetimeDB.Table(Name = "Person", Public = true)]
     public partial struct Person
     {
         public string Name;
@@ -11,13 +11,13 @@ public static partial class Module
     [SpacetimeDB.Reducer]
     public static void Add(ReducerContext ctx, string name)
     {
-        ctx.Db.person.Insert(new Person { Name = name });
+        ctx.Db.Person.Insert(new Person { Name = name });
     }
 
     [SpacetimeDB.Reducer]
     public static void SayHello(ReducerContext ctx)
     {
-        foreach (var person in ctx.Db.person.Iter())
+        foreach (var person in ctx.Db.Person.Iter())
         {
             Log.Info($"Hello, {person.Name}!");
         }
