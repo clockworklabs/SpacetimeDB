f059387ea408fd3038a409b5089d12996f9772bf
Piotr Sarnacki <drogus@gmail.com>
2026-01-27 17:02:37 +0100

    Regenerate bindings

--- templates/chat-react-ts/src/module_bindings/send_message_type.ts
+++ templates/chat-react-ts/src/module_bindings/send_message_type.ts
@@ -8,8 +8,10 @@ import {
   t as __t,
   type AlgebraicTypeType as __AlgebraicTypeType,
   type Infer as __Infer,
-} from 'spacetimedb';
+} from "spacetimedb";
 
-export default __t.object('SendMessage', {
+export default __t.object("SendMessage", {
   text: __t.string(),
 });
+
+
