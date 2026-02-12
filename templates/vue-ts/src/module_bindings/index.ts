9396687041751965986ca0bbb1a303fb2404ad7b
Noa <coolreader18@gmail.com>
2026-02-12 17:09:42 -0600

    Regen template

--- templates/vue-ts/src/module_bindings/index.ts
+++ templates/vue-ts/src/module_bindings/index.ts
@@ -45,16 +45,16 @@ import PersonRow from './person_table';
 /** Type-only namespace exports for generated type groups. */
 
 /** The schema information for all tables in this module. This is defined the same was as the tables would have been defined in the server. */
-const tablesSchema = __schema(
-  __table(
+const tablesSchema = __schema({
+  person: __table(
     {
       name: 'person',
       indexes: [],
       constraints: [],
     },
     PersonRow
-  )
-);
+  ),
+});
 
 /** The schema information for all reducers in this module. This is defined the same way as the reducers would have been defined in the server, except the body of the reducer is omitted in code generation. */
 const reducersSchema = __reducers(
@@ -80,7 +80,7 @@ const REMOTE_MODULE = {
 >;
 
 /** The tables available in this remote SpacetimeDB module. */
-export const tables = __convertToAccessorMap(tablesSchema.schemaType.tables);
+export const tables = tablesSchema.schemaType.tables;
 
 /** A typed query builder for this remote SpacetimeDB module. */
 export const query: __QueryBuilder<typeof tablesSchema.schemaType> =
