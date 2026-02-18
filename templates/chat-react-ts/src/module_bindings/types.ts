d9866e160504a8ca4e9e04f8f3be030481392217
Noa <coolreader18@gmail.com>
2026-02-17 22:26:52 -0600

    Run pnpm generate

--- templates/chat-react-ts/src/module_bindings/types.ts
+++ templates/chat-react-ts/src/module_bindings/types.ts
@@ -10,6 +10,16 @@ import {
   type Infer as __Infer,
 } from 'spacetimedb';
 
-export default __t.row({
-  name: __t.string(),
+export const Message = __t.object('Message', {
+  sender: __t.identity(),
+  sent: __t.timestamp(),
+  text: __t.string(),
 });
+export type Message = __Infer<typeof Message>;
+
+export const User = __t.object('User', {
+  identity: __t.identity(),
+  name: __t.option(__t.string()),
+  online: __t.bool(),
+});
+export type User = __Infer<typeof User>;
