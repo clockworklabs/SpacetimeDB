805a3c5520fb0f98ac80200ab8002470d11f2e42
Noa <coolreader18@gmail.com>
2026-02-18 00:07:51 -0600

    Update imports in tests

--- crates/bindings-typescript/tests/db_connection.test.ts
+++ crates/bindings-typescript/tests/db_connection.test.ts
@@ -1,10 +1,9 @@
+import { beforeEach, describe, expect, test } from 'vitest';
+import { ConnectionId, Identity, type Infer } from '../src';
+import { ServerMessage } from '../src/sdk/client_api/types';
+import WebsocketTestAdapter from '../src/sdk/websocket_test_adapter';
 import { DbConnection } from '../test-app/src/module_bindings';
 import User from '../test-app/src/module_bindings/user_table';
-import { beforeEach, describe, expect, test } from 'vitest';
-import { ConnectionId, type Infer } from '../src';
-import ServerMessage from '../src/sdk/client_api/server_message_type';
-import { Identity } from '../src';
-import WebsocketTestAdapter from '../src/sdk/websocket_test_adapter';
 import {
   anIdentity,
   bobIdentity,
