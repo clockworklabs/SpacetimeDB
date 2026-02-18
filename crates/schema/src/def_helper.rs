c72f6d45bd7fa2792936cf7152d70023459fd9fb
Shubham Mishra <shivam828787@gmail.com>
2026-02-18 22:05:17 +0530

    fix test

---
+++ crates/schema/src/def_helper.rs
@@ -0,0 +1,7 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+use crate::def::{
+    ColumnDef, ConstraintDef, IndexAlgorithm, IndexDef, ModuleDef, SequenceDef, TableDef, TypeDef, ViewColumnDef,
+    ViewDef, ViewParamDef,
+};
