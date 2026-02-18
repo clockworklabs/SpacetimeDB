e152624f5841affd8dc5a58926050d59b2ec370c
Jason Larabie <jason@clockworklabs.io>
2026-02-18 14:37:12 -0800

    Update the notice to mark we're in beta and 2.0 is coming soon

--- docs/src/components/CppModuleVersionNotice.tsx
+++ docs/src/components/CppModuleVersionNotice.tsx
@@ -5,9 +5,9 @@ export function CppModuleVersionNotice(): JSX.Element {
   return (
     <Admonition type="important" title="C++ Modules and SpacetimeDB 2.0">
       <p>
-        SpacetimeDB <code>2.0</code> is available for other module languages, but C++ server modules
-        are currently pinned to <code>v1.12.0</code>. If you are following the C++ tab in this guide,
-        use the <code>v1.12.0</code> release track for now.
+        C++ support is currently in beta and subject to change. SpacetimeDB C++ <code>2.0</code> is
+        coming soon, but C++ server modules are currently pinned to <code>v1.12.0</code>. If you are
+        following the C++ tab in this guide, use the <code>v1.12.0</code> release track for now.
       </p>
     </Admonition>
   );
