7923d6f64b8c0f60fe2eb636054494d7d2136076
Jeffrey Dallatezza <jeffreydallatezza@gmail.com>
2026-02-23 15:09:42 -0800

    Update default doc version to 2.0.0 (#4411)
    # Description of Changes
    
    Docs will now go to version 2.0.0 by default, and there is a banner with
    a link to the newer docs if someone is viewing the v1 docs.
    
    # Expected complexity level and risk
    
    1. Biggest risk here is breaking links.
    
    # Testing
    
    I tested this locally, and checked that links are working.
    
    There is also a broken link checker in the CI.

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -104,12 +104,12 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           sidebarCollapsed: false,
           includeCurrentVersion: true,
-          lastVersion: '1.12.0',
+          lastVersion: 'current',
           versions: {
             current: {
-              label: '2.0.0-rc1',
-              path: '2.0.0-rc1',
-              banner: 'unreleased',
+              label: '2.0.0',
+              path: '',
+              banner: 'none',
             },
             '1.12.0': {
               label: '1.12.0',
