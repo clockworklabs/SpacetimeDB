2fbfd823af9dffa550892a7da0636f7b5a0bff0c
joshua-spacetime <josh@clockworklabs.io>
2026-03-03 22:17:49 -0800

    csharp tests: remove Nullable-named helper classes

--- sdks/csharp/tests~/QueryBuilderTests.cs
+++ sdks/csharp/tests~/QueryBuilderTests.cs
@@ -40,30 +40,6 @@ public sealed class QueryBuilderTests
     private static Table<Row, RowCols, RowIxCols> MakeTable(string tableName) =>
         new(tableName, new RowCols(tableName), new RowIxCols(tableName));
 
-    private sealed class RowNullableCols
-    {
-        public Col<Row, string> Name { get; }
-        public Col<Row, int> Age { get; }
-
-        public RowNullableCols(string tableName)
-        {
-            Name = new Col<Row, string>(tableName, "Name");
-            Age = new Col<Row, int>(tableName, "Age");
-        }
-    }
-
-    private sealed class RowNullableIxCols
-    {
-        public IxCol<Row, string> Name { get; }
-
-        public RowNullableIxCols(string tableName)
-        {
-            Name = new IxCol<Row, string>(tableName, "Name");
-        }
-    }
-
-    private static Table<Row, RowNullableCols, RowNullableIxCols> MakeNullableTable(string tableName) =>
-        new(tableName, new RowNullableCols(tableName), new RowNullableIxCols(tableName));
 
     private sealed class LeftCols
     {
@@ -111,31 +87,6 @@ public sealed class QueryBuilderTests
     private static Table<RightRow, RightCols, RightIxCols> MakeRightTable(string tableName) =>
         new(tableName, new RightCols(tableName), new RightIxCols(tableName));
 
-    private sealed class LeftNullableIxCols
-    {
-        public IxCol<LeftRow, int> Id { get; }
-
-        public LeftNullableIxCols(string tableName)
-        {
-            Id = new IxCol<LeftRow, int>(tableName, "id");
-        }
-    }
-
-    private sealed class RightNullableIxCols
-    {
-        public IxCol<RightRow, int> Uid { get; }
-
-        public RightNullableIxCols(string tableName)
-        {
-            Uid = new IxCol<RightRow, int>(tableName, "uid");
-        }
-    }
-
-    private static Table<LeftRow, LeftCols, LeftNullableIxCols> MakeLeftNullableIxTable(string tableName) =>
-        new(tableName, new LeftCols(tableName), new LeftNullableIxCols(tableName));
-
-    private static Table<RightRow, RightCols, RightNullableIxCols> MakeRightNullableIxTable(string tableName) =>
-        new(tableName, new RightCols(tableName), new RightNullableIxCols(tableName));
 
     [Fact]
     public void All_QuotesTableName()
@@ -175,10 +126,10 @@ public sealed class QueryBuilderTests
     }
 
     [Fact]
-    public void Where_BoolColumn_ImplicitlyConvertsToBoolExpr()
+    public void Where_BoolColumn_FormatsCorrectly()
     {
         var table = MakeTable("T");
-        var sql = table.Where(c => c.IsAdmin).ToSql();
+        var sql = table.Where(c => c.IsAdmin.Eq(true)).ToSql();
         Assert.Equal("SELECT * FROM \"T\" WHERE (\"T\".\"IsAdmin\" = TRUE)", sql);
     }
 
@@ -281,22 +232,6 @@ public sealed class QueryBuilderTests
             "SELECT \"users\".* FROM \"users\" JOIN \"other\" ON \"users\".\"id\" = \"other\".\"uid\"",
             sql
         );
-    }
-
-    [Fact]
-    public void Where_NullableCol_Eq_FormatsCorrectly()
-    {
-        var table = MakeNullableTable("T");
-        var sql = table.Where(c => c.Name.Eq("x")).ToSql();
-        Assert.Equal("SELECT * FROM \"T\" WHERE (\"T\".\"Name\" = 'x')", sql);
-    }
-
-    [Fact]
-    public void Where_NullableCol_Gt_FormatsCorrectly()
-    {
-        var table = MakeNullableTable("T");
-        var sql = table.Where(c => c.Age.Gt(123)).ToSql();
-        Assert.Equal("SELECT * FROM \"T\" WHERE (\"T\".\"Age\" > 123)", sql);
     }
 
     [Fact]
