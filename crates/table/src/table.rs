643810ae01503aac61df9426a9a0e62bea120f67
Mazdak Farrokhzad <twingoow@gmail.com>
2026-04-10 10:57:37 +0000

    TableIndex: privatize indexed_columns

--- crates/table/src/table.rs
+++ crates/table/src/table.rs
@@ -1455,7 +1455,7 @@ impl Table {
                     Found violation at pointer {ptr:?} to row {:?}.",
                     self.schema.table_name,
                     self.schema.table_id,
-                    index.indexed_columns,
+                    index.indexed_columns(),
                     index.key_type(),
                     row,
                 )
@@ -1556,7 +1556,7 @@ impl Table {
     pub fn get_index_by_cols(&self, cols: &ColList) -> Option<(IndexId, &TableIndex)> {
         self.indexes
             .iter()
-            .find(|(_, index)| &index.indexed_columns == cols)
+            .find(|(_, index)| index.indexed_columns() == cols)
             .map(|(id, idx)| (*id, idx))
     }
 
@@ -2278,7 +2278,7 @@ impl UniqueConstraintViolation {
 
         // Fetch the names of the columns used in the index.
         let cols = schema
-            .get_columns(&index.indexed_columns)
+            .get_columns(index.indexed_columns())
             .map(|(_, cs)| cs.unwrap().col_name.clone())
             .collect();
 
@@ -2305,7 +2305,7 @@ impl Table {
         index_id: IndexId,
         row: RowRef<'_>,
     ) -> UniqueConstraintViolation {
-        let value = row.project(&index.indexed_columns).unwrap();
+        let value = index.project_row(row);
         let schema = self.get_schema();
         UniqueConstraintViolation::build(schema, index, index_id, value)
     }
@@ -2429,6 +2429,7 @@ pub(crate) mod test {
     use super::*;
     use crate::blob_store::{HashMapBlobStore, NullBlobStore};
     use crate::page::tests::hash_unmodified_save_get;
+    use crate::table_index::KeySize;
     use crate::var_len::VarLenGranule;
     use proptest::prelude::*;
     use proptest::test_runner::TestCaseResult;
@@ -2561,8 +2562,7 @@ pub(crate) mod test {
             .iter()
             .map(|row_ptr| {
                 let row_ref = table.get_row_ref(blob_store, row_ptr).unwrap();
-                let key = row_ref.project(&index.indexed_columns).unwrap();
-                crate::table_index::KeySize::key_size_in_bytes(&key) as u64
+                index.project_row(row_ref).key_size_in_bytes() as u64
             })
             .sum()
     }
--- crates/table/src/table_index/mod.rs
+++ crates/table/src/table_index/mod.rs
@@ -1832,7 +1832,7 @@ pub struct TableIndex {
     /// Given a full row, typed at some `ty: ProductType`,
     /// these columns are the ones that this index indexes.
     /// Projecting the `ty` to `self.indexed_columns` yields the index's type `self.key_type`.
-    pub indexed_columns: ColList,
+    indexed_columns: ColList,
 }
 
 impl MemoryUsage for TableIndex {
@@ -1886,6 +1886,11 @@ impl TableIndex {
     /// Returns whether this index supports range queries.
     pub fn is_ranged(&self) -> bool {
         self.idx.is_ranged()
+    }
+
+    /// Returns the indexed columns of this index.
+    pub fn indexed_columns(&self) -> &ColList {
+        &self.indexed_columns
     }
 
     /// Returns the key type of this index.
@@ -2320,6 +2325,15 @@ impl TableIndex {
         // 1. We're passing the same `ColList` that was provided during construction.
         // 2. Forward caller requirements.
         unsafe { TypedIndexKey::from_row_ref(&self.key_type, &self.idx, &self.indexed_columns, row_ref) }.into()
+    }
+
+    /// Projects `row_ref` to the columns of `self`.
+    ///
+    /// May panic if `row_ref` doesn't belong to the same table as this inex.
+    pub fn project_row(&self, row_ref: RowRef<'_>) -> AlgebraicValue {
+        row_ref
+            .project(&self.indexed_columns)
+            .expect("`row_ref` should belong to the same table as this index")
     }
 
     /// Inserts `ptr` with the value `row` to this index.
