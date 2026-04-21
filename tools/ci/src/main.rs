9872931bb75787bf8ff5c8daef5032244cf611e6
Zeke Foppa <bfops@users.noreply.github.com>
2026-04-21 12:22:22 -0700

    [bfops/test-pnpm-build]: fix?

--- tools/ci/src/main.rs
+++ tools/ci/src/main.rs
@@ -306,6 +306,8 @@ fn main() -> Result<()> {
 
     match cli.cmd {
         Some(CiCmd::Test) => {
+            cmd!("pnpm", "build").dir("crates/bindings-typescript").run()?;
+
             // TODO: This doesn't work on at least user Linux machines, because something here apparently uses `sudo`?
 
             // Exclude smoketests from `cargo test --all` since they require pre-built binaries.
@@ -368,7 +370,6 @@ fn main() -> Result<()> {
                 "--test-threads=1",
             )
             .run()?;
-            cmd!("pnpm", "build").dir("crates/bindings-typescript").run()?;
             cmd!("bash", "tools/check-diff.sh").run()?;
             cmd!(
                 "cargo",
