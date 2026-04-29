bdac0fc5531e0cab47bbf8cdae57eea91b8b4730
Lisandro Crespo <hey@lisandroct.com>
2026-04-29 09:43:13 -0500

    Add Godot SDK

---
+++ sdks/csharp/src/GodotDebugLogger.cs
@@ -0,0 +1,32 @@
+﻿/*  SpacetimeDB logging for Godot
+ *  *  This class is only used in Godot projects.
+ *  *
+ */
+#if GODOT
+namespace SpacetimeDB
+{
+    internal class GodotDebugLogger : ISpacetimeDBLogger
+    {
+        public void Debug(string message) =>
+            GD.Print(message);
+
+        public void Trace(string message) =>
+            GD.Print(message);
+
+        public void Info(string message) =>
+            GD.Print(message);
+
+        public void Warn(string message) =>
+            GD.PushWarning(message);
+
+        public void Error(string message) =>
+            GD.PrintErr(message);
+
+        public void Exception(string message) =>
+            GD.PrintErr(message);
+
+        public void Exception(Exception e) =>
+            GD.PushError(e);
+    }
+}
+#endif
