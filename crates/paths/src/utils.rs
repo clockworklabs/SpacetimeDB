7f55d664df1de84423799617ce4382a78bc8c5d2
Kim Altintop <kim@eagain.io>
2026-04-30 16:45:33 +0200

    Apparently, a directory can't be synced on windows

--- crates/paths/src/utils.rs
+++ crates/paths/src/utils.rs
@@ -118,6 +118,7 @@ macro_rules! path_type {
                 tmp.write_all(contents.as_ref())?;
                 tmp.as_file().sync_all()?;
                 tmp.persist(&path)?;
+                #[cfg(not(target_os = "windows"))]
                 std::fs::File::open(parent)?.sync_all()?;
 
                 Ok(())
