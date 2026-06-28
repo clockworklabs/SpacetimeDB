6da287d5fe62d6d8433e19be795b9f668a881c38
Shubham Mishra <shivam828787@gmail.com>
2026-06-28 15:55:45 +0530

    make lib and async

--- crates/dst/src/sim/commitlog.rs
+++ crates/dst/src/sim/commitlog.rs
@@ -22,6 +22,12 @@ pub struct InMemoryCommitlog {
     options: Options,
 }
 
+impl Default for InMemoryCommitlog {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl InMemoryCommitlog {
     pub fn new() -> Self {
         Self {
