0b48f882eeac7696a94bf41f98bc73cb6526c084
Shubham Mishra <shivam828787@gmail.com>
2026-08-04 23:39:13 +0530

    config

--- crates/dst/src/config.rs
+++ crates/dst/src/config.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use crate::engine::EngineConfig;
 
 #[derive(Clone, Debug, PartialEq)]
@@ -31,5 +33,72 @@ pub enum RunTargetConfig {
     Replication(ReplicationConfig),
 }
 
-#[derive(Clone, Debug, Default, PartialEq, Eq)]
-pub struct ReplicationConfig {}
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ReplicationConfig {
+    pub cluster: ReplicationClusterConfig,
+    pub workload: ReplicationWorkloadConfig,
+    pub properties: ReplicationPropertyConfig,
+}
+
+impl Default for ReplicationConfig {
+    fn default() -> Self {
+        Self {
+            cluster: ReplicationClusterConfig::default(),
+            workload: ReplicationWorkloadConfig::default(),
+            properties: ReplicationPropertyConfig::default(),
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ReplicationClusterConfig {
+    pub replica_counts: Vec<usize>,
+    pub replica_id_base: ReplicationReplicaIdBase,
+}
+
+impl Default for ReplicationClusterConfig {
+    fn default() -> Self {
+        Self {
+            replica_counts: vec![3, 5, 7],
+            replica_id_base: ReplicationReplicaIdBase::Random,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum ReplicationReplicaIdBase {
+    Random,
+    Fixed(u64),
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct ReplicationWorkloadConfig {
+    pub client_count: usize,
+    pub max_bytes_len: usize,
+    pub max_string_len: usize,
+    pub leader_poll_interval: Option<Duration>,
+}
+
+impl Default for ReplicationWorkloadConfig {
+    fn default() -> Self {
+        Self {
+            client_count: 10,
+            max_bytes_len: 256,
+            max_string_len: 128,
+            leader_poll_interval: None,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct ReplicationPropertyConfig {
+    pub cluster_availability_timeout_polls: u32,
+}
+
+impl Default for ReplicationPropertyConfig {
+    fn default() -> Self {
+        Self {
+            cluster_availability_timeout_polls: 10,
+        }
+    }
+}
