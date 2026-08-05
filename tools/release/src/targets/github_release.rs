6279e646a9cf82280618a88fdd317bcb87475407
Zeke Foppa <bfops@users.noreply.github.com>
2026-08-05 13:13:43 -0700

    [bfops/release-fail]: Release - Fail step if already published

--- tools/release/src/targets/github_release.rs
+++ tools/release/src/targets/github_release.rs
@@ -91,8 +91,7 @@ impl ReleaseTarget for GithubRelease {
     fn release(&self) -> Result<(), String> {
         let release = self.fetch_release()?;
         if !release.is_draft {
-            println!("GitHub release {} is already published: {}", self.version, release.url);
-            return Ok(());
+            bail!("GitHub release {} is already published: {}", self.version, release.url);
         }
 
         println!("Found draft GitHub release {}: {}", self.version, release.url);
