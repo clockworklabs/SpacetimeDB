b3f0234f6c1a37e199c05ac43334081156ced029
Zeke Foppa <bfops@users.noreply.github.com>
2026-08-12 12:53:31 -0700

    [bot/split-public-cargo-ci]: upload timing reports

--- tools/ci/src/main.rs
+++ tools/ci/src/main.rs
@@ -108,7 +108,7 @@ fn is_skipped(command: &Command, skips: &[String]) -> bool {
 }
 
 fn run_package(package: &str, args: &[String]) -> Result<()> {
-    let mut cargo_args = vec!["run", "--package", package, "--"];
+    let mut cargo_args = vec!["run", "--timings", "--package", package, "--"];
     cargo_args.extend(args.iter().map(String::as_str));
     cmd("cargo", cargo_args).run()?;
     Ok(())
